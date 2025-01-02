3f10fea4977b99f2a404f01f6c6f60360842ad31
Lpik97 <dm97fx@gmail.com>
2025-01-02 15:01:12 +0100

    feat: Add constants for header components' text and alt attributes

---
+++ src/components/header/data/constants.tsx
@@ -0,0 +1,4 @@
+export const LOGO_ALT: string = "Logo";
+export const PROJECTS_TEXT: string = "PROJECTS";
+export const CONTACT_TEXT: string = "CONTACT";
+export const MODE_SWITCHER_ALT: string = "Mode switcher";
