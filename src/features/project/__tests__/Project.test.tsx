3097af411865151cb7aed5c6a0cccaff511fbc15
Lpik97 <dm97fx@gmail.com>
2025-01-03 11:20:28 +0100

    test: Add unit tests for Project component rendering and functionality

---
+++ src/features/project/__tests__/Project.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import Project from "../Project";
+
+describe("Project component tests", () => {
+   test("renders project element", () => {
+      render(<Project />);
+      const projectElement = screen.getByLabelText("Project container");
+      expect(projectElement).toBeInTheDocument();
+   });
+
+   test("renders project main cover", () => {
+      render(<Project />);
+      const projectMainCover = screen.getByRole("region", { name: /main cover/i });
+      expect(projectMainCover).toBeInTheDocument();
+   });
+
+   test("renders project overview", () => {
+      render(<Project />);
+      const projectOverview = screen.getByRole("region", { name: /project overview/i });
+      expect(projectOverview).toBeInTheDocument();
+   });
+
+   test("renders project results", () => {
+      render(<Project />);
+      const projectResults = screen.getByRole("region", { name: /project results/i });
+      expect(projectResults).toBeInTheDocument();
+   });
+});
