df979e661ae0d7de6be91a3db4ae5d5b1feacf1f
Lpik97 <dm97fx@gmail.com>
2025-01-03 09:57:31 +0100

    test: Add unit tests for ProjectsList component rendering and functionality

---
+++ src/features/projectsList/__tests__/ProjectsList.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import ProjectsList from "../ProjectsList";
+
+describe("ProjectsList component tests", () => {
+   test("renders the projects list element", () => {
+      render(<ProjectsList />);
+      const projectsList = screen.getByRole("main");
+      expect(projectsList).toBeInTheDocument();
+   });
+
+   test("renders the projects list title", () => {
+      render(<ProjectsList />);
+      const title = screen.getByRole("heading", { name: /some of my work/i });
+      expect(title).toBeInTheDocument();
+   });
+
+   test("renders the projects list container", () => {
+      render(<ProjectsList />);
+      const list = screen.getByLabelText("Projects list container");
+      expect(list).toBeInTheDocument();
+   });
+
+   test("renders the projects list", () => {
+      render(<ProjectsList />);
+      const projectsList = screen.getByRole("list");
+      expect(projectsList).toBeInTheDocument();
+   });
+
+   test("renders the projects list items", () => {
+      render(<ProjectsList />);
+      const listItems = screen.getAllByRole("listitem");
+      expect(listItems).toHaveLength(5);
+   });
+});
