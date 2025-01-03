0112f81cd90907f3375b4e7dba2c35bc944b5dbe
Lpik97 <dm97fx@gmail.com>
2025-01-03 12:40:02 +0100

    feat: Create ProjectMainCoverLogos component for displaying logos

---
+++ src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-logos/ProjectMainCoverLogos.tsx
@@ -0,0 +1,14 @@
+const ProjectMainCoverLogos = () => {
+   return (
+      <div className="main-cover__logos" aria-label="Logos">
+         <div className="main-cover__company-logo">
+            <img src="" alt="Company logo" />
+         </div>
+         <div className="main-cover__own-logo">
+            <img src="" alt="Dylan Martinez's logo" />
+         </div>
+      </div>
+   );
+};
+
+export default ProjectMainCoverLogos;
