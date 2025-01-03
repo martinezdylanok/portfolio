4489666419c4b2e6be9f358fee828c722672a761
Lpik97 <dm97fx@gmail.com>
2025-01-03 12:22:43 +0100

    feat: Add ProjectMainCover component to serve as the main cover section for projects

---
+++ src/features/project/components/project-main-cover/ProjectMainCover.tsx
@@ -0,0 +1,11 @@
+import ProjectMainCoverContainer from "./components/project-main-cover-container/ProjectMainCoverContainer";
+
+const ProjectMainCover = () => {
+   return (
+      <section className="project__main-cover" aria-label="Main cover">
+         <ProjectMainCoverContainer />
+      </section>
+   );
+};
+
+export default ProjectMainCover;
