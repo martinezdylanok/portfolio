0a516fbd5d4a5a1135edb6615f9951286ac95aea
Lpik97 <dm97fx@gmail.com>
2025-01-03 09:56:04 +0100

    feat: Add ProjectsList component to display a list of projects

---
+++ src/features/projectsList/ProjectsList.tsx
@@ -0,0 +1,18 @@
+const ProjectsList = () => {
+   return (
+      <main>
+         <h1 className="projects-list__header">Some of my work</h1>
+         <div className="projects-list__container" aria-label="Projects list container">
+            <ul className="projects-list__list">
+               <li className="project-list__list-item">Project 1</li>
+               <li className="project-list__list-item">Project 2</li>
+               <li className="project-list__list-item">Project 3</li>
+               <li className="project-list__list-item">Project 4</li>
+               <li className="project-list__list-item">Project 5</li>
+            </ul>
+         </div>
+      </main>
+   );
+};
+
+export default ProjectsList;
