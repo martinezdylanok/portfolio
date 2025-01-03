ed84756be554decebd247fe6bbfb47de7c75006b
Lpik97 <dm97fx@gmail.com>
2025-01-03 12:31:33 +0100

    feat: Add ProjectMainCoverContent component for main cover details

---
+++ src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
@@ -0,0 +1,11 @@
+const ProjectMainCoverContent = () => {
+   return (
+      <div className="main-cover__content" aria-label="Main cover content">
+         <h3 className="main-cover__subtitle"></h3>
+         <h2 className="main-cover__title"></h2>
+         <p className="main-cover__project-description"></p>
+      </div>
+   );
+};
+
+export default ProjectMainCoverContent;
