87b4b09cc13d0c184bdec397dd2bf8718758e7f8
Lpik97 <dm97fx@gmail.com>
2025-01-05 09:33:34 +0100

    feat: Add ProjectOverviewContent component for project overview details

---
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
@@ -0,0 +1,11 @@
+const ProjectOverviewContent = () => {
+   return (
+      <div className="overview__content" aria-label="Project overview content">
+         <h2 className="overview__title">Project overview</h2>
+         <h3 className="overview__subtitle">Project details</h3>
+         <p className="overview__paragraph">Project paragraph</p>
+      </div>
+   );
+};
+
+export default ProjectOverviewContent;
