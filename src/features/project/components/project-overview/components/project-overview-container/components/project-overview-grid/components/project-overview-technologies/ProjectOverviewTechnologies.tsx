372ead4130ad7f5ce915d55dcd742487d63ef095
Lpik97 <dm97fx@gmail.com>
2025-01-05 09:40:29 +0100

    feat: Add ProjectOverviewTechnologies component for displaying project technologies

---
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
@@ -0,0 +1,12 @@
+// TODO: Create the logic for fetching the technologies of the project from the database.
+
+const ProjectOverviewTechnologies = () => {
+   return (
+      <div className="grid__technologies" aria-label="Project overview technologies">
+         <h3 className="grid__subtitle">Technologies</h3>
+         <ul className="technologies__list"></ul>
+      </div>
+   );
+};
+
+export default ProjectOverviewTechnologies;
