eb723ac18bbbb2ce9097677ca64de4af7fcbdcad
Lpik97 <dm97fx@gmail.com>
2025-01-05 09:05:46 +0100

    feat: Add ProjectOverview component for project overview section

---
+++ src/features/project/components/project-overview/ProjectOverview.tsx
@@ -0,0 +1,11 @@
+import ProjectOverviewContainer from "./components/project-overview-container/ProjectOverviewContainer";
+
+const ProjectOverview = () => {
+   return (
+      <section className="project__overview" aria-label="Project overview">
+         <ProjectOverviewContainer />
+      </section>
+   );
+};
+
+export default ProjectOverview;
