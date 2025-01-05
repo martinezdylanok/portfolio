88fd8a31ba6ed59dea12ed58e42c74fa37ad9ad9
Lpik97 <dm97fx@gmail.com>
2025-01-05 09:38:06 +0100

    feat: Add ProjectOverviewDocumentation component for project documentation display

---
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-documentation/ProjectOverviewDocumentation.tsx
@@ -0,0 +1,13 @@
+// TODO: Create the logic for fetching the documentation of the project from the database.
+
+const ProjectOverviewDocumentation = () => {
+   return (
+      <div className="grid__documentation" aria-label="Project overview documentation">
+         <a href="." aria-label="Documentation link">
+            <h3 className="grid__subtitle">Documentation</h3>
+         </a>
+      </div>
+   );
+};
+
+export default ProjectOverviewDocumentation;
