ed195fa47f6441df233bb1ad9826412eb418a83e
Lpik97 <dm97fx@gmail.com>
2025-01-05 09:39:16 +0100

    feat: Create ProjectOverviewDuration component for displaying project duration

---
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
@@ -0,0 +1,12 @@
+// TODO: Create the logic for fetching the duration of the project from the database.
+
+const ProjectOverviewDuration = () => {
+   return (
+      <div className="grid__duration" aria-label="Project overview duration">
+         <h3 className="grid__subtitle">Duration</h3>
+         <p className="duration__value"></p>
+      </div>
+   );
+};
+
+export default ProjectOverviewDuration;
