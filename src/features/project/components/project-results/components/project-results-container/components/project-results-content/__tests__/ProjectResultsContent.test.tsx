0d30adc868005c565c96f9e7e14e040b76f1a8c3
Lpik97 <dm97fx@gmail.com>
2025-01-07 08:36:45 +0100

    test: Add unit tests for ProjectResultsContent component rendering

---
+++ src/features/project/components/project-results/components/project-results-container/components/project-results-content/__tests__/ProjectResultsContent.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import ProjectResultsContent from "../ProjectResultsContent";
+
+describe("ProjectResultsContent tests", () => {
+   test("renders ProjectResultsContent element", () => {
+      render(<ProjectResultsContent />);
+      const ProjectResultsElement = screen.getByLabelText("Project results content");
+      expect(ProjectResultsElement).toBeInTheDocument();
+   });
+
+   test("renders Project results subtitle", () => {
+      render(<ProjectResultsContent />);
+      const ProjectResultsSubtitle = screen.getByRole("heading", { name: "Final results", level: 3 });
+      expect(ProjectResultsSubtitle).toBeInTheDocument();
+   });
+
+   test("renders Project results paragraph", () => {
+      render(<ProjectResultsContent />);
+      const ProjectResultsParagraph = screen.getByRole("paragraph");
+      expect(ProjectResultsParagraph).toBeInTheDocument();
+   });
+});
