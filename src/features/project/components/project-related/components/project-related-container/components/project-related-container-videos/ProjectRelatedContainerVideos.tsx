6491a071b62052ca27dbf1486deea0775d893016
Lpik97 <dm97fx@gmail.com>
2025-01-07 18:23:24 +0100

    feat: Add ProjectRelatedContainerVideos component for displaying related project videos

---
+++ src/features/project/components/project-related/components/project-related-container/components/project-related-container-videos/ProjectRelatedContainerVideos.tsx
@@ -0,0 +1,15 @@
+// TODO: Create the logic for fetching the videos of the project from the database.
+
+const ProjectRelatedContainerVideos = () => {
+   return (
+      <div className="related__container-videos" aria-label="Related projects container videos">
+         <ul className="related__container-videos-list">
+            <li className="related__container-videos-list-item"></li>
+            <li className="related__container-videos-list-item"></li>
+            <li className="related__container-videos-list-item"></li>
+         </ul>
+      </div>
+   );
+};
+
+export default ProjectRelatedContainerVideos;
