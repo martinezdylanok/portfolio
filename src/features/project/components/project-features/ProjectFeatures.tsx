8f37ae685ce38fb6c209712a1646a1f2ee570fcc
Lpik97 <dm97fx@gmail.com>
2025-01-07 09:19:39 +0100

    feat: Create ProjectFeatures component for displaying project features

---
+++ src/features/project/components/project-features/ProjectFeatures.tsx
@@ -0,0 +1,11 @@
+import ProjectFeaturesContainer from "./components/project-features-container/ProjectFeaturesContainer";
+
+const ProjectFeatures = () => {
+   return (
+      <section className="project__features" aria-label="Project features">
+         <ProjectFeaturesContainer />
+      </section>
+   );
+};
+
+export default ProjectFeatures;
