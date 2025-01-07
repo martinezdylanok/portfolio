787dac66c5565bc4e068d075854eb2d9cf969157
Lpik97 <dm97fx@gmail.com>
2025-01-07 08:34:21 +0100

    feat: Add ProjectResultsContainer component for encapsulating project results content

---
+++ src/features/project/components/project-results/components/project-results-container/ProjectResultsContainer.tsx
@@ -0,0 +1,11 @@
+import ProjectResultsContent from "./components/project-results-content/ProjectResultsContent";
+
+const ProjectResultsContainer = () => {
+   return (
+      <div className="results__container" aria-label="Results container">
+         <ProjectResultsContent />
+      </div>
+   );
+};
+
+export default ProjectResultsContainer;
