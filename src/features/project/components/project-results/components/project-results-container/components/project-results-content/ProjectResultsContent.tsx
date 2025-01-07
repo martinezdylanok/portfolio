c7a3d8a3121c52b4e6fb524841f4de61577da0d7
Lpik97 <dm97fx@gmail.com>
2025-01-07 08:36:14 +0100

    feat: Create ProjectResultsContent component for displaying project results details

---
+++ src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
@@ -0,0 +1,10 @@
+const ProjectResultsContent = () => {
+   return (
+      <div className="results__content" aria-label="Project results content">
+         <h3 className="results__subtitle">Final results</h3>
+         <p className="results__project-results"></p>
+      </div>
+   );
+};
+
+export default ProjectResultsContent;
