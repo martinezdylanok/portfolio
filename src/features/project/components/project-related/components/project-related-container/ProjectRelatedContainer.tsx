7e9bcdb33a81ffccdde154642b17583709f4419b
Lpik97 <dm97fx@gmail.com>
2025-01-07 18:19:24 +0100

    feat: Create ProjectRelatedContainer component for displaying related projects

---
+++ src/features/project/components/project-related/components/project-related-container/ProjectRelatedContainer.tsx
@@ -0,0 +1,9 @@
+const ProjectRelatedContainer = () => {
+   return (
+      <div className="related__container" aria-label="Related projects container">
+         <div className="a"></div>
+      </div>
+   );
+};
+
+export default ProjectRelatedContainer;
