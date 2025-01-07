ff3e531e9cdf8772083e5868eb8a4e9eb91fb57f
Lpik97 <dm97fx@gmail.com>
2025-01-07 09:21:56 +0100

    feat: Create ProjectFeaturesChallengesList component for displaying project challenges

---
+++ src/features/project/components/project-features/components/project-features-container/components/project-features-challenges-list/ProjectFeaturesChallengesList.tsx
@@ -0,0 +1,12 @@
+// TODO: Create the logic for fetching the challenges of the project from the database.
+
+const ProjectFeaturesChallengesList = () => {
+   return (
+      <div className="features__challenges-list" aria-label="Challenges list">
+         <h3 className="challenges-list__subtitle">Challenges</h3>
+         <p className="challenges-list__paragraph"></p>
+      </div>
+   );
+};
+
+export default ProjectFeaturesChallengesList;
