f99e82de8a0ef184b4e94b548831d0be798a3fc7
Lpik97 <dm97fx@gmail.com>
2025-01-07 09:23:10 +0100

    feat: Create ProjectFeaturesFeaturesList component for displaying project features

---
+++ src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
@@ -0,0 +1,12 @@
+// TODO: Create the logic for fetching the features of the project from the database.
+
+const ProjectFeaturesFeaturesList = () => {
+   return (
+      <div className="features__features-list" aria-label="Features list">
+         <h3 className="features-list__subtitle">Features list</h3>
+         <p className="features-list__paragraph"></p>
+      </div>
+   );
+};
+
+export default ProjectFeaturesFeaturesList;
