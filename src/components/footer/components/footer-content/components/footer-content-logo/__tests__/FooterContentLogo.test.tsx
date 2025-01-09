b009320783d6e58342111f812dd0ce9865cfd7d2
Lpik97 <dm97fx@gmail.com>
2025-01-09 09:10:16 +0100

    test: Add unit tests for FooterContentLogo component

---
+++ src/components/footer/components/footer-content/components/footer-content-logo/__tests__/FooterContentLogo.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import FooterContentLogo from "../FooterContentLogo";
+
+describe("FooterContentLogo tests", () => {
+   test("renders FooterContentLogo element", () => {
+      render(<FooterContentLogo />);
+      const FooterContentLogoElement = screen.getByLabelText("Footer logo container");
+      expect(FooterContentLogoElement).toBeInTheDocument();
+   });
+
+   test("renders FooterContentLogo image", () => {
+      render(<FooterContentLogo />);
+      const FooterContentLogoImage = screen.getByAltText("Dylan Martinez's logo");
+      expect(FooterContentLogoImage).toBeInTheDocument();
+   });
+});
