44a304e4ed615b0fa23d5b0a8f100c2cb5e608c4
Lpik97 <dm97fx@gmail.com>
2025-01-09 09:14:58 +0100

    feat: Add constants for scroll up text and arrow icon in FooterScrollUp component

---
+++ src/components/footer/components/footer-scroll-up/data/constants.ts
@@ -0,0 +1,5 @@
+const SCROLL_UP_TEXT = "Scroll up";
+const ARROW_ICON = "../assets/arrow-icon.svg";
+const ARROW_ICON_ALT_TEXT = "Arrow icon";
+
+export { ARROW_ICON, ARROW_ICON_ALT_TEXT, SCROLL_UP_TEXT };
