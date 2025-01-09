00f6afa6636e52ea1bed821a37b481d8120332fe
Lpik97 <dm97fx@gmail.com>
2025-01-09 09:09:49 +0100

    feat: Add FooterContentLogo component for displaying the footer logo

---
+++ src/components/footer/components/footer-content/components/footer-content-logo/FooterContentLogo.tsx
@@ -0,0 +1,9 @@
+const FooterContentLogo = () => {
+   return (
+      <div className="footer-content__logo" aria-label="Footer logo container">
+         <img src="" alt="Dylan Martinez's logo" />
+      </div>
+   );
+};
+
+export default FooterContentLogo;
