d03c28762e1ab4b3dc0e52fc6a11ba7853b97ea1
Lpik97 <dm97fx@gmail.com>
2025-01-09 09:10:45 +0100

    feat: Add FooterContentSocial component for social links in the footer

---
+++ src/components/footer/components/footer-content/components/footer-content-social/FooterContentSocial.tsx
@@ -0,0 +1,11 @@
+import FooterContentSocialGithub from "./components/footer-content-social-github/FooterContentSocialGithub";
+
+const FooterContentSocial = () => {
+   return (
+      <div className="footer-content__social" aria-label="Footer social container">
+         <FooterContentSocialGithub />
+      </div>
+   );
+};
+
+export default FooterContentSocial;
