8114cc39a6c46c6a84a70114b6fddc4ca95c8dad
Lpik97 <dm97fx@gmail.com>
2025-01-10 13:01:25 +0100

    refactor: Remove unused constants from header data

--- src/components/header/data/constants.tsx
+++
@@ -1,4 +0,0 @@
-export const LOGO_ALT: string = "Logo";
-export const PROJECTS_TEXT: string = "PROJECTS";
-export const CONTACT_TEXT: string = "CONTACT";
-export const MODE_SWITCHER_ALT: string = "Mode switcher";
