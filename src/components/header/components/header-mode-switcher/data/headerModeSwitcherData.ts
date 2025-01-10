cea0be1506de0ea76bad6d6e266def19da059dcc
Lpik97 <dm97fx@gmail.com>
2025-01-10 12:58:53 +0100

    feat: Add constants for button aria label and image alt text in HeaderModeSwitcherData

---
+++ src/components/header/components/header-mode-switcher/data/headerModeSwitcherData.ts
@@ -0,0 +1,4 @@
+const BUTTON_ARIA_LABEL_TEXT: string = "Theme switcher button";
+const IMAGE_ALT_TEXT: string = "Theme switcher button of the website";
+
+export { BUTTON_ARIA_LABEL_TEXT, IMAGE_ALT_TEXT };
