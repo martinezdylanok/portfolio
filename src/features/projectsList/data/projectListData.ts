22b3edb56a5716e30678e2963a195308cfec5c89
Lpik97 <dm97fx@gmail.com>
2025-01-11 11:36:29 +0100

    feat: Add project list data and ARIA label for accessibility

---
+++ src/features/projectsList/data/projectListData.ts
@@ -0,0 +1,11 @@
+const PROJECT_LIST_CONTAINER_ARIA_LABEL_TEXT: string = "Project list container";
+
+const PROJECT_LIST_TEMPORAL: { id: string; name: string }[] = [
+   { id: "project-1", name: "Project 1" },
+   { id: "project-2", name: "Project 2" },
+   { id: "project-3", name: "Project 3" },
+   { id: "project-4", name: "Project 4" },
+   { id: "project-5", name: "Project 5" },
+];
+
+export { PROJECT_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECT_LIST_TEMPORAL };
