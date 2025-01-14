d3ec8072ba1646bfc0f184248ad738e140c01f05
Lpik97 <dm97fx@gmail.com>
2025-01-14 11:50:21 +0100

    feat: Update ProjectOverviewDuration to use constants for aria-label and title text

--- src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
@@ -1,10 +1,12 @@
 // TODO: Create the logic for fetching the duration of the project from the database.
 
+import { PROJECT_OVERVIEW_DURATION_ARIA_LABEL, PROJECT_OVERVIEW_DURATION_TITLE_TEXT } from "./data/projectOverviewDurationData";
+
 const ProjectOverviewDuration = () => {
    return (
-      <div className="grid__duration" aria-label="Project overview duration">
-         <h3 className="grid__subtitle">Duration</h3>
-         <p className="duration__value"></p>
+      <div className="project__overview-grid-duration" aria-label={PROJECT_OVERVIEW_DURATION_ARIA_LABEL}>
+         <h3 className="project__overview-grid-duration-title">{PROJECT_OVERVIEW_DURATION_TITLE_TEXT}</h3>
+         <p className="project__overview-grid-duration-value"></p>
       </div>
    );
 };
