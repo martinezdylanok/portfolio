f26c6be06ee025b8f2607a300787ed4cf3687553
Lpik97 <dm97fx@gmail.com>
2025-01-14 10:28:36 +0100

    test: Update ProjectMainCover test to use correct ARIA label

--- src/features/project/components/project-main-cover/__tests__/ProjectMainCover.test.tsx
+++ src/features/project/components/project-main-cover/__tests__/ProjectMainCover.test.tsx
@@ -4,7 +4,7 @@ import ProjectMainCover from "../ProjectMainCover";
 describe("ProjectMainCover component tests", () => {
    test("render ProjectMainCover element", () => {
       render(<ProjectMainCover />);
-      const mainCover = screen.getByRole("region", { name: /main cover/i });
+      const mainCover = screen.getByRole("region", { name: /project main cover/i });
       expect(mainCover).toBeInTheDocument();
    });
 });
