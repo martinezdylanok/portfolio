47a80317160627beab02060b67047e183520ceba
Lpik97 <dm97fx@gmail.com>
2025-01-14 11:51:44 +0100

    feat: Update ProjectOverviewTechnologies to use constants for aria-label and title

--- src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
@@ -1,10 +1,12 @@
 // TODO: Create the logic for fetching the technologies of the project from the database.
 
+import { PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL, PROJECT_OVERVIEW_TECHNOLOGIES_TITLE } from "./data/projectOverviewTechnologiesData";
+
 const ProjectOverviewTechnologies = () => {
    return (
-      <div className="grid__technologies" aria-label="Project overview technologies">
-         <h3 className="grid__subtitle">Technologies</h3>
-         <ul className="technologies__list"></ul>
+      <div className="project__overview-grid-technologies" aria-label={PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL}>
+         <h3 className="project__overview-grid-technologies-title">{PROJECT_OVERVIEW_TECHNOLOGIES_TITLE}</h3>
+         <ul className="project__overview-grid-technologies-list"></ul>
       </div>
    );
 };
