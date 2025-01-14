61ac833301278081e068b360dde8e029c85aed45
Lpik97 <dm97fx@gmail.com>
2025-01-14 10:30:10 +0100

    feat: Update ProjectMainCoverContainer to use dynamic ARIA label for accessibility

--- src/features/project/components/project-main-cover/components/project-main-cover-container/ProjectMainCoverContainer.tsx
+++ src/features/project/components/project-main-cover/components/project-main-cover-container/ProjectMainCoverContainer.tsx
@@ -1,9 +1,10 @@
 import ProjectMainCoverContent from "./components/project-main-cover-content/ProjectMainCoverContent";
 import ProjectMainCoverLogos from "./components/project-main-cover-logos/ProjectMainCoverLogos";
+import { PROJECT_MAIN_COVER_CONTAINER_ARIA_LABEL } from "./data/projectMainCoverContainerData";
 
 const ProjectMainCoverContainer = () => {
    return (
-      <div className="main-cover__container" aria-label="Main cover container">
+      <div className="main-cover__container" aria-label={PROJECT_MAIN_COVER_CONTAINER_ARIA_LABEL}>
          <ProjectMainCoverContent />
          <ProjectMainCoverLogos />
       </div>
