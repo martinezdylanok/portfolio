77bead25e8cee7610cd631c44e7922b419a6442e
Lpik97 <dm97fx@gmail.com>
2025-01-14 10:26:22 +0100

    feat: Update ProjectOverview component to use dynamic ARIA label for accessibility

--- src/features/project/components/project-overview/ProjectOverview.tsx
+++ src/features/project/components/project-overview/ProjectOverview.tsx
@@ -1,8 +1,9 @@
 import ProjectOverviewContainer from "./components/project-overview-container/ProjectOverviewContainer";
+import PROJECT_OVERVIEW_ARIA_LABEL from "./data/projectOverviewData";
 
 const ProjectOverview = () => {
    return (
-      <section className="project__overview" aria-label="Project overview">
+      <section className="project__overview" aria-label={PROJECT_OVERVIEW_ARIA_LABEL}>
          <ProjectOverviewContainer />
       </section>
    );
