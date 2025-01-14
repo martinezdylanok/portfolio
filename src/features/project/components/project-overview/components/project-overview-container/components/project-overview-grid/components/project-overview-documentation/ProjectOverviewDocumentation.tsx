84ec90f762d4ad4b20e47f01ca229bce15c4fbdc
Lpik97 <dm97fx@gmail.com>
2025-01-14 11:48:37 +0100

    feat: Update ProjectOverviewDocumentation to use constants for aria-label and title

--- src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-documentation/ProjectOverviewDocumentation.tsx
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-documentation/ProjectOverviewDocumentation.tsx
@@ -1,10 +1,12 @@
 // TODO: Create the logic for fetching the documentation of the project from the database.
 
+import { PROJECT_OVERVIEW_GRID_DOCUMENTATION_ARIA_LABEL, PROJECT_OVERVIEW_GRID_DOCUMENTATION_LINK_ARIA_LABEL, PROJECT_OVERVIEW_GRID_DOCUMENTATION_TITLE } from "./data/projectOverviewDocumentationData";
+
 const ProjectOverviewDocumentation = () => {
    return (
-      <div className="grid__documentation" aria-label="Project overview documentation">
-         <a href="." aria-label="Documentation link">
-            <h3 className="grid__subtitle">Documentation</h3>
+      <div className="project__overview-grid-documentation" aria-label={PROJECT_OVERVIEW_GRID_DOCUMENTATION_ARIA_LABEL}>
+         <a href="." className="project__overview-grid-documentation-link" aria-label={PROJECT_OVERVIEW_GRID_DOCUMENTATION_LINK_ARIA_LABEL}>
+            <h2 className="project__overview-grid-documentation-title">{PROJECT_OVERVIEW_GRID_DOCUMENTATION_TITLE}</h2>
          </a>
       </div>
    );
