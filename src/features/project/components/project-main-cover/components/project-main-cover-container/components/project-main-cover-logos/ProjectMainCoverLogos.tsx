4780e852f1b5912596908bb80c71fee4a303be6b
Lpik97 <dm97fx@gmail.com>
2025-01-14 10:34:15 +0100

    feat: Update ProjectMainCoverLogos to use dynamic ARIA label and alt text for accessibility

--- src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-logos/ProjectMainCoverLogos.tsx
+++ src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-logos/ProjectMainCoverLogos.tsx
@@ -1,11 +1,15 @@
+// TODO: Create the logic for fetching the logos of the project from the database.
+
+import { PROJECT_MAIN_COVER_COMPANY_LOGO_ALT_TEXT, PROJECT_MAIN_COVER_LOGOS_ARIA_LABEL, PROJECT_MAIN_COVER_OWN_LOGO_ALT_TEXT } from "./data/projectMainCoverLogosData";
+
 const ProjectMainCoverLogos = () => {
    return (
-      <div className="main-cover__logos" aria-label="Logos">
+      <div className="main-cover__logos" aria-label={PROJECT_MAIN_COVER_LOGOS_ARIA_LABEL}>
          <div className="main-cover__company-logo">
-            <img src="" alt="Company logo" />
+            <img src="" alt={PROJECT_MAIN_COVER_COMPANY_LOGO_ALT_TEXT} />
          </div>
          <div className="main-cover__own-logo">
-            <img src="" alt="Dylan Martinez's logo" />
+            <img src="" alt={PROJECT_MAIN_COVER_OWN_LOGO_ALT_TEXT} />
          </div>
       </div>
    );
