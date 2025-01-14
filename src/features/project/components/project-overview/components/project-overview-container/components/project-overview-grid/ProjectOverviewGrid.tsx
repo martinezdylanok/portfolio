c4f89c649e9789f6e25d9531e5c1e19b1e8d013c
Lpik97 <dm97fx@gmail.com>
2025-01-14 11:48:05 +0100

    feat: Update ProjectOverviewGrid to use PROJECT_OVERVIEW_GRID_ARIA_LABEL for aria-label

--- src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/ProjectOverviewGrid.tsx
+++ src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/ProjectOverviewGrid.tsx
@@ -1,10 +1,11 @@
 import ProjectOverviewTechnologies from "./components/project-overview-technologies/ProjectOverviewTechnologies";
 import ProjectOverviewDuration from "./components/project-overview-duration/ProjectOverviewDuration";
 import ProjectOverviewDocumentation from "./components/project-overview-documentation/ProjectOverviewDocumentation";
+import { PROJECT_OVERVIEW_GRID_ARIA_LABEL } from "./data/projectOverviewGridData";
 
 const ProjectOverviewGrid = () => {
    return (
-      <div className="overview__grid" aria-label="Project overview grid">
+      <div className="project__overview-grid" aria-label={PROJECT_OVERVIEW_GRID_ARIA_LABEL}>
          <ProjectOverviewTechnologies />
          <ProjectOverviewDuration />
          <ProjectOverviewDocumentation />
