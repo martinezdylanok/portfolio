05dfbae82f82c3ba80011bd1de1bfe2683ae2c05
Lpik97 <dm97fx@gmail.com>
2025-01-14 10:31:13 +0100

    test: Update ProjectMainCoverContainer test to use correct label for accessibility

--- src/features/project/components/project-main-cover/components/project-main-cover-container/__tests__/ProjectMainCoverContainer.test.tsx
+++ src/features/project/components/project-main-cover/components/project-main-cover-container/__tests__/ProjectMainCoverContainer.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react";
 import ProjectMainCoverContainer from "../ProjectMainCoverContainer";
 
 describe("ProjectMainCoverContainer", () => {
-   test("render the main cover container", () => {
+   test("render ProjectMainCoverContainer element", () => {
       render(<ProjectMainCoverContainer />);
-      const mainCoverContainer = screen.getByLabelText("Main cover container");
+      const mainCoverContainer = screen.getByLabelText("Project main cover container");
       expect(mainCoverContainer).toBeInTheDocument();
    });
 });
