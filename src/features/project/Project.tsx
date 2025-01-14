67e3fc3f105711b8bfa540500306dc61cddd57fb
Lpik97 <dm97fx@gmail.com>
2025-01-14 10:24:18 +0100

    feat: Add ProjectFeatures and ProjectRelated components to Project container

--- src/features/project/Project.tsx
+++ src/features/project/Project.tsx
@@ -1,5 +1,7 @@
+import ProjectFeatures from "./components/project-features/ProjectFeatures";
 import ProjectMainCover from "./components/project-main-cover/ProjectMainCover";
 import ProjectOverview from "./components/project-overview/ProjectOverview";
+import ProjectRelated from "./components/project-related/ProjectRelated";
 import ProjectResults from "./components/project-results/ProjectResults";
 
 const Project = () => {
@@ -7,7 +9,9 @@ const Project = () => {
       <div className="project__container" aria-label="Project container">
          <ProjectMainCover />
          <ProjectOverview />
+         <ProjectFeatures />
          <ProjectResults />
+         <ProjectRelated />
       </div>
    );
 };
