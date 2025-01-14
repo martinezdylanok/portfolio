1a4379d624be7f02789d9243fb01ad79e75f6109
Lpik97 <dm97fx@gmail.com>
2025-01-14 11:38:18 +0100

    feat: Update ProjectOverviewContainer to use PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL for aria-label

--- src/features/project/components/project-overview/components/project-overview-container/ProjectOverviewContainer.tsx
+++ src/features/project/components/project-overview/components/project-overview-container/ProjectOverviewContainer.tsx
@@ -1,9 +1,10 @@
 import ProjectOverviewContent from "./components/project-overview-content/ProjectOverviewContent";
 import ProjectOverviewGrid from "./components/project-overview-grid/ProjectOverviewGrid";
+import { PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL } from "./data/projectOverviewContainerData";
 
 const ProjectOverviewContainer = () => {
    return (
-      <div className="overview__container" aria-label="Overview container">
+      <div className="project__overview-container" aria-label={PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL}>
          <ProjectOverviewContent />
          <ProjectOverviewGrid />
       </div>
