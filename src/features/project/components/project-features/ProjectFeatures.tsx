a40d244dbb5fe88a4e583c2f822b0d7a38d996ea
Lpik97 <dm97fx@gmail.com>
2025-01-15 09:25:30 +0100

    feat: Use constant for Project Features section aria-label

--- src/features/project/components/project-features/ProjectFeatures.tsx
+++ src/features/project/components/project-features/ProjectFeatures.tsx
@@ -1,8 +1,9 @@
 import ProjectFeaturesContainer from "./components/project-features-container/ProjectFeaturesContainer";
+import { PROJECT_FEATURES_ARIA_LABEL } from "./data/projectFeaturesData";
 
 const ProjectFeatures = () => {
    return (
-      <section className="project__features" aria-label="Project features">
+      <section className="project__features" aria-label={PROJECT_FEATURES_ARIA_LABEL}>
          <ProjectFeaturesContainer />
       </section>
    );
