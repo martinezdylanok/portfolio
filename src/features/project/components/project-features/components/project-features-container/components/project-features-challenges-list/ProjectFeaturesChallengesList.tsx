8c9f1f1c3ff122f99814062a0ecb95aea74a3601
Lpik97 <dm97fx@gmail.com>
2025-01-15 09:28:06 +0100

    feat: Refactor ProjectFeaturesChallengesList to use constants for aria-label and title

--- src/features/project/components/project-features/components/project-features-container/components/project-features-challenges-list/ProjectFeaturesChallengesList.tsx
+++ src/features/project/components/project-features/components/project-features-container/components/project-features-challenges-list/ProjectFeaturesChallengesList.tsx
@@ -1,10 +1,12 @@
 // TODO: Create the logic for fetching the challenges of the project from the database.
 
+import { PROJECT_FEATURES_CHALLENGES_ARIA_LABEL, PROJECT_FEATURES_CHALLENGES_TITLE } from "./data/projectFeaturesChallengesListData";
+
 const ProjectFeaturesChallengesList = () => {
    return (
-      <div className="features__challenges-list" aria-label="Challenges list">
-         <h3 className="challenges-list__subtitle">Challenges</h3>
-         <p className="challenges-list__paragraph"></p>
+      <div className="project__features-container-challenges-list" aria-label={PROJECT_FEATURES_CHALLENGES_ARIA_LABEL}>
+         <h2 className="project__features-container-challenges-title">{PROJECT_FEATURES_CHALLENGES_TITLE}</h2>
+         <p className="project__features-container-description-"></p>
       </div>
    );
 };
