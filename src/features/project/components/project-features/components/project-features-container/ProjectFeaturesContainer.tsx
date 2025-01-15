d38856c83cd2fef0a89f15355486e9b11e0a1a0d
Lpik97 <dm97fx@gmail.com>
2025-01-15 09:26:26 +0100

    feat: Use constant for Project Features Container aria-label

--- src/features/project/components/project-features/components/project-features-container/ProjectFeaturesContainer.tsx
+++ src/features/project/components/project-features/components/project-features-container/ProjectFeaturesContainer.tsx
@@ -1,9 +1,10 @@
 import ProjectFeaturesChallengesList from "./components/project-features-challenges-list/ProjectFeaturesChallengesList";
 import ProjectFeaturesFeaturesList from "./components/project-features-features-list/ProjectFeaturesFeaturesList";
+import { PROJECT_FEATURES_CONTAINER_ARIA_LABEL } from "./data/projectFeaturesContainerData";
 
 const ProjectFeaturesContainer = () => {
    return (
-      <div className="features__container" aria-label="Features container">
+      <div className="project__features-container" aria-label={PROJECT_FEATURES_CONTAINER_ARIA_LABEL}>
          <ProjectFeaturesFeaturesList />
          <ProjectFeaturesChallengesList />
       </div>
