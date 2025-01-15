69b5e63235539a439e5e3c54b1b08ec703990563
Lpik97 <dm97fx@gmail.com>
2025-01-15 19:06:51 +0100

    feat: Use constant for aria-label in ProjectRelatedContainer component

--- src/features/project/components/project-related/components/project-related-container/ProjectRelatedContainer.tsx
+++ src/features/project/components/project-related/components/project-related-container/ProjectRelatedContainer.tsx
@@ -1,9 +1,10 @@
 import ProjectRelatedContainerContent from "./components/project-related-container-content/ProjectRelatedContainerContent";
 import ProjectRelatedContainerVideos from "./components/project-related-container-videos/ProjectRelatedContainerVideos";
+import { PROJECT_RELATED_CONTAINER_ARIA_LABEL } from "./data/projectRelatedContainerData";
 
 const ProjectRelatedContainer = () => {
    return (
-      <div className="related__container" aria-label="Related projects container">
+      <div className="project__related-container" aria-label={PROJECT_RELATED_CONTAINER_ARIA_LABEL}>
          <ProjectRelatedContainerContent />
          <ProjectRelatedContainerVideos />
       </div>
