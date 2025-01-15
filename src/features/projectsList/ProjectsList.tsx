12296f6f118bd41c4e4a96d14e5dea0d0f967d18
Lpik97 <dm97fx@gmail.com>
2025-01-15 19:30:25 +0100

    feat: Use constants for title and aria-label in ProjectsList component

--- src/features/projectsList/ProjectsList.tsx
+++ src/features/projectsList/ProjectsList.tsx
@@ -1,14 +1,14 @@
 // TODO: Create the logic for fetching the project's overview video from the project component.
 
-import { PROJECT_LIST_TEMPORAL } from "./data/projectListData";
+import { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_TITLE, PROJECTS_LIST_TEMPORAL } from "./data/projectListData";
 
 const ProjectsList = () => {
    return (
       <main>
-         <h1 className="projects-list__header">Some of my work</h1>
-         <div className="projects-list__container" aria-label="Projects list container">
+         <h1 className="projects-list__header">{PROJECTS_LIST_TITLE}</h1>
+         <div className="projects-list__container" aria-label={PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT}>
             <ul className="projects-list__list">
-               {PROJECT_LIST_TEMPORAL.map((project) => (
+               {PROJECTS_LIST_TEMPORAL.map((project) => (
                   <li key={project.id} className="projects-list__list-item">
                      {project.name}
                   </li>
