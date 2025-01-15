46b524f0e84bc6bafaaf0b6fa4c8ab18c216b73b
Lpik97 <dm97fx@gmail.com>
2025-01-15 09:27:14 +0100

    test: Update test description for ProjectFeaturesContainer

--- src/features/project/components/project-features/components/project-features-container/__tests__/ProjectFeaturesContainer.test.tsx
+++ src/features/project/components/project-features/components/project-features-container/__tests__/ProjectFeaturesContainer.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import ProjectFeaturesContainer from "../ProjectFeaturesContainer";
 
 describe("ProjectFeaturesContainer", () => {
-   test("renders the features container", () => {
+   test("renders ProjectFeaturesContainer element", () => {
       render(<ProjectFeaturesContainer />);
       const featuresContainer = screen.getByLabelText("Features container");
       expect(featuresContainer).toBeInTheDocument();
