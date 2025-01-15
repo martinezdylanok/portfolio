45634099a45d7d142bfd45d19884d697421c34f9
Lpik97 <dm97fx@gmail.com>
2025-01-15 09:29:57 +0100

    feat: Update ProjectFeaturesFeaturesList to use constants for aria-label and title

--- src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
+++ src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
@@ -1,10 +1,12 @@
 // TODO: Create the logic for fetching the features of the project from the database.
 
+import { PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL, PROJECT_FEATURES_FEATURES_LIST_TITLE } from "./data/projectFeaturesFeaturesListData";
+
 const ProjectFeaturesFeaturesList = () => {
    return (
-      <div className="features__features-list" aria-label="Features list">
-         <h3 className="features-list__subtitle">Features list</h3>
-         <p className="features-list__paragraph"></p>
+      <div className="project__features-container-features-list" aria-label={PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL}>
+         <h2 className="project__features-container-features-title">{PROJECT_FEATURES_FEATURES_LIST_TITLE}</h2>
+         <p className="project__features-container-features-description"></p>
       </div>
    );
 };
