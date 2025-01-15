b54ea11f44de962c0509313f2a95debf6149fd84
Lpik97 <dm97fx@gmail.com>
2025-01-15 19:20:13 +0100

    feat: Use constant for aria-label in Project component

--- src/features/project/Project.tsx
+++ src/features/project/Project.tsx
@@ -3,10 +3,11 @@ import ProjectMainCover from "./components/project-main-cover/ProjectMainCover";
 import ProjectOverview from "./components/project-overview/ProjectOverview";
 import ProjectRelated from "./components/project-related/ProjectRelated";
 import ProjectResults from "./components/project-results/ProjectResults";
+import { PROJECT_ARIA_LABEL } from "./data/projectData";
 
 const Project = () => {
    return (
-      <div className="project__container" aria-label="Project container">
+      <div className="project__container" aria-label={PROJECT_ARIA_LABEL}>
          <ProjectMainCover />
          <ProjectOverview />
          <ProjectFeatures />
