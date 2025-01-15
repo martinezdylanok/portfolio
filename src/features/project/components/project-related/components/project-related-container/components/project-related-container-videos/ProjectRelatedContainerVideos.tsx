82524d1ca04b44e0fefcaa06c883fde8542640f6
Lpik97 <dm97fx@gmail.com>
2025-01-15 19:08:52 +0100

    feat: Use constant for aria-label in ProjectRelatedContainerVideos component

--- src/features/project/components/project-related/components/project-related-container/components/project-related-container-videos/ProjectRelatedContainerVideos.tsx
+++ src/features/project/components/project-related/components/project-related-container/components/project-related-container-videos/ProjectRelatedContainerVideos.tsx
@@ -1,12 +1,14 @@
 // TODO: Create the logic for fetching the videos of the project from the database.
 
+import { PROJECT_RELATED_CONTAINER_VIDEOS_ARIA_LABEL } from "./data/projectRelatedContainerVideosData";
+
 const ProjectRelatedContainerVideos = () => {
    return (
-      <div className="related__container-videos" aria-label="Related projects container videos">
-         <ul className="related__container-videos-list">
-            <li className="related__container-videos-list-item"></li>
-            <li className="related__container-videos-list-item"></li>
-            <li className="related__container-videos-list-item"></li>
+      <div className="project__related-videos" aria-label={PROJECT_RELATED_CONTAINER_VIDEOS_ARIA_LABEL}>
+         <ul className="project__related-videos-list">
+            <li className="project__related-videos-item"></li>
+            <li className="project__related-videos-item"></li>
+            <li className="project__related-videos-item"></li>
          </ul>
       </div>
    );
