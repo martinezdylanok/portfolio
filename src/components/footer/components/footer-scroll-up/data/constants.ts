4cd4eb0381f2a3d5cecf1e98c2e558d174b94852
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:05:46 +0100

    feat: Replace footer scroll up constants with updated naming and structure for improved clarity and accessibility

--- src/components/footer/components/footer-scroll-up/data/constants.ts
+++
@@ -1,5 +0,0 @@
-const SCROLL_UP_TEXT = "Scroll up";
-const ARROW_ICON = "../assets/arrow-icon.svg";
-const ARROW_ICON_ALT_TEXT = "Arrow icon";
-
-export { ARROW_ICON, ARROW_ICON_ALT_TEXT, SCROLL_UP_TEXT };
---
+++ src/components/footer/components/footer-scroll-up/data/footerScrollUpData.ts
@@ -0,0 +1,6 @@
+const FOOTER_SCROLL_UP_ARIA_LABEL: string = "Footer scroll up container";
+const FOOTER_SCROLL_UP_TEXT: string = "Scroll up";
+const FOOTER_SCROLL_UP_ARROW_ICON: string = "../assets/arrow-icon.svg";
+const FOOTER_SCROLL_UP_ARROW_ICON_ALT_TEXT: string = "Arrow icon";
+
+export { FOOTER_SCROLL_UP_ARROW_ICON, FOOTER_SCROLL_UP_ARROW_ICON_ALT_TEXT, FOOTER_SCROLL_UP_ARIA_LABEL, FOOTER_SCROLL_UP_TEXT };
