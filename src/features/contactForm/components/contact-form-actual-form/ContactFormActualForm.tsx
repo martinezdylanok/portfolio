a3a1ef58af844fbe43c9c724b0f3780e3375435b
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:24:10 +0100

    feat: Refactor ContactFormActualForm component to use constants for placeholders and ARIA label for improved maintainability and accessibility

--- src/features/contactForm/components/contact-form-actual-form/ContactFormActualForm.tsx
+++ src/features/contactForm/components/contact-form-actual-form/ContactFormActualForm.tsx
@@ -1,23 +1,25 @@
+import { CONTACT_FORM_ACTUAL_FORM_ARIA_LABEL, CONTACT_FORM_ACTUAL_FORM_EMAIL_PLACEHOLDER, CONTACT_FORM_ACTUAL_FORM_MESSAGE_PLACEHOLDER, CONTACT_FORM_ACTUAL_FORM_NAME_PLACEHOLDER, CONTACT_FORM_ACTUAL_FORM_PHONE_PLACEHOLDER, CONTACT_FORM_ACTUAL_FORM_SEND_BUTTON_TEXT } from "./data/contactFormActualFormData";
+
 const ContactFormActualForm = () => {
    return (
-      <form className="contact-form__actual-form" aria-label="Contact form">
-         <div className="actual-form__input-field">
+      <form className="contact-form__actual-form" aria-label={CONTACT_FORM_ACTUAL_FORM_ARIA_LABEL}>
+         <div className="contact-form__actual-form-input-field">
             <label htmlFor="name"></label>
-            <input type="text" placeholder="NAME" id="name" required />
+            <input type="text" placeholder={CONTACT_FORM_ACTUAL_FORM_NAME_PLACEHOLDER} id="name" required />
          </div>
-         <div className="actual-form__input-field">
+         <div className="contact-form__actual-form-input-field">
             <label htmlFor="phone"></label>
-            <input type="tel" placeholder="PHONE NUMBER" id="phone" required />
+            <input type="tel" placeholder={CONTACT_FORM_ACTUAL_FORM_PHONE_PLACEHOLDER} id="phone" required />
          </div>
-         <div className="actual-form__input-field">
+         <div className="contact-form__actual-form-input-field">
             <label htmlFor="email"></label>
-            <input type="email" placeholder="EMAIL" id="email" required />
+            <input type="email" placeholder={CONTACT_FORM_ACTUAL_FORM_EMAIL_PLACEHOLDER} id="email" required />
          </div>
-         <div className="actual-form__input-field">
+         <div className="contact-form__actual-form-input-field">
             <label htmlFor="message"></label>
-            <textarea placeholder="MESSAGE" id="message" />
+            <textarea placeholder={CONTACT_FORM_ACTUAL_FORM_MESSAGE_PLACEHOLDER} id="message" />
          </div>
-         <button type="submit">SEND</button>
+         <button type="submit">{CONTACT_FORM_ACTUAL_FORM_SEND_BUTTON_TEXT}</button>
       </form>
    );
 };
