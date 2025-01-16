23a1972f98d115fc2648291b41d488601f942450
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:25:08 +0100

    feat: Replace hardcoded ARIA labels and text in ContactFormSelector with constants for improved maintainability and accessibility

--- src/features/contactForm/components/contact-form-selector/ContactFormSelector.tsx
+++ src/features/contactForm/components/contact-form-selector/ContactFormSelector.tsx
@@ -1,13 +1,15 @@
 // TODO: Add :before for each h2 element to add a radio button
 
+import { CONTACT_FORM_SELECTOR_ARIA_LABEL, CONTACT_FORM_SELECTOR_FIRST_ARIA_LABEL, CONTACT_FORM_SELECTOR_SECOND_ARIA_LABEL, CONTACT_FORM_SELECTOR_FIRST_TEXT, CONTACT_FORM_SELECTOR_SECOND_TEXT } from "./data/contactFormSelectorData";
+
 const ContactFormSelector = () => {
    return (
-      <div className="contact-form__selector" aria-label="Contact form selector">
-         <div className="contact-form__selector-first" aria-label="Contact form first selector">
-            <h2>CONTACT</h2>
+      <div className="contact-form__selector" aria-label={CONTACT_FORM_SELECTOR_ARIA_LABEL}>
+         <div className="contact-form__selector-first" aria-label={CONTACT_FORM_SELECTOR_FIRST_ARIA_LABEL}>
+            <h2>{CONTACT_FORM_SELECTOR_FIRST_TEXT}</h2>
          </div>
-         <div className="contact-form__selector-second" aria-label="Contact form second selector">
-            <h2>JOB OFFER</h2>
+         <div className="contact-form__selector-second" aria-label={CONTACT_FORM_SELECTOR_SECOND_ARIA_LABEL}>
+            <h2>{CONTACT_FORM_SELECTOR_SECOND_TEXT}</h2>
          </div>
       </div>
    );
