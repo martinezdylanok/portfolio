1ac45a8ebc7f2e1337e005a6867a145d49f4a46e
Lpik97 <dm97fx@gmail.com>
2025-01-16 10:57:35 +0100

    feat: Update FooterCallToAction component to use new constants and improve accessibility

--- src/components/footer/components/footer-call-to-action/FooterCallToAction.tsx
+++ src/components/footer/components/footer-call-to-action/FooterCallToAction.tsx
@@ -1,10 +1,10 @@
-import { REPOSITORY_URL, CALL_TO_ACTION_TEXT } from "./data/constants";
+import { FOOTER_CALL_TO_ACTION_ARIA_LABEL, FOOTER_REPOSITORY_URL, FOOTER_CALL_TO_ACTION_TEXT } from "./data/footerCallToActionData";
 
 const FooterCallToAction = () => {
    return (
-      <div className="footer__call-to-action" aria-label="Footer call to action">
-         <a href={REPOSITORY_URL} className="call-to-action__link">
-            <span className="link__text">{CALL_TO_ACTION_TEXT}</span>
+      <div className="footer__call-to-action" aria-label={FOOTER_CALL_TO_ACTION_ARIA_LABEL}>
+         <a href={FOOTER_REPOSITORY_URL} className="footer__call-to-action-link">
+            <span className="footer__call-to-action-text">{FOOTER_CALL_TO_ACTION_TEXT}</span>
          </a>
       </div>
    );
