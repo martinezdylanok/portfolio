9e5ce7806e60bee6c91e47b57b4f11df72530dfb
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:01:28 +0100

    feat: Update FooterContentSocialGithub component to use dynamic aria-label for improved accessibility

--- src/components/footer/components/footer-content/components/footer-content-social/components/footer-content-social-github/FooterContentSocialGithub.tsx
+++ src/components/footer/components/footer-content/components/footer-content-social/components/footer-content-social-github/FooterContentSocialGithub.tsx
@@ -1,8 +1,8 @@
-import { GITHUB_LOGO_URL, GITHUB_LOGO_ALT, GITHUB_PROFILE_URL } from "./data/constants";
+import { FOOTER_CONTENT_SOCIAL_GITHUB_ARIA_LABEL, GITHUB_LOGO_URL, GITHUB_LOGO_ALT, GITHUB_PROFILE_URL } from "./data/footerContentSocialGithubData";
 
 const FooterContentSocialGithub = () => {
    return (
-      <div className="social__github-logo-container" aria-label="Github logo container">
+      <div className="social__github-logo-container" aria-label={FOOTER_CONTENT_SOCIAL_GITHUB_ARIA_LABEL}>
          <a href={GITHUB_PROFILE_URL} target="_blank">
             <img src={GITHUB_LOGO_URL} alt={GITHUB_LOGO_ALT} />
          </a>
