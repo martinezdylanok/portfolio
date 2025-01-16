3dee9bc760d43b9f884b64649debeb0158b9e289
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:00:23 +0100

    feat: Add constants for aria-label and alt text in footer content logo for improved accessibility

---
+++ src/components/footer/components/footer-content/components/footer-content-logo/data/footerContentLogoData.ts
@@ -0,0 +1,4 @@
+const FOOTER_CONTENT_LOGO__ARIA_LABEL: string = "Footer content logo container";
+const FOOTER_CONTENT_LOGO__IMAGE_ALT: string = "Dylan Martinez's logo";
+
+export { FOOTER_CONTENT_LOGO__ARIA_LABEL, FOOTER_CONTENT_LOGO__IMAGE_ALT };
