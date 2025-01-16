21d6d721148cf17ab44c32713c44f29789abb14b
Lpik97 <dm97fx@gmail.com>
2025-01-16 10:57:02 +0100

    test: Change 'it' to 'test' for consistency in Footer component tests

--- src/components/footer/__tests__/Footer.test.tsx
+++ src/components/footer/__tests__/Footer.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Footer from "../Footer";
 
 describe("Footer Component tests", () => {
-   it("render Footer element", () => {
+   test("render Footer element", () => {
       render(<Footer />);
       const footerElement = screen.getByRole("contentinfo");
       expect(footerElement).toBeInTheDocument();
