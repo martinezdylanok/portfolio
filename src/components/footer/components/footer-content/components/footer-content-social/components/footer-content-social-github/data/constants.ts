09f7d8604ae77a634ff3887381f46af635d3e400
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:02:09 +0100

    feat: Refactor GitHub footer content constants for improved organization and accessibility

--- src/components/footer/components/footer-content/components/footer-content-social/components/footer-content-social-github/data/constants.ts
+++
@@ -1,5 +0,0 @@
-const GITHUB_LOGO_URL = "../assets/github-logo.svg";
-const GITHUB_LOGO_ALT = "Github logo";
-const GITHUB_PROFILE_URL = "https://github.com/martinezdylanok";
-
-export { GITHUB_LOGO_URL, GITHUB_LOGO_ALT, GITHUB_PROFILE_URL };
