386ad1c0a2b6361e3fde69ff313172bde3734926
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:23:40 +0100

    feat: Replace hardcoded ARIA label in ContactForm component with constant for improved maintainability

--- src/features/contactForm/ContactForm.tsx
+++ src/features/contactForm/ContactForm.tsx
@@ -1,10 +1,11 @@
 import ContactFormTitle from "./components/contact-form-title/ContactFormTitle";
 import ContactFormSelector from "./components/contact-form-selector/ContactFormSelector";
 import ContactFormActualForm from "./components/contact-form-actual-form/ContactFormActualForm";
+import { CONTACT_FORM_ARIA_LABEL } from "./data/contactFormData";
 
 const ContactForm = () => {
    return (
-      <div className="project__contact-form" aria-label="Contact form container">
+      <div className="project__contact-form" aria-label={CONTACT_FORM_ARIA_LABEL}>
          <ContactFormTitle />
          <ContactFormSelector />
          <ContactFormActualForm />
