3c380aa33eb9ef6195b2f39ab0631bffe7a40458
Lpik97 <dm97fx@gmail.com>
2025-01-16 10:58:24 +0100

    feat: Replace old constants with new footer call to action data for improved clarity and accessibility

--- src/components/footer/components/footer-call-to-action/data/constants.ts
+++
@@ -1,4 +0,0 @@
-const REPOSITORY_URL = "https://github.com/martinezdylanok/portfolio";
-const CALL_TO_ACTION_TEXT = "Get this template";
-
-export { REPOSITORY_URL, CALL_TO_ACTION_TEXT };
---
+++ src/components/footer/components/footer-call-to-action/data/footerCallToActionData.ts
@@ -0,0 +1,5 @@
+const FOOTER_CALL_TO_ACTION_ARIA_LABEL = "Footer call to action";
+const FOOTER_REPOSITORY_URL = "https://github.com/martinezdylanok/portfolio";
+const FOOTER_CALL_TO_ACTION_TEXT = "Get this template";
+
+export { FOOTER_CALL_TO_ACTION_ARIA_LABEL, FOOTER_REPOSITORY_URL, FOOTER_CALL_TO_ACTION_TEXT };
