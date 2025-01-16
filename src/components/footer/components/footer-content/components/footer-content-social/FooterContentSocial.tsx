02434abcd582f65f3e0a235d4f8b27ed9ed27f21
Lpik97 <dm97fx@gmail.com>
2025-01-16 11:00:49 +0100

    feat: Update FooterContentSocial component to use dynamic aria-label for improved accessibility

--- src/components/footer/components/footer-content/components/footer-content-social/FooterContentSocial.tsx
+++ src/components/footer/components/footer-content/components/footer-content-social/FooterContentSocial.tsx
@@ -1,8 +1,9 @@
 import FooterContentSocialGithub from "./components/footer-content-social-github/FooterContentSocialGithub";
+import { FOOTER_CONTENT_SOCIAL__ARIA_LABEL } from "./data/footerContentSocialData";
 
 const FooterContentSocial = () => {
    return (
-      <div className="footer-content__social" aria-label="Footer social container">
+      <div className="footer-content__social" aria-label={FOOTER_CONTENT_SOCIAL__ARIA_LABEL}>
          <FooterContentSocialGithub />
       </div>
    );
