139fe648117e85e26cb1c94f992c03ecbab5e96c
martinezdylanok <dm97fx@gmail.com>
2025-02-04 13:24:07 +0100

    refactor: replace express app initialization with server module imported

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,17 +1,10 @@
-import express, {Request, Response} from "express";
+import server from "./server.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-const app = express();
-
-app.use(express.json());
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 
-app.get("/", (_req: Request, res: Response) => {
-   res.send("Hello World");
-});
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
 });
