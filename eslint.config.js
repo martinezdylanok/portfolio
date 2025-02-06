17c539ea1348fb9d9c4170a2a98a755a6812777b
martinezdylanok <dm97fx@gmail.com>
2025-02-06 13:24:05 +0100

    refactor: update ESLint configuration to improve TypeScript and Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
-export default tseslint
-   .config(
-      { ignores: ["dist"] },
-      {
-         extends: [js.configs.recommended, ...tseslint.configs.recommended],
-         files: ["**/*.{ts,tsx,js,jsx}"],
-         languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-         },
-         plugins: {
-            "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
-         },
-         rules: {
-            ...reactHooks.configs.recommended.rules,
-            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-         },
+export default [
+   {
+      ignores: ["packages/client/", "packages/server/", "dist"],
+   },
+   {
+      files: ["**/*.{ts,tsx,js,jsx}"],
+      languageOptions: {
+         ecmaVersion: 2020,
+         globals: globals.browser,
       },
-   )
-   .concat(eslintPluginPrettier);
+      plugins: {
+         "@typescript-eslint": tseslint,
+         prettier: eslintPluginPrettier,
+      },
+      rules: {
+         ...js.configs.recommended.rules,
+         ...tseslint.configs.recommended.rules,
+         "prettier/prettier": "error",
+         "no-var": "error",
+      },
+   },
+];
