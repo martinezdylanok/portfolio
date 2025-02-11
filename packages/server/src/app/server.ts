8a37ca837bb44e951e49b3d154b6ec8723afd18e
martinezdylanok <dm97fx@gmail.com>
2025-02-11 11:17:09 +0100

    feat: update server static file serving to use dynamic path resolution

--- packages/server/src/app/server.ts
+++ packages/server/src/app/server.ts
@@ -1,18 +1,28 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import projectRoutes from "../routes/projectRoutes/projectRoutes.js";
 
 const server = express();
 
+/* Middlewares */
 server.use(cors());
 server.use(express.json());
 
-/* Routers */
-server.use(express.static(path.join(__dirname, "../client/dist")));
+/* Static files */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const clientDistPath = path.join(__dirname, "../../../client/dist");
+
+server.use(express.static(clientDistPath));
+
+/* Routes */
 server.get("*", (__, res) => {
-   res.sendFile(path.join(__dirname, ".../client/dist", "index.html"));
+   res.sendFile(path.join(clientDistPath, "index.html"));
 });
+
 server.use("/projects", projectRoutes);
 
 export default server;
