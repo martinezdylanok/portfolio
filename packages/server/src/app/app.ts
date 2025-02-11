170dc08270759aab031aec19aa4bb37b52fc770b
martinezdylanok <dm97fx@gmail.com>
2025-02-11 10:42:29 +0100

    feat: set up server connection with environment configuration

---
+++ packages/server/src/app/app.ts
@@ -0,0 +1,10 @@
+import server from "./server.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT: number = parseInt(process.env.SERVER_PORT as string, 10) || 3000;
+
+server.listen(PORT, () => {
+   console.log(`Server is running on http://localhost:${PORT}`);
+});
