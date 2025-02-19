b721563451fe78206bc2119ea0ae5d720c475816
martinezdylanok <dm97fx@gmail.com>
2025-02-19 10:39:48 +0100

    feat: add initial  and second seeds data for projects table

---
+++ packages/server/src/database/seeds/002_second_seed.sql
@@ -0,0 +1,23 @@
+INSERT INTO projects (
+    project_name,
+    project_logo_1,
+    project_logo_2,
+    project_description,
+    project_overview_description,
+    project_overview_technologies,
+    project_overview_duration,
+    project_features,
+    project_challenges,
+    project_final_results
+) VALUES (
+    'project 01',
+    NULL,
+    NULL,
+    'This is a description of the sample project.',
+    'Overview description of the sample project.',
+    'Technologies used in the sample project.',
+    'Duration of the sample project.',
+    'Features of the sample project.',
+    'Challenges faced during the sample project.',
+    'Final results of the sample project.'
+);
