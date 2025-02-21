24cee599c906a5032442f472de246a726bd5c67a
martinezdylanok <dm97fx@gmail.com>
2025-02-21 10:26:39 +0100

    test: improve server test descriptions for clarity and consistency

--- packages/server/src/app/__tests__/server.test.ts
+++ packages/server/src/app/__tests__/server.test.ts
@@ -2,28 +2,27 @@ import request from "supertest";
 import server from "../server.js";
 
 describe("Express server tests", () => {
-   /*First two will not work until we set a index route */
-   test("GET / responds with 200 status code", async () => {
+   test("should responds with 200 status code when using GET /", async () => {
       const response = await request(server).get("/");
       expect(response.status).toBe(200);
    });
 
-   test("GET / responds with 'Welcome to the API'", async () => {
+   test("should return the HTML body when using GET /'", async () => {
       const response = await request(server).get("/");
-      expect(response.text).toContain("<!DOCTYPE html>");
+      expect(response.text).toContain("<!doctype html>");
    });
 
-   test("CORS headers are set", async () => {
+   test("should set CORS headers", async () => {
       const response = await request(server).get("/");
       expect(response.headers["access-control-allow-origin"]).toBe("*");
    });
 
-   test("JSON parsing middleware works", async () => {
+   test("should not be 404 when sending JSON parsing middlework", async () => {
       const response = await request(server).post("/").send({ name: "Test Project" }).set("Content-Type", "application/json");
       expect(response.status).not.toBe(400);
    });
 
-   test("Unknown route responds with 404", async () => {
+   test("should respond with 404 when trying to GET into an unknown route", async () => {
       const response = await request(server).get("/unknown-route");
       expect(response.status).toBe(404);
    });
