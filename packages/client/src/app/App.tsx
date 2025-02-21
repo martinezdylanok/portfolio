7bb733c028d2ef4c83e741ab5ee7af9462ca8ab4
martinezdylanok <dm97fx@gmail.com>
2025-02-21 11:12:27 +0100

    feat: implement main application layout with header, footer, projects list, and contact form

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,5 +1,17 @@
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import ContactForm from "../features/contactForm/ContactForm";
+import ProjectsList from "../features/projectsList/ProjectsList";
+
 function App() {
-   return <></>;
+   return (
+      <div>
+         <Header />
+         <ProjectsList />
+         <ContactForm />
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
