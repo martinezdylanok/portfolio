f7a5270aab4680171f520f383f92ff01813a752d
martinezdylanok <dm97fx@gmail.com>
2025-02-22 09:57:24 +0100

    feat: update header logo to use DM logo SVG

--- packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
+++ packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
@@ -4,7 +4,7 @@ const HeaderLogo = () => {
    return (
       <div className="header__logo" aria-label={LOGO_CONTAINER_ARIA_LABEL}>
          <a href="/" className="header__logo-link">
-            <img src="" className="header__logo-image" alt={LOGO_IMAGE_ALT_TEXT} />
+            <img src="../../../../../public/images/header/dm_logo.svg" className="header__logo-image" alt={LOGO_IMAGE_ALT_TEXT} />
          </a>
       </div>
    );
