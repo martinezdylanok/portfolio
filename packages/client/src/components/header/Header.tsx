2c333e8f2b5fa2cebc49664582d1dd629f2db7dc
martinezdylanok <dm97fx@gmail.com>
2025-03-05 10:17:15 +0100

    feat: integrate theme context into Header component for dynamic styling

--- packages/client/src/components/header/Header.tsx
+++ packages/client/src/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import HeaderLogo from "./components/header-logo/HeaderLogo";
 import HeaderNavigation from "./components/header-navigation/HeaderNavigation";
 import HeaderModeSwitcher from "./components/header-mode-switcher/HeaderModeSwitcher";
+import { useThemeContext } from "../../utils/theme-provider/themeProvider";
 
 const Header = () => {
+   const { mode } = useThemeContext();
    return (
-      <header>
+      <header className={`header flex items-center justify-between p-5 ${mode === "light" ? "bg-[#EDF2FB]" : "bg-[#ABC4FF]"}`}>
          <HeaderLogo />
          <HeaderNavigation />
          <HeaderModeSwitcher />
