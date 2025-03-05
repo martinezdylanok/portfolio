7fe52c8d89cc294663782bc7698d702c3a69ab24
martinezdylanok <dm97fx@gmail.com>
2025-03-05 10:19:16 +0100

    feat: implement mode switcher with animation and theme context integration

--- packages/client/src/components/header/components/header-mode-switcher/HeaderModeSwitcher.tsx
+++ packages/client/src/components/header/components/header-mode-switcher/HeaderModeSwitcher.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import { BUTTON_ARIA_LABEL_TEXT, IMAGE_ALT_TEXT } from "./data/headerModeSwitcherData";
+import "./styles/toggle-mode-animation.css";
+import { useThemeContext } from "../../../../utils/theme-provider/themeProvider";
 
 const HeaderModeSwitcher = () => {
+   const { mode, toggleMode } = useThemeContext();
+
+   const [animate, setAnimate] = useState(false);
+
+   const handleClick = () => {
+      toggleMode();
+      setAnimate(true);
+      setTimeout(() => {
+         setAnimate(false);
+      }, 1500);
+   };
+
    return (
-      <button className="header__mode-switcher-button" aria-label={BUTTON_ARIA_LABEL_TEXT}>
-         <img src="" className="header__mode-switcher-image" alt={IMAGE_ALT_TEXT} />
+      <button className={`p-1.5 header__mode-switcher-button`} aria-label={BUTTON_ARIA_LABEL_TEXT} onClick={handleClick}>
+         {mode === "light" ? <img src="/header/light_mode_icon.svg" className={`cursor-pointer header__mode-switcher-image ${animate ? "toggle-mode-animation" : ""}`} alt={IMAGE_ALT_TEXT} /> : <img src="/header/dark_mode_icon.svg" className={`cursor-pointer header__mode-switcher-image ${animate ? "toggle-mode-animation" : ""}`} alt={IMAGE_ALT_TEXT} />}
+         <img src="/header/retro_mode_icon.svg" className="hidden cursor-pointer header__mode-switcher-image" alt={IMAGE_ALT_TEXT} />
       </button>
    );
 };
