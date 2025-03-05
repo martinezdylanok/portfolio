e92b1ff5ef4452eec8fa63d4d7795b0515f919ba
martinezdylanok <dm97fx@gmail.com>
2025-03-05 10:17:55 +0100

    feat: enhance HeaderLogo component with theme-based logo rendering and hover effects

--- packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
+++ packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
@@ -1,10 +1,24 @@
 import { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT } from "./data/headerLogoConstants";
+import { useState } from "react";
+import "./styles/logo-animations.css";
+import { useThemeContext } from "../../../../utils/theme-provider/themeProvider";
+
+// NOTE: Set a width/height for the header__logo-wrapper element that is equal to the width/height of the image.
 
 const HeaderLogo = () => {
+   const { mode } = useThemeContext();
+   const [isHovered, setIsHovered] = useState(false);
+
+   const handleMouseEnter = () => setIsHovered(true);
+   const handleMouseLeave = () => setIsHovered(false);
+
    return (
-      <div className="header__logo" aria-label={LOGO_CONTAINER_ARIA_LABEL}>
-         <a href="/" className="header__logo-link">
-            <img src="../../../../../public/images/header/dm_logo.svg" className="header__logo-image" alt={LOGO_IMAGE_ALT_TEXT} />
+      <div className="header__logo" aria-label={LOGO_CONTAINER_ARIA_LABEL} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+         <a href="/" className="text-center header__logo-link">
+            <div className="relative w-[5.945rem] h-[2.813rem] header__logo-wrapper">
+               {mode === "light" ? <img src="/header/dm_logo_light_mode.svg" className={`absolute top-0 left-0 header__logo-image ${isHovered ? "lifted" : ""}`} alt={LOGO_IMAGE_ALT_TEXT} /> : <img src="/header/dm_logo_dark_mode.svg" className={`absolute top-0 left-0 header__logo-image ${isHovered ? "lifted" : ""}`} alt={LOGO_IMAGE_ALT_TEXT} />}
+               <span className={`absolute left-0 text-[0.74rem] ${mode === "light" ? "text-[#ABC4FF]" : "text-[#EDF2FB]"} min-w-max header__logo-title ${isHovered ? "unlifted" : ""}`}>DYLAN MARTINEZ</span>
+            </div>
          </a>
       </div>
    );
