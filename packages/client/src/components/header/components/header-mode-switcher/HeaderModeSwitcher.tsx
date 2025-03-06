2c1ba94f843a9924d472940d14c50c02cdd14874
martinezdylanok <dm97fx@gmail.com>
2025-03-06 18:07:56 +0100

    feat: update HeaderModeSwitcher to include alt text for light, dark, and retro modes

--- packages/client/src/components/header/components/header-mode-switcher/HeaderModeSwitcher.tsx
+++ packages/client/src/components/header/components/header-mode-switcher/HeaderModeSwitcher.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { BUTTON_ARIA_LABEL_TEXT, IMAGE_ALT_TEXT } from "./data/headerModeSwitcherData";
+import { BUTTON_ARIA_LABEL_TEXT, LIGHT_MODE_ALT_TEXT, DARK_MODE_ALT_TEXT, RETRO_MODE_ALT_TEXT } from "./data/headerModeSwitcherData";
 import "./styles/toggle-mode-animation.css";
-import { useThemeContext } from "../../../../utils/theme-provider/themeProvider";
+import { useThemeContext } from "../../../../utils/hooks/useTheme";
 
 const HeaderModeSwitcher = () => {
    const { mode, toggleMode } = useThemeContext();
@@ -18,8 +18,8 @@ const HeaderModeSwitcher = () => {
 
    return (
       <button className={`p-1.5 header__mode-switcher-button`} aria-label={BUTTON_ARIA_LABEL_TEXT} onClick={handleClick}>
-         {mode === "light" ? <img src="/header/light_mode_icon.svg" className={`cursor-pointer header__mode-switcher-image ${animate ? "toggle-mode-animation" : ""}`} alt={IMAGE_ALT_TEXT} /> : <img src="/header/dark_mode_icon.svg" className={`cursor-pointer header__mode-switcher-image ${animate ? "toggle-mode-animation" : ""}`} alt={IMAGE_ALT_TEXT} />}
-         <img src="/header/retro_mode_icon.svg" className="hidden cursor-pointer header__mode-switcher-image" alt={IMAGE_ALT_TEXT} />
+         {mode === "light" ? <img src="/header/light_mode_icon.svg" className={`cursor-pointer header__mode-switcher-image ${animate ? "toggle-mode-animation" : ""}`} alt={LIGHT_MODE_ALT_TEXT} /> : <img src="/header/dark_mode_icon.svg" className={`cursor-pointer header__mode-switcher-image ${animate ? "toggle-mode-animation" : ""}`} alt={DARK_MODE_ALT_TEXT} />}
+         <img src="/header/retro_mode_icon.svg" className="hidden cursor-pointer header__mode-switcher-image" alt={RETRO_MODE_ALT_TEXT} />
       </button>
    );
 };
