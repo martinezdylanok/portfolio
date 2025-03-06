ce6528a8e259168ed8b349717807ced5b7ed6acb
martinezdylanok <dm97fx@gmail.com>
2025-03-06 18:11:48 +0100

    feat: add alt text for light, dark, and retro mode icons in header mode switcher

--- packages/client/src/components/header/components/header-mode-switcher/data/headerModeSwitcherData.ts
+++ packages/client/src/components/header/components/header-mode-switcher/data/headerModeSwitcherData.ts
@@ -1,4 +1,6 @@
 const BUTTON_ARIA_LABEL_TEXT: string = "Theme switcher button";
-const IMAGE_ALT_TEXT: string = "Theme switcher button of the website";
+const LIGHT_MODE_ALT_TEXT: string = "Light mode icon";
+const DARK_MODE_ALT_TEXT: string = "Dark mode icon";
+const RETRO_MODE_ALT_TEXT: string = "Retro mode icon";
 
-export { BUTTON_ARIA_LABEL_TEXT, IMAGE_ALT_TEXT };
+export { BUTTON_ARIA_LABEL_TEXT, LIGHT_MODE_ALT_TEXT, DARK_MODE_ALT_TEXT, RETRO_MODE_ALT_TEXT };
