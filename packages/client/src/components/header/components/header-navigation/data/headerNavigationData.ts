e1b97cf299062d656b6fa802be8eb6955e933805
martinezdylanok <dm97fx@gmail.com>
2025-03-07 08:44:29 +0100

    feat: add ARIA labels for projects menu and search input in header navigation

--- packages/client/src/components/header/components/header-navigation/data/headerNavigationData.ts
+++ packages/client/src/components/header/components/header-navigation/data/headerNavigationData.ts
@@ -1,9 +1,13 @@
 const NAV_ARIA_LABEL_TEXT: string = "Main navigation";
+const BUTTON_ARIA_LABEL_TEXT: string = "Toggle projects menu";
+const INPUT_ARIA_LABEL_TEXT: string = "Search projects";
 
 interface Project {
    project_id: number;
    project_name: string;
 }
 
-export { NAV_ARIA_LABEL_TEXT };
+const SPAN_TEXT: string = "~/documents/Projects $";
+
+export { NAV_ARIA_LABEL_TEXT, BUTTON_ARIA_LABEL_TEXT, INPUT_ARIA_LABEL_TEXT, SPAN_TEXT };
 export type { Project };
