8fe35abb04a953b58fde084a654a6a2a61fb077f
martinezdylanok <dm97fx@gmail.com>
2025-03-07 09:24:50 +0100

    feat: update HeaderLogo component to use dynamic logo title from constants

--- packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
+++ packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
@@ -1,7 +1,7 @@
-import { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT } from "./data/headerLogoConstants";
+import { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT, LOGO_TITLE_TEXT } from "./data/headerLogoConstants";
 import { useState } from "react";
 import "./styles/logo-animations.css";
-import { useThemeContext } from "../../../../utils/theme-provider/themeProvider";
+import { useThemeContext } from "../../../../utils/hooks/useTheme";
 
 // NOTE: Set a width/height for the header__logo-wrapper element that is equal to the width/height of the image.
 
@@ -17,7 +17,7 @@ const HeaderLogo = () => {
          <a href="/" className="text-center header__logo-link">
             <div className="relative w-[5.945rem] h-[2.813rem] header__logo-wrapper">
                {mode === "light" ? <img src="/header/dm_logo_light_mode.svg" className={`absolute top-0 left-0 header__logo-image ${isHovered ? "lifted" : ""}`} alt={LOGO_IMAGE_ALT_TEXT} /> : <img src="/header/dm_logo_dark_mode.svg" className={`absolute top-0 left-0 header__logo-image ${isHovered ? "lifted" : ""}`} alt={LOGO_IMAGE_ALT_TEXT} />}
-               <span className={`absolute left-0 text-[0.74rem] ${mode === "light" ? "text-[#ABC4FF]" : "text-[#EDF2FB]"} min-w-max header__logo-title ${isHovered ? "unlifted" : ""}`}>DYLAN MARTINEZ</span>
+               <span className={`absolute left-0 text-[0.74rem] ${mode === "light" ? "text-[#ABC4FF]" : "text-[#EDF2FB]"} min-w-max header__logo-title ${isHovered ? "unlifted" : ""}`}>{LOGO_TITLE_TEXT}</span>
             </div>
          </a>
       </div>
