9eb6c76f87cd28720d58e09d8fd3a6587f61d776
martinezdylanok <dm97fx@gmail.com>
2025-03-07 08:45:07 +0100

    test: enhance HeaderNavigation tests with additional aria-label checks and visibility logic

--- packages/client/src/components/header/components/header-navigation/__tests__/HeaderNavigation.test.tsx
+++ packages/client/src/components/header/components/header-navigation/__tests__/HeaderNavigation.test.tsx
@@ -1,25 +1,72 @@
 import { render, screen } from "@testing-library/react";
 import HeaderNavigation from "../HeaderNavigation";
-import { NAV_ARIA_LABEL_TEXT, PROJECT_TEMPORAL_NAMES } from "../data/headerNavigationData";
+import { NAV_ARIA_LABEL_TEXT, BUTTON_ARIA_LABEL_TEXT, INPUT_ARIA_LABEL_TEXT } from "../data/headerNavigationData";
+import { setupLightMode, resetModes, mockProjects } from "./test-utils/test-utils";
 
 describe("HeaderNavigation Component tests", () => {
-   test("renders the navigation with the correct aria-label", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme");
+      mockProjects();
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
+   test("should render the navigation element with the correct aria-label", () => {
       render(<HeaderNavigation />);
       const navElement = screen.getByLabelText(NAV_ARIA_LABEL_TEXT);
       expect(navElement).toBeInTheDocument();
    });
 
-   test("renders the correct number of project links elements", () => {
+   test("should render the button element with the correct role", () => {
       render(<HeaderNavigation />);
-      const projectLinks = screen.getAllByRole("link");
-      expect(projectLinks).toHaveLength(PROJECT_TEMPORAL_NAMES.length);
+      const buttonElement = screen.getByLabelText(BUTTON_ARIA_LABEL_TEXT);
+      expect(buttonElement).toBeInTheDocument();
    });
 
-   test("renders project links elements with correct names", () => {
+   test("should render the input element with the correct text", () => {
       render(<HeaderNavigation />);
-      PROJECT_TEMPORAL_NAMES.forEach((project) => {
-         const linkElement = screen.getByText(project.name);
-         expect(linkElement).toBeInTheDocument();
-      });
+      const inputElement = screen.getByLabelText(INPUT_ARIA_LABEL_TEXT);
+      expect(inputElement).toBeInTheDocument();
+   });
+
+   test("should render the ul element with the correct role", () => {
+      render(<HeaderNavigation />);
+      const ulElement = screen.getByRole("list");
+      expect(ulElement).toBeInTheDocument();
+   });
+
+   test("should check if the ul element is hidden by default", () => {
+      render(<HeaderNavigation />);
+      const ulElement = screen.getByRole("list");
+      expect(ulElement).toHaveClass("hidden");
+   });
+
+   test("should turn the ul element visible when the button is clicked", async () => {
+      render(<HeaderNavigation />);
+      const buttonElement = screen.getByLabelText(BUTTON_ARIA_LABEL_TEXT);
+      const ulElement = screen.getByRole("list");
+      expect(ulElement).toHaveClass("hidden");
+      await buttonElement.click();
+      expect(ulElement).toHaveClass("visible");
+   });
+
+   test("should render the list elements with the correct role", async () => {
+      render(<HeaderNavigation />);
+      const listItems = await screen.findAllByRole("listitem");
+      expect(listItems).toHaveLength(3);
+   });
+
+   test("should render the correct list item text", async () => {
+      render(<HeaderNavigation />);
+      const listItems = await screen.findAllByRole("listitem");
+      expect(listItems[0]).toHaveTextContent("Project 1");
+      expect(listItems[1]).toHaveTextContent("Project 2");
+      expect(listItems[2]).toHaveTextContent("Project 3");
    });
 });
