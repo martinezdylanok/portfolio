9d11cc6dd81ee10099d15ec5d1fb589e00b81925
martinezdylanok <dm97fx@gmail.com>
2025-03-07 09:27:04 +0100

    feat: add logo title constant for dynamic logo rendering

--- packages/client/src/components/header/components/header-logo/data/headerLogoConstants.ts
+++ packages/client/src/components/header/components/header-logo/data/headerLogoConstants.ts
@@ -1,4 +1,6 @@
 const LOGO_CONTAINER_ARIA_LABEL: string = "Logo container";
 const LOGO_IMAGE_ALT_TEXT: string = "Logo of the website";
 
-export { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT };
+const LOGO_TITLE_TEXT: string = "DYLAN MARTINEZ";
+
+export { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT, LOGO_TITLE_TEXT };
