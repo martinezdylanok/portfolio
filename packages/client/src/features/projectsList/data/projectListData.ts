d4ea7391378451877c99764c5fb781920b66f3d9
martinezdylanok <dm97fx@gmail.com>
2025-03-12 10:49:29 +0100

    feat: update project list data structure and add new constants for project assets

--- packages/client/src/features/projectsList/data/projectListData.ts
+++ packages/client/src/features/projectsList/data/projectListData.ts
@@ -1,12 +1,15 @@
 const PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT: string = "Projects list container";
-const PROJECTS_LIST_TITLE: string = "Some of my work";
+const PROJECTS_LIST_TITLE: string = "MY WORK";
+const PROJECT_LOGO_ALT_TEXT: string = "Project logo";
+const PROJECT_SHOWCASE_IMAGE_ALT_TEXT: string = "Project showcase image";
+const DUMMY_LOGO_IMAGE_URL = "../../../public/projectLists/coronavirus_56dp_EDF2FB_FILL0_wght400_GRAD0_opsz48.svg";
+const DUMMY_LOGO_IMAGE_URL_1 = "../../../public/projectLists/south_america_24dp_EDF2FB_FILL0_wght400_GRAD0_opsz24.svg";
 
-const PROJECTS_LIST_TEMPORAL: { id: string; name: string }[] = [
-   { id: "project-1", name: "Project 1" },
-   { id: "project-2", name: "Project 2" },
-   { id: "project-3", name: "Project 3" },
-   { id: "project-4", name: "Project 4" },
-   { id: "project-5", name: "Project 5" },
-];
+interface Project {
+   project_id: number;
+   project_name: string;
+   project_technologies: string;
+}
 
-export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_TITLE, PROJECTS_LIST_TEMPORAL };
+export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_TITLE, PROJECT_LOGO_ALT_TEXT, PROJECT_SHOWCASE_IMAGE_ALT_TEXT, DUMMY_LOGO_IMAGE_URL, DUMMY_LOGO_IMAGE_URL_1 };
+export type { Project };
