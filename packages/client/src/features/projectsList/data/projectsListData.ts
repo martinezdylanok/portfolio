cb4ce8cb4d499f2794efc8e5e9b0e57a2b79078c
martinezdylanok <dm97fx@gmail.com>
2025-03-13 10:51:59 +0100

    feat: add project list data constants and interface for project details

--- packages/client/src/features/projectsList/data/projectsListData.ts
+++ packages/client/src/features/projectsList/data/projectsListData.ts
@@ -1,5 +1,4 @@
 const PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT: string = "Projects list container";
-const PROJECTS_LIST_TITLE: string = "MY WORK";
 const PROJECT_LOGO_ALT_TEXT: string = "Project logo";
 const PROJECT_SHOWCASE_IMAGE_ALT_TEXT: string = "Project showcase image";
 const DUMMY_LOGO_IMAGE_URL = "../../../public/projectLists/coronavirus_56dp_EDF2FB_FILL0_wght400_GRAD0_opsz48.svg";
@@ -8,8 +7,9 @@ const DUMMY_LOGO_IMAGE_URL_1 = "../../../public/projectLists/south_america_24dp_
 interface Project {
    project_id: number;
    project_name: string;
+   project_description: string;
    project_technologies: string;
 }
 
-export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_TITLE, PROJECT_LOGO_ALT_TEXT, PROJECT_SHOWCASE_IMAGE_ALT_TEXT, DUMMY_LOGO_IMAGE_URL, DUMMY_LOGO_IMAGE_URL_1 };
+export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECT_LOGO_ALT_TEXT, PROJECT_SHOWCASE_IMAGE_ALT_TEXT, DUMMY_LOGO_IMAGE_URL, DUMMY_LOGO_IMAGE_URL_1 };
 export type { Project };
