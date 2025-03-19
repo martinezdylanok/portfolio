56c1b2b6ca1c0308c2eb58a2ac4d653de6562715
martinezdylanok <dm97fx@gmail.com>
2025-03-19 19:15:21 +0100

    feat: update ContactFormTitle component to support theme-based styling

--- packages/client/src/features/contactForm/components/contact-form-title/ContactFormTitle.tsx
+++ packages/client/src/features/contactForm/components/contact-form-title/ContactFormTitle.tsx
@@ -1,7 +1,10 @@
 import { CONTACT_FORM_TITLE } from "./data/contactFormTitleData";
+import { useThemeContext } from "../../../../utils/hooks/useTheme";
 
 const ContactFormTitle = () => {
-   return <h2 className="contact-form__title">{CONTACT_FORM_TITLE}</h2>;
+   const { mode } = useThemeContext();
+
+   return <h2 className={`text-[5.938rem] p-5 ${mode === "light" ? "text-[#ABC4FF]" : "text-[#EDF2FB]"} contact-form__title`}>{CONTACT_FORM_TITLE}</h2>;
 };
 
 export default ContactFormTitle;
