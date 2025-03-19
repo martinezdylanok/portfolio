abfe17cae4923dcc11b5a49eef85b4dbb5906ccb
martinezdylanok <dm97fx@gmail.com>
2025-03-19 19:12:31 +0100

    feat: implement active state management in ContactForm component

--- packages/client/src/features/contactForm/ContactForm.tsx
+++ packages/client/src/features/contactForm/ContactForm.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import ContactFormTitle from "./components/contact-form-title/ContactFormTitle";
 import ContactFormSelector from "./components/contact-form-selector/ContactFormSelector";
 import ContactFormActualForm from "./components/contact-form-actual-form/ContactFormActualForm";
-import { CONTACT_FORM_ARIA_LABEL } from "./data/contactFormData";
+import { CONTACT_FORM_ARIA_LABEL, ActiveState } from "./data/contactFormData";
 
 const ContactForm = () => {
+   const [active, setActive] = useState<ActiveState>("first");
+
+   const handleActiveStateChange = (newActiveState: ActiveState) => {
+      setActive(newActiveState);
+   };
+
    return (
-      <div className="project__contact-form" aria-label={CONTACT_FORM_ARIA_LABEL}>
+      <div data-direction={`${active === "first" ? "left" : "right"}`} className={`relative project__contact-form`} aria-label={CONTACT_FORM_ARIA_LABEL}>
          <ContactFormTitle />
-         <ContactFormSelector />
-         <ContactFormActualForm />
+         <ContactFormSelector activeState={{ active }} activeStateChange={{ handleActiveStateChange }} />
+         <ContactFormActualForm activeState={{ active }} />
       </div>
    );
 };
