87c9fc8c45caafaf4be096949449559f8e31d0c6
martinezdylanok <dm97fx@gmail.com>
2025-03-20 08:45:25 +0100

    test: enhance ContactFormSelector tests with light mode setup and default props

--- packages/client/src/features/contactForm/components/contact-form-selector/__tests__/ContactFormSelector.test.tsx
+++ packages/client/src/features/contactForm/components/contact-form-selector/__tests__/ContactFormSelector.test.tsx
@@ -1,33 +1,47 @@
 import { render, screen } from "@testing-library/react";
 import ContactFormSelector from "../ContactFormSelector";
+import { setupLightMode, resetModes, mockHandleActiveStateChange, defaultProps } from "./test-utils/testUtils";
 
 describe("ContactFormSelector tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+      mockHandleActiveStateChange.mockClear();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders ContactFormSelector element", () => {
-      render(<ContactFormSelector />);
+      render(<ContactFormSelector {...defaultProps} />);
       const element = screen.getByLabelText("Contact form selector");
       expect(element).toBeInTheDocument();
    });
 
    test("renders ContactFormSelector first element", () => {
-      render(<ContactFormSelector />);
+      render(<ContactFormSelector {...defaultProps} />);
       const element = screen.getByLabelText("Contact form first selector");
       expect(element).toBeInTheDocument();
    });
 
    test("renders ContactFormSelector first element's h2 element", () => {
-      render(<ContactFormSelector />);
+      render(<ContactFormSelector {...defaultProps} />);
       const element = screen.getByRole("heading", { name: "CONTACT", level: 2 });
       expect(element).toBeInTheDocument();
    });
 
    test("renders ContactFormSelector second element", () => {
-      render(<ContactFormSelector />);
+      render(<ContactFormSelector {...defaultProps} />);
       const element = screen.getByLabelText("Contact form second selector");
       expect(element).toBeInTheDocument();
    });
 
    test("renders ContactFormSelector second element's h2 element", () => {
-      render(<ContactFormSelector />);
+      render(<ContactFormSelector {...defaultProps} />);
       const element = screen.getByRole("heading", { name: "JOB OFFER", level: 2 });
       expect(element).toBeInTheDocument();
    });
