33d43c3ce6658b88607246ab5042c01c365f17ad
martinezdylanok <dm97fx@gmail.com>
2025-03-20 18:48:52 +0100

    test: enhance ContactFormActualForm tests with light mode setup and detailed field checks

--- packages/client/src/features/contactForm/components/contact-form-actual-form/__tests__/ContactFormActualForm.test.tsx
+++ packages/client/src/features/contactForm/components/contact-form-actual-form/__tests__/ContactFormActualForm.test.tsx
@@ -1,40 +1,116 @@
 import { render, screen } from "@testing-library/react";
 import ContactFormActualForm from "../ContactFormActualForm";
+import { setupLightMode, resetModes, mockHandleActiveStateChange, defaultProps } from "./test-utils/testUtils";
 
 describe("ContactFormActualForm tests", () => {
-   test("renders ContactFormActualForm element", () => {
-      render(<ContactFormActualForm />);
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+      mockHandleActiveStateChange.mockClear();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
+   test("renders ContactFormActualForm contact form element", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
       const contactFormActualFormElement = screen.getByRole("form", { name: "Contact form" });
       expect(contactFormActualFormElement).toBeInTheDocument();
    });
 
-   test("renders name input field", () => {
-      render(<ContactFormActualForm />);
-      const nameInput = screen.getByPlaceholderText("NAME");
+   test("renders name input field element in the contact form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Contact form" });
+      const nameInput = contactFormActualFormElement.querySelector("input[type='text']");
+      expect(nameInput).toContainHTML("NAME");
       expect(nameInput).toBeInTheDocument();
    });
 
-   test("renders phone input field", () => {
-      render(<ContactFormActualForm />);
-      const phoneInput = screen.getByPlaceholderText("PHONE NUMBER");
+   test("renders phone input field element in the contact form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Contact form" });
+      const phoneInput = contactFormActualFormElement.querySelector("input[type='tel']");
+      expect(phoneInput).toContainHTML("PHONE NUMBER");
       expect(phoneInput).toBeInTheDocument();
    });
 
-   test("renders email input field", () => {
-      render(<ContactFormActualForm />);
-      const emailInput = screen.getByPlaceholderText("EMAIL");
+   test("renders email input field element in the contact form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Contact form" });
+      const emailInput = contactFormActualFormElement.querySelector("input[type='email']");
+      expect(emailInput).toContainHTML("EMAIL");
       expect(emailInput).toBeInTheDocument();
    });
 
-   test("renders message textarea field", () => {
-      render(<ContactFormActualForm />);
-      const messageTextarea = screen.getByPlaceholderText("MESSAGE");
-      expect(messageTextarea).toBeInTheDocument();
+   test("renders message textarea field element in the contact form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Contact form" });
+      const textArea = contactFormActualFormElement.querySelector("textarea");
+      expect(textArea).toContainHTML("MESSAGE");
+      expect(textArea).toBeInTheDocument();
    });
 
-   test("renders submit button", () => {
-      render(<ContactFormActualForm />);
-      const submitButton = screen.getByRole("button", { name: "SEND" });
-      expect(submitButton).toBeInTheDocument();
+   test("renders submit button element in the contact form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Contact form" });
+      const button = contactFormActualFormElement.querySelector("button");
+      expect(button).toHaveAttribute("type", "submit");
+      expect(button).toBeInTheDocument();
+   });
+
+   test("renders ContactFormActualForm job offer element", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Job offer form" });
+      expect(contactFormActualFormElement).toBeInTheDocument();
+   });
+
+   test("renders job offer text element in the job offer form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const jobOfferText = screen.getByText(/IF YOU FIND MY WORK INTERESTING/);
+      expect(jobOfferText).toBeInTheDocument();
+   });
+
+   test("renders name input field element in the job offer form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Job offer form" });
+      const nameInput = contactFormActualFormElement.querySelector("input[type='text']");
+      expect(nameInput).toContainHTML("NAME");
+      expect(nameInput).toBeInTheDocument();
+   });
+
+   test("renders phone input field element in the job offer form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Job offer form" });
+      const phoneInput = contactFormActualFormElement.querySelector("input[type='tel']");
+      expect(phoneInput).toContainHTML("PHONE NUMBER");
+      expect(phoneInput).toBeInTheDocument();
+   });
+
+   test("renders email input field element in the job offer form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Job offer form" });
+      const emailInput = contactFormActualFormElement.querySelector("input[type='email']");
+      expect(emailInput).toContainHTML("EMAIL");
+      expect(emailInput).toBeInTheDocument();
+   });
+
+   test("renders message textarea field element in the job offer form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Job offer form" });
+      const textArea = contactFormActualFormElement.querySelector("textarea");
+      expect(textArea).toContainHTML("MESSAGE");
+      expect(textArea).toBeInTheDocument();
+   });
+
+   test("renders submit button element in the job offer form", () => {
+      render(<ContactFormActualForm {...defaultProps} />);
+      const contactFormActualFormElement = screen.getByRole("form", { name: "Job offer form" });
+      const button = contactFormActualFormElement.querySelector("button");
+      expect(button).toHaveAttribute("type", "submit");
+      expect(button).toBeInTheDocument();
    });
 });
