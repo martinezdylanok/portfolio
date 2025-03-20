1badbe13a6042f46c8b87251bf13474cca0c7ceb
martinezdylanok <dm97fx@gmail.com>
2025-03-20 08:16:37 +0100

    test: enhance ContactFormTitle tests with light mode setup and reset utilities

--- packages/client/src/features/contactForm/components/contact-form-title/__tests__/ContactFormTitle.test.tsx
+++ packages/client/src/features/contactForm/components/contact-form-title/__tests__/ContactFormTitle.test.tsx
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import ContactFormTitle from "../ContactFormTitle";
+import { setupLightMode, resetModes } from "./test-utils/testUtils";
 
-describe("ContactFormTitle tests", () => {
+describe("ContactFormTitle component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders ContactFormTitle element", () => {
       render(<ContactFormTitle />);
-      const titleElement = screen.getByRole("heading", { name: "Let's talk" });
+      const titleElement = screen.getByRole("heading", { name: "LET'S TALK" });
       expect(titleElement).toBeInTheDocument();
    });
 });
