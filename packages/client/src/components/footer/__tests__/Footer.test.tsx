4c1fd36f3815ce68d044b7fea3579e2f2037d361
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:57:59 +0100

    test: enhance Footer component tests with theme mode setup and teardown

--- packages/client/src/components/footer/__tests__/Footer.test.tsx
+++ packages/client/src/components/footer/__tests__/Footer.test.tsx
@@ -1,7 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import Footer from "../Footer";
+import { setupLightMode, resetModes } from "./test-utils/test-utils";
 
 describe("Footer Component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("render Footer element", () => {
       render(<Footer />);
       const footerElement = screen.getByRole("contentinfo");
