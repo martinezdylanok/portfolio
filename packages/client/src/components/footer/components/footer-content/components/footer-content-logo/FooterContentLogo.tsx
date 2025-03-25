fedf23196ea594ba61f8c592668495919c101c12
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:14:11 +0100

    feat: implement dynamic logo switching for light and dark modes in FooterContentLogo

--- packages/client/src/components/footer/components/footer-content/components/footer-content-logo/FooterContentLogo.tsx
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-logo/FooterContentLogo.tsx
@@ -1,11 +1,14 @@
-//TODO: Fetch image from database.
-
-import { FOOTER_CONTENT_LOGO__ARIA_LABEL, FOOTER_CONTENT_LOGO__IMAGE_ALT } from "./data/footerContentLogoData";
+import { FOOTER_CONTENT_LOGO__ARIA_LABEL, FOOTER_CONTENT_LOGO__IMAGE_ALT, FOOTER_CONTENT_LOGO_DARK_MODE, FOOTER_CONTENT_LOGO_LIGHT_MODE } from "./data/footerContentLogoData";
+import { useThemeContext } from "../../../../../../utils/hooks/useTheme";
 
 const FooterContentLogo = () => {
+   const { mode } = useThemeContext();
+
    return (
-      <div className="footer-content__logo" aria-label={FOOTER_CONTENT_LOGO__ARIA_LABEL}>
-         <img src="" alt={FOOTER_CONTENT_LOGO__IMAGE_ALT} />
+      <div className="py-20 flex w-fit h-fit justify-self-center footer-content__logo" aria-label={FOOTER_CONTENT_LOGO__ARIA_LABEL}>
+         <a className="inline-block self-center" href="/">
+            <img src={`${mode === "light" ? FOOTER_CONTENT_LOGO_LIGHT_MODE : FOOTER_CONTENT_LOGO_DARK_MODE}`} className={`cursor-pointer footer-content__logo-image transition-all duration-500 ease-in-out hover:scale-110`} alt={FOOTER_CONTENT_LOGO__IMAGE_ALT} />
+         </a>
       </div>
    );
 };
