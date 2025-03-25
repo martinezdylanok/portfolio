c27b03b6cbdfe24350f7d8a537a899d0c6d70f8e
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:20:31 +0100

    feat: add GitHub logo data and accessibility labels for footer component

---
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/components/footer-content-social-github/data/footerContentSocialGithubData.ts
@@ -0,0 +1,7 @@
+const FOOTER_CONTENT_SOCIAL_GITHUB_ARIA_LABEL: string = "Github logo container";
+const GITHUB_LOGO_DARK_MODE: string = "/footer/github_logo_dark_mode.svg";
+const GITHUB_LOGO_LIGHT_MODE: string = "/footer/github_logo_light_mode.svg";
+const GITHUB_LOGO_ALT: string = "Github logo";
+const GITHUB_PROFILE_URL: string = "https://github.com/martinezdylanok";
+
+export { FOOTER_CONTENT_SOCIAL_GITHUB_ARIA_LABEL, GITHUB_LOGO_DARK_MODE, GITHUB_LOGO_LIGHT_MODE, GITHUB_LOGO_ALT, GITHUB_PROFILE_URL };
