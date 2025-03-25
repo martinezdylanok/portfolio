3010dbf16d8da7ef179efe5f59d41c8db8d12397
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:12:27 +0100

    feat: add light and dark mode logo paths to footer content logo data

--- packages/client/src/components/footer/components/footer-content/components/footer-content-logo/data/footerContentLogoData.ts
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-logo/data/footerContentLogoData.ts
@@ -1,4 +1,6 @@
 const FOOTER_CONTENT_LOGO__ARIA_LABEL: string = "Footer content logo container";
 const FOOTER_CONTENT_LOGO__IMAGE_ALT: string = "Dylan Martinez's logo";
+const FOOTER_CONTENT_LOGO_LIGHT_MODE: string = "/footer/dm_logo_light_mode.svg";
+const FOOTER_CONTENT_LOGO_DARK_MODE: string = "/footer/dm_logo_dark_mode.svg";
 
-export { FOOTER_CONTENT_LOGO__ARIA_LABEL, FOOTER_CONTENT_LOGO__IMAGE_ALT };
+export { FOOTER_CONTENT_LOGO__ARIA_LABEL, FOOTER_CONTENT_LOGO__IMAGE_ALT, FOOTER_CONTENT_LOGO_LIGHT_MODE, FOOTER_CONTENT_LOGO_DARK_MODE };
