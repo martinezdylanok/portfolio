e6528a3068998a24030edc41f3dee0de9f19b4c5
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:07:42 +0100

    refactor: remove unused FooterCallToAction and FooterScrollUp components from Footer

--- packages/client/src/components/footer/Footer.tsx
+++ packages/client/src/components/footer/Footer.tsx
@@ -1,13 +1,9 @@
-import FooterCallToAction from "./components/footer-call-to-action/FooterCallToAction";
 import FooterContent from "./components/footer-content/FooterContent";
-import FooterScrollUp from "./components/footer-scroll-up/FooterScrollUp";
 
 const Footer = () => {
    return (
       <footer>
          <FooterContent />
-         <FooterCallToAction />
-         <FooterScrollUp />
       </footer>
    );
 };
