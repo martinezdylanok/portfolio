49644c50e0466ff982e7304435987605ccaa67f9
martinezdylanok <dm97fx@gmail.com>
2025-03-25 19:14:35 +0100

    test: enhance FooterContentLogo tests with light and dark mode setups

--- packages/client/src/components/footer/components/footer-content/components/footer-content-logo/__tests__/FooterContentLogo.test.tsx
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-logo/__tests__/FooterContentLogo.test.tsx
@@ -1,7 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import FooterContentLogo from "../FooterContentLogo";
+import { setupLightMode, resetModes, setupDarkMode } from "./test-utils/test-utils";
 
 describe("FooterContentLogo tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders FooterContentLogo element", () => {
       render(<FooterContentLogo />);
       const FooterContentLogoElement = screen.getByLabelText("Footer content logo container");
@@ -12,5 +25,18 @@ describe("FooterContentLogo tests", () => {
       render(<FooterContentLogo />);
       const FooterContentLogoImage = screen.getByAltText("Dylan Martinez's logo");
       expect(FooterContentLogoImage).toBeInTheDocument();
+   });
+
+   test("renders FooterContentLogo image in light mode", () => {
+      render(<FooterContentLogo />);
+      const FooterContentLogoImage = screen.getByAltText("Dylan Martinez's logo");
+      expect(FooterContentLogoImage).toHaveAttribute("src", "/footer/dm_logo_light_mode.svg");
+   });
+
+   test("renders FooterContentLogo image in dark mode", () => {
+      setupDarkMode();
+      render(<FooterContentLogo />);
+      const FooterContentLogoImage = screen.getByAltText("Dylan Martinez's logo");
+      expect(FooterContentLogoImage).toHaveAttribute("src", "/footer/dm_logo_dark_mode.svg");
    });
 });
