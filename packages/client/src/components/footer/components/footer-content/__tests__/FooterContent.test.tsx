3d3fab1bd14e29a06292232311de4993cc377405
martinezdylanok <dm97fx@gmail.com>
2025-03-25 19:04:19 +0100

    test: enhance FooterContent tests with setup and teardown for theme modes

--- packages/client/src/components/footer/components/footer-content/__tests__/FooterContent.test.tsx
+++ packages/client/src/components/footer/components/footer-content/__tests__/FooterContent.test.tsx
@@ -1,8 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import FooterContent from "../FooterContent";
+import { setupLightMode, resetModes } from "./test-utils/test-utils";
 
 describe("FooterContent tests", () => {
-   test("renders FooterContent element", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
+   test("should render the FooterContent element", () => {
       render(<FooterContent />);
       const footerContent = screen.getByLabelText("Footer content container");
       expect(footerContent).toBeInTheDocument();
