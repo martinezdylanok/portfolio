cd7902bed93498b1113969970450da547b0e5c83
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:21:50 +0100

    feat: add accessibility labels and span text for footer social links

---
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/data/footerContentSocialData.ts
@@ -0,0 +1,4 @@
+const FOOTER_CONTENT_SOCIAL__ARIA_LABEL: string = "Footer social container";
+const FOOTER_CONTENT_SOCIAL__SPAN_TEXT: string = "Follow me on";
+
+export { FOOTER_CONTENT_SOCIAL__ARIA_LABEL, FOOTER_CONTENT_SOCIAL__SPAN_TEXT };
