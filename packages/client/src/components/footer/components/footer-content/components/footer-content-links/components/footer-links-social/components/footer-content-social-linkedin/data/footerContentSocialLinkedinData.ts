6c21a496d23423afe9f57e086c81b3b80da02ad9
martinezdylanok <dm97fx@gmail.com>
2025-03-25 18:18:28 +0100

    feat: add LinkedIn logo data and accessibility labels for footer component

---
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/components/footer-content-social-linkedin/data/footerContentSocialLinkedinData.ts
@@ -0,0 +1,7 @@
+const FOOTER_CONTENT_SOCIAL_LINKEDIN_ARIA_LABEL: string = "Linkedin logo container";
+const LINKEDIN_LOGO_DARK_MODE: string = "/footer/linkedin_logo_dark_mode.svg";
+const LINKEDIN_LOGO_LIGHT_MODE: string = "/footer/linkedin_logo_light_mode.svg";
+const LINKEDIN_LOGO_ALT: string = "Linkedin logo";
+const LINKEDIN_PROFILE_URL: string = "https://www.linkedin.com/in/martinezdylanok/";
+
+export { FOOTER_CONTENT_SOCIAL_LINKEDIN_ARIA_LABEL, LINKEDIN_LOGO_DARK_MODE, LINKEDIN_LOGO_LIGHT_MODE, LINKEDIN_LOGO_ALT, LINKEDIN_PROFILE_URL };
