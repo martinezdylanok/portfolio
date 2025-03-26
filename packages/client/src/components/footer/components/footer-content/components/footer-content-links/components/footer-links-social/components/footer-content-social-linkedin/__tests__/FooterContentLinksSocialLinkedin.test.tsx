24d98f3f2a4ecd48b712870ddf66572631125584
martinezdylanok <dm97fx@gmail.com>
2025-03-26 08:25:25 +0100

    test: add tests for FooterContentLinksSocialLinkedin component with light and dark mode setups

---
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/components/footer-content-social-linkedin/__tests__/FooterContentLinksSocialLinkedin.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import FooterContentLinksSocialLinkedin from "../FooterContentLinksSocialLinkedin";
+import { setupLightMode, resetModes, setupDarkMode } from "./test-utils/testUtils";
+
+describe("FooterContentLinksSocialLinkedin component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../../../../../utils/hooks/useTheme");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
+   test("renders the FooterContentLinksLinkeding div element", () => {
+      render(<FooterContentLinksSocialLinkedin />);
+      const footerCOntentSocialLinkedinContainer = screen.getByLabelText("Linkedin logo container");
+      expect(footerCOntentSocialLinkedinContainer).toBeInTheDocument();
+   });
+
+   test("renders the FooterContentLinksLinkeding a element", () => {
+      render(<FooterContentLinksSocialLinkedin />);
+      const footerContentLinksSocialLinkedinLink = screen.getByRole("link");
+      expect(footerContentLinksSocialLinkedinLink).toBeInTheDocument();
+   });
+
+   test("renders the FooterContentLinksLinkeding element", () => {
+      render(<FooterContentLinksSocialLinkedin />);
+      const footerContentLinksSocialLinkedin = screen.getByAltText("Linkedin logo");
+      expect(footerContentLinksSocialLinkedin).toBeInTheDocument();
+   });
+
+   test("renders the FooterContentLinksLinkeding light mode element", () => {
+      render(<FooterContentLinksSocialLinkedin />);
+      const footerContentLinksSocialLinkedinLightMode = screen.getByAltText("Linkedin logo");
+      expect(footerContentLinksSocialLinkedinLightMode).toHaveAttribute("src", " /footer/linkedin_logo_dark_mode.svg");
+   });
+
+   test("renders the FooterContentLinksLinkeding dark mode element", () => {
+      setupDarkMode();
+      render(<FooterContentLinksSocialLinkedin />);
+      const footerContentLinksSocialLinkedinDarkMode = screen.getByAltText("Linkedin logo");
+      expect(footerContentLinksSocialLinkedinDarkMode).toHaveAttribute("src", " /footer/linkedin_logo_light_mode.svg");
+   });
+});
