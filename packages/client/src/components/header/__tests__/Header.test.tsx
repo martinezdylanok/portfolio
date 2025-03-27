cae7010019ce4472adcd1ffd4d9b8a92dc5dac85
martinezdylanok <dm97fx@gmail.com>
2025-03-27 11:36:35 +0100

    test: enhance Header component tests for background color and scroll behavior

--- packages/client/src/components/header/__tests__/Header.test.tsx
+++ packages/client/src/components/header/__tests__/Header.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Header from "../Header";
-import { setupLightMode, resetModes } from "./test-utils/test-utils";
+import { setupLightMode, resetModes, setupDarkMode, getPageYOffset } from "./test-utils/test-utils";
+import { act } from "react";
 
 describe("Header component tests", () => {
    beforeAll(() => {
@@ -17,7 +18,42 @@ describe("Header component tests", () => {
 
    test("should render the header element", () => {
       render(<Header />);
-      const header = screen.getByRole("banner");
+      const header = screen.getByRole("banner", { name: "Site header" });
       expect(header).toBeInTheDocument();
+   });
+
+   test("should render the header correct background color", () => {
+      setupDarkMode();
+      render(<Header />);
+      const header = screen.getByRole("banner", { name: "Site header" });
+      expect(header).toHaveClass("bg-[#ABC4FF]");
+   });
+
+   test("should render the header with the correct background color in light mode", () => {
+      render(<Header />);
+      const header = screen.getByRole("banner", { name: "Site header" });
+      expect(header).toHaveClass("bg-[#EDF2FB]");
+   });
+
+   test("should render the header with the correct Y position", () => {
+      render(<Header />);
+      const header = screen.getByRole("banner", { name: "Site header" });
+      expect(header).toHaveClass("transform-none");
+   });
+
+   test("should render the header with the correct Y position when scrolling", async () => {
+      render(<Header />);
+      const header = screen.getByRole("banner", { name: "Site header" });
+      expect(header).toHaveClass("transform-none");
+
+      getPageYOffset();
+
+      act(() => {
+         window.dispatchEvent(new Event("scroll"));
+      });
+
+      await waitFor(() => {
+         expect(header).toHaveClass("-translate-y-full");
+      });
    });
 });
