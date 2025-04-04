72d5a5f04f43c7f16b7ff5866094f2f96b9d5734
martinezdylanok <dm97fx@gmail.com>
2025-04-04 10:48:54 +0200

    refactor: rename ActiveState to ActiveForm and update related handlers in ContactForm

--- packages/client/src/features/contactForm/ContactForm.tsx
+++ packages/client/src/features/contactForm/ContactForm.tsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 import ContactFormTitle from "./components/contact-form-title/ContactFormTitle";
 import ContactFormSelector from "./components/contact-form-selector/ContactFormSelector";
 import ContactFormActualForm from "./components/contact-form-actual-form/ContactFormActualForm";
-import { CONTACT_FORM_ARIA_LABEL, ActiveState } from "./data/contactFormData";
+import { CONTACT_FORM_ARIA_LABEL, ActiveForm } from "./data/contactFormData";
 
 const ContactForm = () => {
-   const [active, setActive] = useState<ActiveState>("first");
+   const [activeForm, setActiveForm] = useState<ActiveForm>("firstForm");
 
-   const handleActiveStateChange = (newActiveState: ActiveState) => {
-      setActive(newActiveState);
+   const handleActiveFormUpdate = (newActiveForm: ActiveForm) => {
+      setActiveForm(newActiveForm);
    };
 
    return (
-      <div data-direction={`${active === "first" ? "left" : "right"}`} className={`relative project__contact-form`} aria-label={CONTACT_FORM_ARIA_LABEL}>
+      <div data-direction={`${activeForm === "firstForm" ? "left" : "right"}`} className={`relative project__contact-form`} aria-label={CONTACT_FORM_ARIA_LABEL}>
          <ContactFormTitle />
-         <ContactFormSelector activeState={{ active }} activeStateChange={{ handleActiveStateChange }} />
-         <ContactFormActualForm activeState={{ active }} />
+         <ContactFormSelector activeForm={activeForm} handleActiveFormUpdate={handleActiveFormUpdate} />
+         <ContactFormActualForm activeForm={activeForm} />
       </div>
    );
 };
