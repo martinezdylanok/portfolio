ef890451de0f50866ef888694e797ee6c07a6e47
martinezdylanok <dm97fx@gmail.com>
2025-04-04 10:48:14 +0200

    test: enhance ContactForm tests with theme setup and teardown utilities

--- packages/client/src/features/contactForm/__tests__/contactForm.test.tsx
+++ packages/client/src/features/contactForm/__tests__/contactForm.test.tsx
@@ -1,5 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import ContactForm from "../ContactForm";
+import { setupLightMode, resetModes } from "./test-utils/testUtils";
+
+beforeAll(() => {
+   vi.mock("../../../utils/hooks/useTheme");
+});
+
+beforeEach(() => {
+   setupLightMode();
+});
+
+afterAll(() => {
+   resetModes();
+});
 
 describe("ContactForm tests", () => {
    test("renders ContactForm element", () => {
