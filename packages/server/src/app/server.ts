cef3f66d95c49a542cccb94af4496846842a8c66
martinezdylanok <dm97fx@gmail.com>
2025-04-08 12:09:14 +0200

    feat: enhance server configuration with CORS options and update API routes

--- packages/server/src/app/server.ts
+++ packages/server/src/app/server.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import projectRoutes from "../routes/projectRoutes/projectRoutes.js";
+import { corsOptions } from "./data/serverData.js";
 
 const server = express();
 
 /* Middlewares */
 server.use(cors());
 server.use(express.json());
+server.use(cors(corsOptions));
 
 /* Static files */
 const __filename = fileURLToPath(import.meta.url);
@@ -17,10 +19,11 @@ const __dirname = path.dirname(__filename);
 const clientDistPath = path.join(__dirname, "../../../client/dist");
 server.use(express.static(clientDistPath));
 
-/* Routes */
-server.use("/projects", projectRoutes);
+/* API Routes */
+server.use("/api/projects", projectRoutes);
 
-server.get("/", (__, res) => {
+/* Catch all route */
+server.get("*", (__, res) => {
    res.sendFile(path.join(clientDistPath, "index.html"));
 });
 
