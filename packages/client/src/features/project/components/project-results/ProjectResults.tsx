8a4e1db081dea6e3462c9b0c948f2cb38c2adc25
martinezdylanok <dm97fx@gmail.com>
2025-04-09 15:00:52 +0200

    feat: update ProjectResults to accept project props and pass them to ProjectResultsContainer

--- packages/client/src/features/project/components/project-results/ProjectResults.tsx
+++ packages/client/src/features/project/components/project-results/ProjectResults.tsx
@@ -1,10 +1,10 @@
 import ProjectResultsContainer from "./components/project-results-container/ProjectResultsContainer";
-import { PROJECT_RESULTS_ARIA_LABEL } from "./data/projectResultsData";
+import { PROJECT_RESULTS_ARIA_LABEL, ProjectResultsProps } from "./data/projectResultsData";
 
-const ProjectResults = () => {
+const ProjectResults = ({ project }: ProjectResultsProps) => {
    return (
       <section className="project__results" aria-label={PROJECT_RESULTS_ARIA_LABEL}>
-         <ProjectResultsContainer />
+         <ProjectResultsContainer project={project} />
       </section>
    );
 };
