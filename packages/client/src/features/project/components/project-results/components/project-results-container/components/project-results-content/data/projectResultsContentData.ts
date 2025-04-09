addfc182fb79bbaa92daad2a9d39eb64951294ca
martinezdylanok <dm97fx@gmail.com>
2025-04-09 15:04:01 +0200

    feat: define ProjectInterface and ProjectResultsContentProps for project data structure

--- packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/data/projectResultsContentData.ts
+++ packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/data/projectResultsContentData.ts
@@ -1,4 +1,20 @@
 const PROJECT_RESULTS_CONTENT_ARIA_LABEL: string = "Project results content";
 const PROJECT_RESULTS_CONTENT_TITLE: string = "Final results";
 
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectResultsContentProps {
+   project: ProjectInterface;
+}
+
 export { PROJECT_RESULTS_CONTENT_ARIA_LABEL, PROJECT_RESULTS_CONTENT_TITLE };
