4acfab9fc6404966e5609e5911386523b63b926f
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:16:55 +0200

    feat: implement routing for project details and enhance app structure

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,16 +1,24 @@
 import { ThemeProvider } from "../utils/themeProvider/themeProvider";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import ContactForm from "../features/contactForm/ContactForm";
 import ProjectsList from "../features/projectsList/ProjectsList";
+import Project from "../features/project/Project";
 
 function App() {
    return (
       <ThemeProvider>
-         <Header />
-         <ProjectsList />
-         <ContactForm />
-         <Footer />
+         <BrowserRouter>
+            <Header />
+            <Routes>
+               <Route path="/" element={<ProjectsList />} />
+               <Route path="/projects" element={<ProjectsList />} />
+               <Route path="/projects/:projectName" element={<Project />} />
+            </Routes>
+            <ContactForm />
+            <Footer />
+         </BrowserRouter>
       </ThemeProvider>
    );
 }
