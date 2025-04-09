4864f03201af11d2e9e833cfb75776dbebf857fa
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:26:40 +0200

    feat: pass project data to ProjectMainCoverContainer for content rendering

--- packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/ProjectMainCoverContainer.tsx
+++ packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/ProjectMainCoverContainer.tsx
@@ -1,11 +1,11 @@
 import ProjectMainCoverContent from "./components/project-main-cover-content/ProjectMainCoverContent";
 import ProjectMainCoverLogos from "./components/project-main-cover-logos/ProjectMainCoverLogos";
-import { PROJECT_MAIN_COVER_CONTAINER_ARIA_LABEL } from "./data/projectMainCoverContainerData";
+import { PROJECT_MAIN_COVER_CONTAINER_ARIA_LABEL, ProjectMainCoverContainerProps } from "./data/projectMainCoverContainerData";
 
-const ProjectMainCoverContainer = () => {
+const ProjectMainCoverContainer = ({ project }: ProjectMainCoverContainerProps) => {
    return (
       <div className="main-cover__container" aria-label={PROJECT_MAIN_COVER_CONTAINER_ARIA_LABEL}>
-         <ProjectMainCoverContent />
+         <ProjectMainCoverContent project={project} />
          <ProjectMainCoverLogos />
       </div>
    );
