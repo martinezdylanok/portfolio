25e57d1247c92634779baaadb54724e056986851
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:22:57 +0200

    feat: add ProjectInterface to define project data structure

--- packages/client/src/features/project/data/projectData.ts
+++ packages/client/src/features/project/data/projectData.ts
@@ -1,1 +1,13 @@
 export const PROJECT_ARIA_LABEL: string = "Project container";
+
+export interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
