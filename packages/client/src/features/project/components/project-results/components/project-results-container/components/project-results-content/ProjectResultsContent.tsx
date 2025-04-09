02f822785b5721f7ec49da3edbee4b652a0f9c3c
martinezdylanok <dm97fx@gmail.com>
2025-04-09 15:03:34 +0200

    feat: update ProjectResultsContent to accept project props and display final results

--- packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
+++ packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
@@ -1,12 +1,12 @@
 // TODO: Create the logic for fetching the results of the project from the database.
 
-import { PROJECT_RESULTS_CONTENT_ARIA_LABEL, PROJECT_RESULTS_CONTENT_TITLE } from "./data/projectResultsContentData";
+import { PROJECT_RESULTS_CONTENT_ARIA_LABEL, PROJECT_RESULTS_CONTENT_TITLE, ProjectResultsContentProps } from "./data/projectResultsContentData";
 
-const ProjectResultsContent = () => {
+const ProjectResultsContent = ({ project }: ProjectResultsContentProps) => {
    return (
       <div className="project__results-content" aria-label={PROJECT_RESULTS_CONTENT_ARIA_LABEL}>
          <h2 className="project__results-title">{PROJECT_RESULTS_CONTENT_TITLE}</h2>
-         <p className="project__results-description"></p>
+         <p className="project__results">{project.project_final_results}</p>
       </div>
    );
 };
