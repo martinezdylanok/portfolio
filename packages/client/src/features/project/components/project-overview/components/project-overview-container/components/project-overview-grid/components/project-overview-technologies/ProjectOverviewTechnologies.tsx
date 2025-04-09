41f435213e8726f590f2a2d51034f3f37611286a
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:40:24 +0200

    feat: update ProjectOverviewTechnologies to display project technologies

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
@@ -1,12 +1,12 @@
 // TODO: Create the logic for fetching the technologies of the project from the database.
 
-import { PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL, PROJECT_OVERVIEW_TECHNOLOGIES_TITLE } from "./data/projectOverviewTechnologiesData";
+import { PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL, PROJECT_OVERVIEW_TECHNOLOGIES_TITLE, ProjectOverviewTechnologiesProps } from "./data/projectOverviewTechnologiesData";
 
-const ProjectOverviewTechnologies = () => {
+const ProjectOverviewTechnologies = ({ project }: ProjectOverviewTechnologiesProps) => {
    return (
       <div className="project__overview-grid-technologies" aria-label={PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL}>
          <h3 className="project__overview-grid-technologies-title">{PROJECT_OVERVIEW_TECHNOLOGIES_TITLE}</h3>
-         <ul className="project__overview-grid-technologies-list"></ul>
+         <ul className="project__overview-grid-technologies-list">{project.project_overview_technologies}</ul>
       </div>
    );
 };
