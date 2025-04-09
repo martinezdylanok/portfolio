a27a1246acb6794468c8ee0d88c19fd0d9117860
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:33:58 +0200

    feat: refactor ProjectInterface for global use in project overview content

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/data/projectOverviewContentData.ts
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/data/projectOverviewContentData.ts
@@ -1,1 +1,19 @@
 export const PROJECT_OVERVIEW_CONTENT_ARIA_LABEL: string = "Project overview content";
+
+// REFACTOR: Make this interface to be used globally so there is no need to declare it locally.
+
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectOverviewContentProps {
+   project: ProjectInterface;
+}
