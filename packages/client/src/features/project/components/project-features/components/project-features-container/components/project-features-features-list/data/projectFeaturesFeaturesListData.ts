00f5fbe63f8122593a92b385c0f8bce33e0505de
martinezdylanok <dm97fx@gmail.com>
2025-04-09 14:57:40 +0200

    feat: add ProjectInterface and ProjectFeaturesListProps for project data structure

--- packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/data/projectFeaturesFeaturesListData.ts
+++ packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/data/projectFeaturesFeaturesListData.ts
@@ -1,4 +1,20 @@
 const PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL: string = "Project features features list";
 const PROJECT_FEATURES_FEATURES_LIST_TITLE: string = "Features";
 
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectFeaturesListProps {
+   project: ProjectInterface;
+}
+
 export { PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL, PROJECT_FEATURES_FEATURES_LIST_TITLE };
