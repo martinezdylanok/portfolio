1022abc6915e6f079ccd069f6bb8630e4d8cb2ad
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:31:24 +0200

    feat: pass project data to ProjectOverviewContainer for enhanced content rendering

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/ProjectOverviewContainer.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/ProjectOverviewContainer.tsx
@@ -1,12 +1,12 @@
 import ProjectOverviewContent from "./components/project-overview-content/ProjectOverviewContent";
 import ProjectOverviewGrid from "./components/project-overview-grid/ProjectOverviewGrid";
-import { PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL } from "./data/projectOverviewContainerData";
+import { PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL, ProjectOvervierContainerProps } from "./data/projectOverviewContainerData";
 
-const ProjectOverviewContainer = () => {
+const ProjectOverviewContainer = ({ project }: ProjectOvervierContainerProps) => {
    return (
       <div className="project__overview-container" aria-label={PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL}>
-         <ProjectOverviewContent />
-         <ProjectOverviewGrid />
+         <ProjectOverviewContent project={project} />
+         <ProjectOverviewGrid project={project} />
       </div>
    );
 };
