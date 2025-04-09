6a1dac765abdc9cff20b5d0e093ef97d6cfe4462
martinezdylanok <dm97fx@gmail.com>
2025-04-09 14:57:09 +0200

    feat: update ProjectFeaturesFeaturesList to accept project props and display project features

--- packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
+++ packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
@@ -1,12 +1,12 @@
 // TODO: Create the logic for fetching the features of the project from the database.
 
-import { PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL, PROJECT_FEATURES_FEATURES_LIST_TITLE } from "./data/projectFeaturesFeaturesListData";
+import { PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL, PROJECT_FEATURES_FEATURES_LIST_TITLE, ProjectFeaturesListProps } from "./data/projectFeaturesFeaturesListData";
 
-const ProjectFeaturesFeaturesList = () => {
+const ProjectFeaturesFeaturesList = ({ project }: ProjectFeaturesListProps) => {
    return (
       <div className="project__features-container-features-list" aria-label={PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL}>
          <h2 className="project__features-container-features-title">{PROJECT_FEATURES_FEATURES_LIST_TITLE}</h2>
-         <p className="project__features-container-features-description"></p>
+         <p className="project__features-container-features">{project.project_features}</p>
       </div>
    );
 };
