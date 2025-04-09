88e61c21342693660e00f68d25beccbe656b6319
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:28:24 +0200

    feat: update ProjectMainCoverContent to display project title and description

--- packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
+++ packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
@@ -1,12 +1,12 @@
 // TODO: Create the logic for fetching the title and description of the project from the database.
 
-import { PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL } from "./data/projectMainCoverContentData";
+import { PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL, ProjectMainCoverContentProps } from "./data/projectMainCoverContentData";
 
-const ProjectMainCoverContent = () => {
+const ProjectMainCoverContent = ({ project }: ProjectMainCoverContentProps) => {
    return (
       <div className="main-cover__content" aria-label={PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL}>
-         <h3 className="main-cover__project-title"></h3>
-         <p className="main-cover__project-description"></p>
+         <h3 className="main-cover__project-title">{project.project_name}</h3>
+         <p className="main-cover__project-description">{project.project_description}</p>
       </div>
    );
 };
