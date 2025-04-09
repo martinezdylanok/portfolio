069febf785020d16085002ff959d5b6507f0b93c
martinezdylanok <dm97fx@gmail.com>
2025-04-09 14:58:19 +0200

    feat: update ProjectFeaturesChallengesList to accept project props and display project challenges

--- packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-challenges-list/ProjectFeaturesChallengesList.tsx
+++ packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-challenges-list/ProjectFeaturesChallengesList.tsx
@@ -1,12 +1,12 @@
 // TODO: Create the logic for fetching the challenges of the project from the database.
 
-import { PROJECT_FEATURES_CHALLENGES_ARIA_LABEL, PROJECT_FEATURES_CHALLENGES_TITLE } from "./data/projectFeaturesChallengesListData";
+import { PROJECT_FEATURES_CHALLENGES_ARIA_LABEL, PROJECT_FEATURES_CHALLENGES_TITLE, ProjectFeaturesChallengesListProps } from "./data/projectFeaturesChallengesListData";
 
-const ProjectFeaturesChallengesList = () => {
+const ProjectFeaturesChallengesList = ({ project }: ProjectFeaturesChallengesListProps) => {
    return (
       <div className="project__features-container-challenges-list" aria-label={PROJECT_FEATURES_CHALLENGES_ARIA_LABEL}>
          <h2 className="project__features-container-challenges-title">{PROJECT_FEATURES_CHALLENGES_TITLE}</h2>
-         <p className="project__features-container-description-"></p>
+         <p className="project__features-container-challenges">{project.project_challenges}</p>
       </div>
    );
 };
