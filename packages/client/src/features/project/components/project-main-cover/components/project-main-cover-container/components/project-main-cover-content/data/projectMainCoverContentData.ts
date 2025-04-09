eadd4d0af2925685fa9ab7bd4a80f0cf880905f4
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:29:03 +0200

    feat: refactor ProjectInterface for global use in project main cover content

--- packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/data/projectMainCoverContentData.ts
+++ packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/data/projectMainCoverContentData.ts
@@ -1,1 +1,19 @@
 export const PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL: string = "Project main cover content";
+
+// REFACTOR: Make this interface to be used globally so there is no need to declare it locally.
+
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectMainCoverContentProps {
+   project: ProjectInterface;
+}
