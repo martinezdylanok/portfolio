6edcea5123d93f653ebc21422299128a9f3a5c51
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:41:49 +0200

    feat: enhance ProjectOverviewDuration to display project duration from props

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
@@ -1,12 +1,12 @@
 // TODO: Create the logic for fetching the duration of the project from the database.
 
-import { PROJECT_OVERVIEW_DURATION_ARIA_LABEL, PROJECT_OVERVIEW_DURATION_TITLE_TEXT } from "./data/projectOverviewDurationData";
+import { PROJECT_OVERVIEW_DURATION_ARIA_LABEL, PROJECT_OVERVIEW_DURATION_TITLE_TEXT, ProjectOverviewDurationProps } from "./data/projectOverviewDurationData";
 
-const ProjectOverviewDuration = () => {
+const ProjectOverviewDuration = ({ project }: ProjectOverviewDurationProps) => {
    return (
       <div className="project__overview-grid-duration" aria-label={PROJECT_OVERVIEW_DURATION_ARIA_LABEL}>
          <h3 className="project__overview-grid-duration-title">{PROJECT_OVERVIEW_DURATION_TITLE_TEXT}</h3>
-         <p className="project__overview-grid-duration-value"></p>
+         <p className="project__overview-grid-duration">{project.project_overview_duration}</p>
       </div>
    );
 };
