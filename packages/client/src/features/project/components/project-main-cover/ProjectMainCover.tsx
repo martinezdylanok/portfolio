54db3f969f85b77ae6924f492994fd0eff7932e0
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:24:19 +0200

    feat: pass project data to ProjectMainCoverContainer for enhanced rendering

--- packages/client/src/features/project/components/project-main-cover/ProjectMainCover.tsx
+++ packages/client/src/features/project/components/project-main-cover/ProjectMainCover.tsx
@@ -1,10 +1,10 @@
 import ProjectMainCoverContainer from "./components/project-main-cover-container/ProjectMainCoverContainer";
-import { PROJECT_MAIN_COVER_ARIA_LABEL } from "./data/projectMainCoverData";
+import { PROJECT_MAIN_COVER_ARIA_LABEL, ProjectMainCoverProps } from "./data/projectMainCoverData";
 
-const ProjectMainCover = () => {
+const ProjectMainCover = ({ project }: ProjectMainCoverProps) => {
    return (
       <section className="project__main-cover" aria-label={PROJECT_MAIN_COVER_ARIA_LABEL}>
-         <ProjectMainCoverContainer />
+         <ProjectMainCoverContainer project={project} />
       </section>
    );
 };
