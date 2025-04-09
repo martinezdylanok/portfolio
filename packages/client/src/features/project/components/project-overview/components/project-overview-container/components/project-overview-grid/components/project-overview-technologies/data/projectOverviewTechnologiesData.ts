9fc7d3e8b6c97b1a6c55fa548c394303ba3bd5f8
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:40:48 +0200

    feat: refactor ProjectInterface for global use in project overview technologies

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/data/projectOverviewTechnologiesData.ts
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/data/projectOverviewTechnologiesData.ts
@@ -1,4 +1,22 @@
 const PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL: string = "Project overview technologies";
 const PROJECT_OVERVIEW_TECHNOLOGIES_TITLE: string = "Technologies";
 
+// REFACTOR: Make this interface to be used globally so there is no need to declare it locally.
+
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectOverviewTechnologiesProps {
+   project: ProjectInterface;
+}
+
 export { PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL, PROJECT_OVERVIEW_TECHNOLOGIES_TITLE };
