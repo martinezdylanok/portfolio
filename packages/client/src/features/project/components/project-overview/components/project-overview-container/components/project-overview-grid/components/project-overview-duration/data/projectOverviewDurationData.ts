a43f9da66f3e874b3be0ba179d6def922946577f
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:42:22 +0200

    feat: refactor ProjectInterface for global use in project overview duration component

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/data/projectOverviewDurationData.ts
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/data/projectOverviewDurationData.ts
@@ -1,4 +1,22 @@
 const PROJECT_OVERVIEW_DURATION_ARIA_LABEL: string = "Project overview duration";
 const PROJECT_OVERVIEW_DURATION_TITLE_TEXT: string = "Duration";
 
+// REFACTOR: Make this interface to be used globally so there is no need to declare it locally.
+
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectOverviewDurationProps {
+   project: ProjectInterface;
+}
+
 export { PROJECT_OVERVIEW_DURATION_ARIA_LABEL, PROJECT_OVERVIEW_DURATION_TITLE_TEXT };
