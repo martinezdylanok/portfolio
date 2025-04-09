ef48ba66e18336a5f66a6d1dd532fdca096ce3eb
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:39:09 +0200

    feat: pass project data to ProjectOverviewGrid for enhanced component rendering

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/ProjectOverviewGrid.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/ProjectOverviewGrid.tsx
@@ -1,14 +1,12 @@
 import ProjectOverviewTechnologies from "./components/project-overview-technologies/ProjectOverviewTechnologies";
 import ProjectOverviewDuration from "./components/project-overview-duration/ProjectOverviewDuration";
-import ProjectOverviewDocumentation from "./components/project-overview-documentation/ProjectOverviewDocumentation";
-import { PROJECT_OVERVIEW_GRID_ARIA_LABEL } from "./data/projectOverviewGridData";
+import { PROJECT_OVERVIEW_GRID_ARIA_LABEL, ProjecOverviewGridProps } from "./data/projectOverviewGridData";
 
-const ProjectOverviewGrid = () => {
+const ProjectOverviewGrid = ({ project }: ProjecOverviewGridProps) => {
    return (
       <div className="project__overview-grid" aria-label={PROJECT_OVERVIEW_GRID_ARIA_LABEL}>
-         <ProjectOverviewTechnologies />
-         <ProjectOverviewDuration />
-         <ProjectOverviewDocumentation />
+         <ProjectOverviewTechnologies project={project} />
+         <ProjectOverviewDuration project={project} />
       </div>
    );
 };
