4f6a62e48edc15d437fa373d1053b21ab4ea55e3
martinezdylanok <dm97fx@gmail.com>
2025-04-09 14:55:51 +0200

    feat: update ProjectFeaturesContainer to accept project props and pass to child components

--- packages/client/src/features/project/components/project-features/components/project-features-container/ProjectFeaturesContainer.tsx
+++ packages/client/src/features/project/components/project-features/components/project-features-container/ProjectFeaturesContainer.tsx
@@ -1,12 +1,12 @@
 import ProjectFeaturesChallengesList from "./components/project-features-challenges-list/ProjectFeaturesChallengesList";
 import ProjectFeaturesFeaturesList from "./components/project-features-features-list/ProjectFeaturesFeaturesList";
-import { PROJECT_FEATURES_CONTAINER_ARIA_LABEL } from "./data/projectFeaturesContainerData";
+import { PROJECT_FEATURES_CONTAINER_ARIA_LABEL, ProjectFeaturesContainerProps } from "./data/projectFeaturesContainerData";
 
-const ProjectFeaturesContainer = () => {
+const ProjectFeaturesContainer = ({ project }: ProjectFeaturesContainerProps) => {
    return (
       <div className="project__features-container" aria-label={PROJECT_FEATURES_CONTAINER_ARIA_LABEL}>
-         <ProjectFeaturesFeaturesList />
-         <ProjectFeaturesChallengesList />
+         <ProjectFeaturesFeaturesList project={project} />
+         <ProjectFeaturesChallengesList project={project} />
       </div>
    );
 };
