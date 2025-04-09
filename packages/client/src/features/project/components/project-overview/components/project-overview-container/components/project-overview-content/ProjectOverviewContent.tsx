8b2f682b4b1f1c81ffa2f67f85947bcb5bb4edd2
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:32:41 +0200

    feat: enhance ProjectOverviewContent to display project description

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
@@ -1,12 +1,12 @@
 //TODO: Fetch project's title and details from database.
 
-import { PROJECT_OVERVIEW_CONTENT_ARIA_LABEL } from "./data/projectOverviewContentData";
+import { PROJECT_OVERVIEW_CONTENT_ARIA_LABEL, ProjectOverviewContentProps } from "./data/projectOverviewContentData";
 
-const ProjectOverviewContent = () => {
+const ProjectOverviewContent = ({ project }: ProjectOverviewContentProps) => {
    return (
       <div className="project__overview-content" aria-label={PROJECT_OVERVIEW_CONTENT_ARIA_LABEL}>
          <h2 className="project__overview-content-title"></h2>
-         <p className="project__overview-project-details"></p>
+         <p className="project__overview-project-description">{project.project_overview_description}</p>
       </div>
    );
 };
