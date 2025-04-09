49e789864f93a25ca0695b0744721a469dd9930d
martinezdylanok <dm97fx@gmail.com>
2025-04-09 13:15:59 +0200

    feat: enhance project retrieval responses with success status and structured messages

--- packages/server/src/controllers/projectController/projectController.ts
+++ packages/server/src/controllers/projectController/projectController.ts
@@ -4,29 +4,47 @@ export const getProjectByName = async (req: any, res: any) => {
    const projectName = req.params.projectName;
 
    if (!projectName) {
-      return res.status(400).json({ message: "Project name is required" });
+      return res.status(400).json({
+         success: false,
+         message: "Project name is required",
+      });
    }
 
    try {
       const result = await dbQuery("SELECT * FROM projects WHERE LOWER(project_name) = $1", [projectName.toLowerCase()]);
 
       if (result.rows.length === 0) {
-         return res.status(404).json({ message: `Project with name ${projectName} not found` });
+         return res.status(404).json({
+            success: false,
+            message: `Project with name ${projectName} not found`,
+         });
       } else {
-         return res.json(result.rows[0]);
+         return res.json({
+            success: true,
+            data: result.rows[0],
+         });
       }
    } catch (error) {
       console.error("Error fetching project:", error);
-      return res.status(500).json({ message: "Internal server error" });
+      return res.status(500).json({
+         success: false,
+         message: "Internal server error",
+      });
    }
 };
 
 export const getAllProjects = async (_: any, res: any) => {
    try {
       const result = await dbQuery("SELECT * FROM projects");
-      res.json(result.rows);
+      res.json({
+         success: true,
+         data: result.rows,
+      });
    } catch (error) {
       console.error("Error fetching projects:", error);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({
+         success: true,
+         message: "Internal server error",
+      });
    }
 };
