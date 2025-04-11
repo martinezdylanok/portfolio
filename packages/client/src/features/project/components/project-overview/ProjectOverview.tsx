80c13eaf770d1a4ff22633b6e791bf0209c70332
martinezdylanok <dm97fx@gmail.com>
2025-04-11 15:35:23 +0200

    feat: update ProjectOverview component to enhance layout with responsive styling

--- packages/client/src/features/project/components/project-overview/ProjectOverview.tsx
+++ packages/client/src/features/project/components/project-overview/ProjectOverview.tsx
@@ -3,7 +3,7 @@ import { PROJECT_OVERVIEW_ARIA_LABEL, ProjectOverviewProps } from "./data/projec
 
 const ProjectOverview = ({ project }: ProjectOverviewProps) => {
    return (
-      <section className="project__overview" aria-label={PROJECT_OVERVIEW_ARIA_LABEL}>
+      <section className="max-w-[80%] mx-auto mb-50 project__overview" aria-label={PROJECT_OVERVIEW_ARIA_LABEL}>
          <ProjectOverviewContainer project={project} />
       </section>
    );
