84f4df32f739a2bd8233756facbc43f4b17ecbbd
martinezdylanok <dm97fx@gmail.com>
2025-04-11 08:42:16 +0200

    feat: rename Project interface to ProjectInterface and export HeaderNavigationProps

--- packages/client/src/components/header/components/header-navigation/data/headerNavigationData.ts
+++ packages/client/src/components/header/components/header-navigation/data/headerNavigationData.ts
@@ -3,7 +3,7 @@ const WRAPPER_ARIA_LABEL_TEXT: string = "Navigation wrapper";
 const FORM_ARIA_LABEL_TEXT: string = "Search projects form";
 const INPUT_ARIA_LABEL_TEXT: string = "Search projects";
 
-interface Project {
+export interface ProjectInterface {
    project_id: number;
    project_name: string;
 }
@@ -12,10 +12,8 @@ const SPAN_TEXT: string = "~/documents/Projects $";
 
 const COMMANDS_LIST: string[] = ["ls", "cd"];
 
-interface HeaderNavigationProps {
+export interface HeaderNavigationProps {
    headerIsVisible: boolean;
 }
 
 export { NAV_ARIA_LABEL_TEXT, WRAPPER_ARIA_LABEL_TEXT, FORM_ARIA_LABEL_TEXT, INPUT_ARIA_LABEL_TEXT, SPAN_TEXT, COMMANDS_LIST };
-export type { Project };
-export type { HeaderNavigationProps };
