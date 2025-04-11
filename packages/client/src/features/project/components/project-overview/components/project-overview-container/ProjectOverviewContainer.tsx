13a5581e3a50b61fa0b0739dded799fccdaca812
martinezdylanok <dm97fx@gmail.com>
2025-04-11 15:34:45 +0200

    feat: restructure ProjectOverviewContainer to include ProjectOverviewImage component

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/ProjectOverviewContainer.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/ProjectOverviewContainer.tsx
@@ -1,12 +1,16 @@
 import ProjectOverviewContent from "./components/project-overview-content/ProjectOverviewContent";
 import ProjectOverviewGrid from "./components/project-overview-grid/ProjectOverviewGrid";
+import ProjectOverviewImage from "./components/project-overview-image/ProjectOverviewImage";
 import { PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL, ProjectOvervierContainerProps } from "./data/projectOverviewContainerData";
 
 const ProjectOverviewContainer = ({ project }: ProjectOvervierContainerProps) => {
    return (
-      <div className="project__overview-container" aria-label={PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL}>
-         <ProjectOverviewContent project={project} />
-         <ProjectOverviewGrid project={project} />
+      <div className="flex flex-col project__overview-container" aria-label={PROJECT_OVERVIEW_CONTAINER_ARIA_LABEL}>
+         <ProjectOverviewImage project={project} />
+         <div className="flex project__overview-inner-container">
+            <ProjectOverviewGrid project={project} />
+            <ProjectOverviewContent project={project} />
+         </div>
       </div>
    );
 };
