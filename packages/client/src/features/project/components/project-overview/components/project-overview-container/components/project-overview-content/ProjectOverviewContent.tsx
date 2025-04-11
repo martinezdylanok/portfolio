253b7b905b19b8356f831a08fb83595b1448489a
martinezdylanok <dm97fx@gmail.com>
2025-04-11 15:34:17 +0200

    feat: implement theme-based styling for ProjectOverviewContent component

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
@@ -1,12 +1,13 @@
-//TODO: Fetch project's title and details from database.
-
+import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
 import { PROJECT_OVERVIEW_CONTENT_ARIA_LABEL, ProjectOverviewContentProps } from "./data/projectOverviewContentData";
 
 const ProjectOverviewContent = ({ project }: ProjectOverviewContentProps) => {
+   const { mode } = useThemeContext();
+
    return (
-      <div className="project__overview-content" aria-label={PROJECT_OVERVIEW_CONTENT_ARIA_LABEL}>
-         <h2 className="project__overview-content-title"></h2>
-         <p className="project__overview-project-description">{project.project_overview_description}</p>
+      <div className="max-w-1/2 flex flex-col justify-end project__overview-content" aria-label={PROJECT_OVERVIEW_CONTENT_ARIA_LABEL}>
+         <h2 className={`text-8xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-bold mb-12 project_overview__content-title`}>OVERVIEW</h2>
+         <p className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} text-left project_overview__content-description`}>{project.project_overview_description}</p>
       </div>
    );
 };
