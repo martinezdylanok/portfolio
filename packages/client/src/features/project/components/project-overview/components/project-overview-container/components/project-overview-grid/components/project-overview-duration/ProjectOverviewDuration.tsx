b8c62cc6a1c0e176a37929687fc0938c75fc6720
martinezdylanok <dm97fx@gmail.com>
2025-04-11 15:33:09 +0200

    feat: enhance ProjectOverviewDuration component with theme-based styling

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/ProjectOverviewDuration.tsx
@@ -1,12 +1,16 @@
-// TODO: Create the logic for fetching the duration of the project from the database.
-
+import { useThemeContext } from "../../../../../../../../../../utils/hooks/useTheme";
 import { PROJECT_OVERVIEW_DURATION_ARIA_LABEL, PROJECT_OVERVIEW_DURATION_TITLE_TEXT, ProjectOverviewDurationProps } from "./data/projectOverviewDurationData";
 
 const ProjectOverviewDuration = ({ project }: ProjectOverviewDurationProps) => {
+   const { mode } = useThemeContext();
+
+   // REFACTOR: Make these be in the tailwind config.
+   const textColorClass = mode === "light" ? "text-[#B6CCFE]" : "text-[#E2EAFC]";
+
    return (
       <div className="project__overview-grid-duration" aria-label={PROJECT_OVERVIEW_DURATION_ARIA_LABEL}>
-         <h3 className="project__overview-grid-duration-title">{PROJECT_OVERVIEW_DURATION_TITLE_TEXT}</h3>
-         <p className="project__overview-grid-duration">{project.project_overview_duration}</p>
+         <h3 className={`text-xl mb-6 ${textColorClass}`}>{PROJECT_OVERVIEW_DURATION_TITLE_TEXT}</h3>
+         <p className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__overview-grid-duration`}>{project.project_overview_duration}</p>
       </div>
    );
 };
