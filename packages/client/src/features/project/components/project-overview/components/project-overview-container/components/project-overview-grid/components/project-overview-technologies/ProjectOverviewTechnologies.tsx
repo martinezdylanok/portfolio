63ac14bab1b11217c07178b6fa45c06e9ff55bcd
martinezdylanok <dm97fx@gmail.com>
2025-04-11 15:32:20 +0200

    feat: implement theme-based styling for ProjectOverviewTechnologies component

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-technologies/ProjectOverviewTechnologies.tsx
@@ -1,12 +1,16 @@
-// TODO: Create the logic for fetching the technologies of the project from the database.
-
+import { useThemeContext } from "../../../../../../../../../../utils/hooks/useTheme";
 import { PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL, PROJECT_OVERVIEW_TECHNOLOGIES_TITLE, ProjectOverviewTechnologiesProps } from "./data/projectOverviewTechnologiesData";
 
 const ProjectOverviewTechnologies = ({ project }: ProjectOverviewTechnologiesProps) => {
+   const { mode } = useThemeContext();
+
+   // REFACTOR: Make these be in the tailwind config.
+   const textColorClass = mode === "light" ? "text-[#B6CCFE]" : "text-[#E2EAFC]";
+
    return (
       <div className="project__overview-grid-technologies" aria-label={PROJECT_OVERVIEW_TECHNOLOGIES_ARIA_LABEL}>
-         <h3 className="project__overview-grid-technologies-title">{PROJECT_OVERVIEW_TECHNOLOGIES_TITLE}</h3>
-         <ul className="project__overview-grid-technologies-list">{project.project_overview_technologies}</ul>
+         <h3 className={`text-xl mb-6 ${textColorClass} project__overview-grid-technologies-title`}>{PROJECT_OVERVIEW_TECHNOLOGIES_TITLE}</h3>
+         <p className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__overview-grid-technologies-list`}>{project.project_overview_technologies}</p>
       </div>
    );
 };
