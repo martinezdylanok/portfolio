5daf3d7e6e298c06292bcfebd6d83045760166c5
martinezdylanok <dm97fx@gmail.com>
2025-04-11 15:30:14 +0200

    feat: add ProjectOverviewImage data structure and interface for project details

---
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-image/data/projectOverviewImageData.ts
@@ -0,0 +1,17 @@
+export const PROJECT_OVERVIEW_IMAGE_ARIA_LABEL: string = "Project overview image";
+
+interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_overview_description: string;
+   project_overview_technologies: string;
+   project_overview_duration: string;
+   project_features: string;
+   project_challenges: string;
+   project_final_results: string;
+}
+
+export interface ProjectOverviewImageProps {
+   project: ProjectInterface;
+}
