606652d6ad14e770b6513eebd8263ccb5a2e20c5
martinezdylanok <dm97fx@gmail.com>
2025-04-11 08:48:29 +0200

    feat: rename Project interface to ProjectInterface and export it for consistency

--- packages/client/src/features/projectsList/data/projectsListData.ts
+++ packages/client/src/features/projectsList/data/projectsListData.ts
@@ -4,7 +4,7 @@ const PROJECT_SHOWCASE_IMAGE_ALT_TEXT: string = "Project showcase image";
 const DUMMY_LOGO_IMAGE_URL = "../../../public/projectLists/coronavirus_56dp_EDF2FB_FILL0_wght400_GRAD0_opsz48.svg";
 const DUMMY_LOGO_IMAGE_URL_1 = "../../../public/projectLists/south_america_24dp_EDF2FB_FILL0_wght400_GRAD0_opsz24.svg";
 
-interface Project {
+export interface ProjectInterface {
    project_id: number;
    project_name: string;
    project_description: string;
@@ -12,4 +12,3 @@ interface Project {
 }
 
 export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECT_LOGO_ALT_TEXT, PROJECT_SHOWCASE_IMAGE_ALT_TEXT, DUMMY_LOGO_IMAGE_URL, DUMMY_LOGO_IMAGE_URL_1 };
-export type { Project };
