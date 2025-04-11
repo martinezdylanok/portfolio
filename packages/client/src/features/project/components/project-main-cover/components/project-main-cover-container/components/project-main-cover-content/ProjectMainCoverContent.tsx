953b29bb1a47412a89cc4b1c2cf43ac0082d5523
martinezdylanok <dm97fx@gmail.com>
2025-04-11 12:34:13 +0200

    feat: enhance ProjectMainCoverContent with theme-based styling for project title and description

--- packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
+++ packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
@@ -1,12 +1,20 @@
-// TODO: Create the logic for fetching the title and description of the project from the database.
-
 import { PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL, ProjectMainCoverContentProps } from "./data/projectMainCoverContentData";
+import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
 
 const ProjectMainCoverContent = ({ project }: ProjectMainCoverContentProps) => {
+   const { mode } = useThemeContext();
+
+   const projectNameParts = project.project_name.split(":");
+
+   const firstPart = projectNameParts[0]?.toUpperCase() || "";
+
+   const secondPart = projectNameParts[1]?.trim() || "";
+
    return (
-      <div className="main-cover__content" aria-label={PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL}>
-         <h3 className="main-cover__project-title">{project.project_name}</h3>
-         <p className="main-cover__project-description">{project.project_description}</p>
+      <div className="max-w-1/2 main-cover__content" aria-label={PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL}>
+         {secondPart && <h2 className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} mb-6`}>{secondPart}</h2>}
+         <h3 className={`text-8xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-bold mb-12 main-cover__project-title`}>{firstPart}</h3>
+         <p className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} text-left main-cover__project-description`}>{project.project_description}</p>
       </div>
    );
 };
