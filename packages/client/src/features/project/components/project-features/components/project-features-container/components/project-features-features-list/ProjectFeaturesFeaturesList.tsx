78d4d4bd2f04d5d907e6b1f23f52f5df809179c9
martinezdylanok <dm97fx@gmail.com>
2025-04-12 09:57:26 +0200

    feat: update ProjectFeaturesFeaturesList to use theme-based styling and change title to h4 for consistency

--- packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
+++ packages/client/src/features/project/components/project-features/components/project-features-container/components/project-features-features-list/ProjectFeaturesFeaturesList.tsx
@@ -1,12 +1,14 @@
 // TODO: Create the logic for fetching the features of the project from the database.
-
+import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
 import { PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL, PROJECT_FEATURES_FEATURES_LIST_TITLE, ProjectFeaturesListProps } from "./data/projectFeaturesFeaturesListData";
 
 const ProjectFeaturesFeaturesList = ({ project }: ProjectFeaturesListProps) => {
+   const { mode } = useThemeContext();
+
    return (
       <div className="project__features-container-features-list" aria-label={PROJECT_FEATURES_FEATURES_LIST_ARIA_LABEL}>
-         <h2 className="project__features-container-features-title">{PROJECT_FEATURES_FEATURES_LIST_TITLE}</h2>
-         <p className="project__features-container-features">{project.project_features}</p>
+         <h4 className={`text-2xl mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-semibold project__features-container-features-title`}>{PROJECT_FEATURES_FEATURES_LIST_TITLE}</h4>
+         <p className={`text-justify text-xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__features-container-features`}>{project.project_features}</p>
       </div>
    );
 };
