55c656835d5c217e3f578e04058d7ec9fb903255
martinezdylanok <dm97fx@gmail.com>
2025-04-12 11:18:38 +0200

    feat: enhance ProjectResultsContent with theme-based styling and additional project statistics

--- packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
+++ packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
@@ -1,12 +1,32 @@
-// TODO: Create the logic for fetching the results of the project from the database.
-
+import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
 import { PROJECT_RESULTS_CONTENT_ARIA_LABEL, PROJECT_RESULTS_CONTENT_TITLE, ProjectResultsContentProps } from "./data/projectResultsContentData";
 
 const ProjectResultsContent = ({ project }: ProjectResultsContentProps) => {
+   const { mode } = useThemeContext();
+
+   // TODO: Put these definitions into tailwind's config
+   const lighestColors = mode === "light" ? "text-[#ABC4FF40]" : "text-[#EDF2FB40]";
+   const lightColors = mode === "light" ? "text-[#ABC4FF80]" : "text-[#EDF2FB80]";
+
+   //TODO: Create stats for results in the database and fetch it here.
    return (
-      <div className="project__results-content" aria-label={PROJECT_RESULTS_CONTENT_ARIA_LABEL}>
-         <h2 className="project__results-title">{PROJECT_RESULTS_CONTENT_TITLE}</h2>
-         <p className="project__results">{project.project_final_results}</p>
+      <div className="grid grid-cols-2 grid-rows-2 project__results-content" aria-label={PROJECT_RESULTS_CONTENT_ARIA_LABEL}>
+         <p className={`mt-12 text-2xl col-start-1 row-start-2 text-justify text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__results`}>{project.project_final_results}</p>
+         <div className={`flex gap-20 text-2xl col-start-2 col-end-3 row-start-2 place-self-center text-justify project__results`}>
+            <div className="flex flex-col">
+               <span className={`mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>CTR</span>
+               <span className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>12%</span>
+            </div>
+            <div className="flex flex-col">
+               <span className={`mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>Number of clients</span>
+               <span className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>245</span>
+            </div>
+         </div>
+         <div className="flex flex-col col-start-2 row-start-1 project_results-inner-container">
+            <span className={`text-8xl -mb-6 -z-2 font-bold ${lighestColors}`}>FINAL RESULTS</span>
+            <span className={`text-8xl -mb-6 -z-1 font-bold ${lightColors}`}>FINAL RESULTS</span>
+            <h2 className={`text-8xl font-bold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>{PROJECT_RESULTS_CONTENT_TITLE}</h2>
+         </div>
       </div>
    );
 };
