c35c373f6fecc7ce20e58dcf525d793aafc8bddb
martinezdylanok <dm97fx@gmail.com>
2025-04-14 17:39:22 +0200

    feat: enhance ProjectRelatedContainerContent with theme-based styling and improved layout

--- packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-content/ProjectRelatedContainerContent.tsx
+++ packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-content/ProjectRelatedContainerContent.tsx
@@ -1,10 +1,14 @@
+import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
 import { PROJECT_RELATED_CONTAINER_CONTENT_ARIA_LABEL, PROJECT_RELATED_CONTAINER_CONTENT_TITLE, PROJECT_RELATED_CONTAINER_CONTENT_SUBTITLE } from "./data/projectRelatedContainerConentData";
+import { Link } from "react-router";
 
 const ProjectRelatedContainerContent = () => {
+   const { mode } = useThemeContext();
+
    return (
-      <div className="project__related-content" aria-label={PROJECT_RELATED_CONTAINER_CONTENT_ARIA_LABEL}>
-         <h2 className="project__related-content-title">{PROJECT_RELATED_CONTAINER_CONTENT_TITLE}</h2>
-         <h3 className="project__related-content-subtitle">{PROJECT_RELATED_CONTAINER_CONTENT_SUBTITLE}</h3>
+      <div className="flex justify-between items-center project__related-content" aria-label={PROJECT_RELATED_CONTAINER_CONTENT_ARIA_LABEL}>
+         <h2 className={`text-6xl font-bold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__related-content-title`}>{PROJECT_RELATED_CONTAINER_CONTENT_TITLE}</h2>
+         <span className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__related-content-text`}>{PROJECT_RELATED_CONTAINER_CONTENT_SUBTITLE}</span>
       </div>
    );
 };
