ddff69dbea05de8142f1f6a2b825e0b9ef0f3c14
martinezdylanok <dm97fx@gmail.com>
2025-04-14 17:40:19 +0200

    feat: update ProjectRelated component with theme-based background colors for improved styling

--- packages/client/src/features/project/components/project-related/ProjectRelated.tsx
+++ packages/client/src/features/project/components/project-related/ProjectRelated.tsx
@@ -1,9 +1,15 @@
+import { useThemeContext } from "../../../../utils/hooks/useTheme";
 import ProjectRelatedContainer from "./components/project-related-container/ProjectRelatedContainer";
 import { PROJECT_RELATED_ARIA_LABEL } from "./data/projectRelatedData";
 
 const ProjectRelated = () => {
+   const { mode } = useThemeContext();
+
+   // TODO: Put these definitions into tailwind's config
+   const bgColors = mode === "light" ? "bg-[#D7E3FC]" : "bg-[#C1D3FE]";
+
    return (
-      <section className="project__related" aria-label={PROJECT_RELATED_ARIA_LABEL}>
+      <section className={`h-96 mb-50 ${bgColors} project__related`} aria-label={PROJECT_RELATED_ARIA_LABEL}>
          <ProjectRelatedContainer />
       </section>
    );
