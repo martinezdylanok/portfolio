f384057cf0c9265c30e39d8680ace222367a81d8
martinezdylanok <dm97fx@gmail.com>
2025-04-15 11:30:12 +0200

    fix: update ProjectRelatedContainer to replace videos component with projects component and adjust container width

--- packages/client/src/features/project/components/project-related/components/project-related-container/ProjectRelatedContainer.tsx
+++ packages/client/src/features/project/components/project-related/components/project-related-container/ProjectRelatedContainer.tsx
@@ -1,12 +1,12 @@
 import ProjectRelatedContainerContent from "./components/project-related-container-content/ProjectRelatedContainerContent";
-import ProjectRelatedContainerVideos from "./components/project-related-container-videos/ProjectRelatedContainerVideos";
+import ProjectRelatedContainerProjects from "./components/project-related-container-projects/ProjectRelatedContainerProjects";
 import { PROJECT_RELATED_CONTAINER_ARIA_LABEL } from "./data/projectRelatedContainerData";
 
 const ProjectRelatedContainer = () => {
    return (
-      <div className="max-w-[80%] mx-auto pt-25 project__related-container" aria-label={PROJECT_RELATED_CONTAINER_ARIA_LABEL}>
+      <div className="max-w-[90%] mx-auto py-25 project__related-container" aria-label={PROJECT_RELATED_CONTAINER_ARIA_LABEL}>
          <ProjectRelatedContainerContent />
-         <ProjectRelatedContainerVideos />
+         <ProjectRelatedContainerProjects />
       </div>
    );
 };
