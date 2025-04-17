7c13df07345101b56c23821ed817c2f958443009
martinezdylanok <dm97fx@gmail.com>
2025-04-17 19:38:27 +0200

    test: update ProjectOverview tests to include project prop and improve setup

--- packages/client/src/features/project/components/project-overview/__tests__/ProjectOverview.test.tsx
+++ packages/client/src/features/project/components/project-overview/__tests__/ProjectOverview.test.tsx
@@ -1,9 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import ProjectOverview from "../ProjectOverview";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectOverview component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders the ProjectOverview element", () => {
-      render(<ProjectOverview />);
+      render(<ProjectOverview project={mockProjects[0]} />);
       const projectOverview = screen.getByLabelText("Project overview");
       expect(projectOverview).toBeInTheDocument();
    });
