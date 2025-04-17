46761c5c2707bff9e51a786dff5ae26f0b14e548
martinezdylanok <dm97fx@gmail.com>
2025-04-17 20:22:12 +0200

    test: enhance ProjectOverviewGrid tests with project prop and aria-hidden validation

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/__tests__/ProjectOverviewGrid.test.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/__tests__/ProjectOverviewGrid.test.tsx
@@ -1,10 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import ProjectOverviewGrid from "../ProjectOverviewGrid";
+import { mockProjects, resetModes, setupLightMode } from "./data-utils/dataUtils";
 
 describe("ProjectOverviewGrid tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders ProjectOverviewGrid element", () => {
-      render(<ProjectOverviewGrid />);
+      render(<ProjectOverviewGrid project={mockProjects[0]} />);
       const gridElement = screen.getByLabelText("Project overview grid");
       expect(gridElement).toBeInTheDocument();
+   });
+
+   test("renders a divider that is aria-hidden", () => {
+      render(<ProjectOverviewGrid project={mockProjects[0]} />);
+      const divider = screen.getByTestId("divider");
+      expect(divider).toHaveAttribute("aria-hidden", "true");
+      expect(divider).toBeInTheDocument();
    });
 });
