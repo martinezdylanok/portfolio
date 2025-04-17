73e5253f5269f9487eeae3dd47c36dad41d558cf
martinezdylanok <dm97fx@gmail.com>
2025-04-17 19:27:08 +0200

    refactor: rename variables for clarity in ProjectMainCoverContent component

--- packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
+++ packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/components/project-main-cover-content/ProjectMainCoverContent.tsx
@@ -6,15 +6,23 @@ const ProjectMainCoverContent = ({ project }: ProjectMainCoverContentProps) => {
 
    const projectNameParts = project.project_name.split(":");
 
-   const firstPart = projectNameParts[0]?.toUpperCase() || "";
+   const firstPartName = projectNameParts[0]?.toUpperCase() || "";
 
-   const secondPart = projectNameParts[1]?.trim() || "";
+   const secondPartName = projectNameParts[1]?.trim() || "";
 
    return (
       <div className="max-w-1/2 main-cover__content" aria-label={PROJECT_MAIN_COVER_CONTENT_ARIA_LABEL}>
-         {secondPart && <h2 className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} mb-6`}>{secondPart}</h2>}
-         <h2 className={`text-8xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-bold mb-12 main-cover__project-title`}>{firstPart}</h2>
-         <p className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} text-left main-cover__project-description`}>{project.project_description}</p>
+         {secondPartName && (
+            <h2 className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} mb-6`} data-testid="Second part name">
+               {secondPartName}
+            </h2>
+         )}
+         <h2 className={`text-8xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-bold mb-12 main-cover__project-title`} data-testid="First part name">
+            {firstPartName}
+         </h2>
+         <p className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} text-left main-cover__project-description`} data-testid="Project description">
+            {project.project_description}
+         </p>
       </div>
    );
 };
