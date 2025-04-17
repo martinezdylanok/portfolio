ccaabf6d92a6edc1221595f8c6802fe0f5aae1f3
martinezdylanok <dm97fx@gmail.com>
2025-04-17 19:59:43 +0200

    fix: update ARIA labels and alt text for ProjectOverviewImage component

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-image/ProjectOverviewImage.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-image/ProjectOverviewImage.tsx
@@ -1,12 +1,12 @@
 //TODO: Fetch project's images.
 
-import { PROJECT_OVERVIEW_IMAGE_ARIA_LABEL, ProjectOverviewImageProps } from "./data/projectOverviewImageData";
+import { PROJECT_OVERVIEW_IMAGE_ALT_TEXT, PROJECT_OVERVIEW_IMAGE_CONTAINER_ARIA_LABEL, PROJECT_OVERVIEW_IMAGE_WRAPPER_ARIA_LABEL, ProjectOverviewImageProps } from "./data/projectOverviewImageData";
 
 const ProjectOverviewImage = ({ project }: ProjectOverviewImageProps) => {
    return (
-      <div className="project__overview-image-wrapper" aria-label={PROJECT_OVERVIEW_IMAGE_ARIA_LABEL}>
-         <div className="project__overview_image-container">
-            <img src="" alt="" />
+      <div className="project__overview-image-wrapper" aria-label={PROJECT_OVERVIEW_IMAGE_WRAPPER_ARIA_LABEL}>
+         <div className="project__overview_image-container" aria-label={PROJECT_OVERVIEW_IMAGE_CONTAINER_ARIA_LABEL}>
+            <img src="" alt={PROJECT_OVERVIEW_IMAGE_ALT_TEXT} />
          </div>
       </div>
    );
