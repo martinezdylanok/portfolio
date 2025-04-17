698e658f670cd025421042095449fb8715dad4fa
martinezdylanok <dm97fx@gmail.com>
2025-04-17 18:45:44 +0200

    test: update ProjectMainCoverContainer tests to include project prop and improve setup

--- packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/__tests__/ProjectMainCoverContainer.test.tsx
+++ packages/client/src/features/project/components/project-main-cover/components/project-main-cover-container/__tests__/ProjectMainCoverContainer.test.tsx
@@ -1,9 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import ProjectMainCoverContainer from "../ProjectMainCoverContainer";
 
-describe("ProjectMainCoverContainer", () => {
+import ProjectMainCoverContainer from "../ProjectMainCoverContainer";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
+
+describe("ProjectMainCoverContainer component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("render ProjectMainCoverContainer element", () => {
-      render(<ProjectMainCoverContainer />);
+      render(<ProjectMainCoverContainer project={mockProjects[0]} />);
       const mainCoverContainer = screen.getByLabelText("Project main cover container");
       expect(mainCoverContainer).toBeInTheDocument();
    });
