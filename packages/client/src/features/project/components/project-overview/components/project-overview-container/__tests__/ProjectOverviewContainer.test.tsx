04b5226fad158cec195e8a9a8556a3df43996292
martinezdylanok <dm97fx@gmail.com>
2025-04-17 19:45:15 +0200

    test: update ProjectOverviewContainer tests to include project prop and setup hooks

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/__tests__/ProjectOverviewContainer.test.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/__tests__/ProjectOverviewContainer.test.tsx
@@ -1,9 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import ProjectOverviewContainer from "../ProjectOverviewContainer";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectOverviewContainer tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
    test("renders ProjectOverviewContainer element", () => {
-      render(<ProjectOverviewContainer />);
+      render(<ProjectOverviewContainer project={mockProjects[0]} />);
       const ProjectOverviewContainerElement = screen.getByLabelText("Overview container");
       expect(ProjectOverviewContainerElement).toBeInTheDocument();
    });
