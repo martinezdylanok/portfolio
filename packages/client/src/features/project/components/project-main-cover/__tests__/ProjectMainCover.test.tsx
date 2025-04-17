6cd285590f276c255152d2ea8e72457495a247c0
martinezdylanok <dm97fx@gmail.com>
2025-04-17 18:27:35 +0200

    test: update ProjectMainCover tests to include project prop and improve setup

--- packages/client/src/features/project/components/project-main-cover/__tests__/ProjectMainCover.test.tsx
+++ packages/client/src/features/project/components/project-main-cover/__tests__/ProjectMainCover.test.tsx
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import ProjectMainCover from "../ProjectMainCover";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectMainCover component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("render ProjectMainCover element", () => {
-      render(<ProjectMainCover />);
-      const mainCover = screen.getByRole("region", { name: /project main cover/i });
+      render(<ProjectMainCover project={mockProjects[0]} />);
+      const mainCover = screen.getByLabelText("Project main cover");
       expect(mainCover).toBeInTheDocument();
    });
 });
