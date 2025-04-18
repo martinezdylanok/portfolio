45d09d4a0bcf12f0bf9b17b6885ae838af28cb5a
martinezdylanok <dm97fx@gmail.com>
2025-04-18 11:08:04 +0200

    refactor: improve structure and semantics of ProjectResultsContent component

--- packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
+++ packages/client/src/features/project/components/project-results/components/project-results-container/components/project-results-content/ProjectResultsContent.tsx
@@ -11,20 +11,22 @@ const ProjectResultsContent = ({ project }: ProjectResultsContentProps) => {
    //TODO: Create stats for results in the database and fetch it here.
    return (
       <div className="grid grid-cols-2 grid-rows-2 project__results-content" aria-label={PROJECT_RESULTS_CONTENT_ARIA_LABEL}>
-         <p className={`mt-12 text-2xl col-start-1 row-start-2 text-justify text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__results`}>{project.project_final_results}</p>
-         <div className={`flex gap-20 text-2xl col-start-2 col-end-3 row-start-2 place-self-center text-justify project__results`}>
-            <div className="flex flex-col">
+         <p className={`mt-12 text-2xl col-start-1 row-start-2 text-justify text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__results-paragraph`} data-testid="results-content-paragraph">
+            {project.project_final_results}
+         </p>
+         <div className={`flex gap-20 text-2xl col-start-2 col-end-3 row-start-2 place-self-center text-justify project__results`} data-testid="results-content-container">
+            <div className="flex flex-col project_results-inner-container" data-testid="results-content-inner-container">
                <span className={`mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>CTR</span>
                <span className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>12%</span>
             </div>
-            <div className="flex flex-col">
-               <span className={`mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>Number of clients</span>
+            <div className="flex flex-col project_results-inner-container" data-testid="results-content-inner-container">
+               <span className={`mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>Number of new clients</span>
                <span className={`text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>245</span>
             </div>
          </div>
-         <div className="flex flex-col col-start-2 row-start-1 project_results-inner-container">
-            <span className={`text-8xl -mb-6 -z-2 font-bold ${lighestColors}`}>FINAL RESULTS</span>
-            <span className={`text-8xl -mb-6 -z-1 font-bold ${lightColors}`}>FINAL RESULTS</span>
+         <div className="flex flex-col col-start-2 row-start-1 project__results-header" data-testid="results-content-header">
+            <span className={`text-8xl -mb-6 -z-2 font-bold ${lighestColors}`}>{PROJECT_RESULTS_CONTENT_TITLE}</span>
+            <span className={`text-8xl -mb-6 -z-1 font-bold ${lightColors}`}>{PROJECT_RESULTS_CONTENT_TITLE}</span>
             <h2 className={`text-8xl font-bold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"}`}>{PROJECT_RESULTS_CONTENT_TITLE}</h2>
          </div>
       </div>
