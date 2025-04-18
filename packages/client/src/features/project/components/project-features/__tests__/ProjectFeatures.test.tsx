8fd8bfbfeb6b84ca08a0ec2edf3f1efe14bb28ca
martinezdylanok <dm97fx@gmail.com>
2025-04-18 08:45:14 +0200

    test: enhance ProjectFeatures tests with project prop and lifecycle hooks

--- packages/client/src/features/project/components/project-features/__tests__/ProjectFeatures.test.tsx
+++ packages/client/src/features/project/components/project-features/__tests__/ProjectFeatures.test.tsx
@@ -1,9 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import ProjectFeatures from "../ProjectFeatures";
+import { mockProjects, resetModes, setupLightMode } from "./data-utils/dataUtils";
 
 describe("ProjectFeatures component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders the ProjectFeatures element", () => {
-      render(<ProjectFeatures />);
+      render(<ProjectFeatures project={mockProjects[0]} />);
       const projectFeatures = screen.getByLabelText("Project features");
       expect(projectFeatures).toBeInTheDocument();
    });
