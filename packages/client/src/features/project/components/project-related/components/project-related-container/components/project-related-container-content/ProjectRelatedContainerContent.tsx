044b80866e7c760eb5257d3914d6cf037efaf1ee
martinezdylanok <dm97fx@gmail.com>
2025-04-18 11:53:54 +0200

    fix: include missing PROJECT_RELATED_CONTAINER_CONTENT_LINK_ARIA_LABEL in ProjectRelatedContainerContent

--- packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-content/ProjectRelatedContainerContent.tsx
+++ packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-content/ProjectRelatedContainerContent.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
-import { PROJECT_RELATED_CONTAINER_CONTENT_ARIA_LABEL, PROJECT_RELATED_CONTAINER_CONTENT_SUBTITLE, PROJECT_RELATED_CONTAINER_CONTENT_TITLE } from "./data/projectRelatedContainerConentData";
+import { PROJECT_RELATED_CONTAINER_CONTENT_ARIA_LABEL, PROJECT_RELATED_CONTAINER_CONTENT_LINK_ARIA_LABEL, PROJECT_RELATED_CONTAINER_CONTENT_SUBTITLE, PROJECT_RELATED_CONTAINER_CONTENT_TITLE } from "./data/projectRelatedContainerConentData";
 
 const ProjectRelatedContainerContent = () => {
    const { mode } = useThemeContext();
@@ -9,10 +9,10 @@ const ProjectRelatedContainerContent = () => {
       //TODO: Add placeholders in the DATA file.
       <div className="flex justify-between items-center project__related-content" aria-label={PROJECT_RELATED_CONTAINER_CONTENT_ARIA_LABEL}>
          <h2 className={`text-6xl font-bold mb-6 text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__related-content-title`}>{PROJECT_RELATED_CONTAINER_CONTENT_TITLE}</h2>
-         <Link to={`/projects/`} aria-label={`See all the projects`}>
+         <Link to={`/projects/`} aria-label={PROJECT_RELATED_CONTAINER_CONTENT_LINK_ARIA_LABEL}>
             <span className={`relative group text-2xl font-semibold text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} project__related-content-text`}>
                {PROJECT_RELATED_CONTAINER_CONTENT_SUBTITLE}
-               <span className="absolute left-0 bottom-0 h-[2px] bg-current w-0 group-hover:w-full transition-all duration-300"></span>
+               <span className="absolute left-0 bottom-0 h-[2px] bg-current w-0 group-hover:w-full transition-all duration-300" aria-hidden="true"></span>
             </span>
          </Link>
       </div>
