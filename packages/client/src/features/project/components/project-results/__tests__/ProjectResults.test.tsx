5004d330e474ee32e7f655b5b353a0d3a5618cde
martinezdylanok <dm97fx@gmail.com>
2025-04-18 09:49:07 +0200

    test: update ProjectResults tests to include project prop and lifecycle hooks

--- packages/client/src/features/project/components/project-results/__tests__/ProjectResults.test.tsx
+++ packages/client/src/features/project/components/project-results/__tests__/ProjectResults.test.tsx
@@ -1,9 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import ProjectResults from "../ProjectResults";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectResults component tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders the ProjectResults element", () => {
-      render(<ProjectResults />);
+      render(<ProjectResults project={mockProjects[0]} />);
       const projectResults = screen.getByLabelText("Project results");
       expect(projectResults).toBeInTheDocument();
    });
