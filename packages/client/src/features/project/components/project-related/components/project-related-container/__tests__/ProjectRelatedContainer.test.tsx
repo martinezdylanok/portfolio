c9dd710448675d0edf724899baccae4de1a2dc1c
martinezdylanok <dm97fx@gmail.com>
2025-04-18 11:20:33 +0200

    test: enhance ProjectRelatedContainer tests with routing and setup hooks

--- packages/client/src/features/project/components/project-related/components/project-related-container/__tests__/ProjectRelatedContainer.test.tsx
+++ packages/client/src/features/project/components/project-related/components/project-related-container/__tests__/ProjectRelatedContainer.test.tsx
@@ -1,10 +1,28 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import ProjectRelatedContainer from "../ProjectRelatedContainer";
+import { resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectRelatedContainer tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders ProjectRelatedContainer element", () => {
-      render(<ProjectRelatedContainer />);
-      const containerElement = screen.getByLabelText("Related projects container");
-      expect(containerElement).toBeInTheDocument();
+      render(
+         <MemoryRouter>
+            <ProjectRelatedContainer />
+         </MemoryRouter>,
+      );
+      const relatedContainerElement = screen.getByLabelText("Related projects container");
+      expect(relatedContainerElement).toBeInTheDocument();
    });
 });
