67a8e150d73a19783b3c9623600858931c567c55
martinezdylanok <dm97fx@gmail.com>
2025-04-18 08:35:04 +0200

    feat: update ProjectOverviewContent to use PROJECT_OVERVIEW_CONTENT_TITLE_TEXT constant

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-content/ProjectOverviewContent.tsx
@@ -1,12 +1,12 @@
 import { useThemeContext } from "../../../../../../../../utils/hooks/useTheme";
-import { PROJECT_OVERVIEW_CONTENT_ARIA_LABEL, ProjectOverviewContentProps } from "./data/projectOverviewContentData";
+import { PROJECT_OVERVIEW_CONTENT_ARIA_LABEL, PROJECT_OVERVIEW_CONTENT_TITLE_TEXT, ProjectOverviewContentProps } from "./data/projectOverviewContentData";
 
 const ProjectOverviewContent = ({ project }: ProjectOverviewContentProps) => {
    const { mode } = useThemeContext();
 
    return (
       <div className="max-w-1/2 flex flex-col justify-end project__overview-content" aria-label={PROJECT_OVERVIEW_CONTENT_ARIA_LABEL}>
-         <h2 className={`text-8xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-bold mb-12 project_overview__content-title`}>OVERVIEW</h2>
+         <h2 className={`text-8xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} font-bold mb-12 project_overview__content-title`}>{PROJECT_OVERVIEW_CONTENT_TITLE_TEXT}</h2>
          <p className={`text-2xl text-${mode === "light" ? "[#ABC4FF]" : "[#EDF2FB]"} text-left project_overview__content-description`}>{project.project_overview_description}</p>
       </div>
    );
