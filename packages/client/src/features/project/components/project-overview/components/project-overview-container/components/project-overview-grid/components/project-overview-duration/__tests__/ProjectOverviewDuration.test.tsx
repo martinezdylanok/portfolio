62da45bb6782c97b07c11b77c7e171031e2d9d96
martinezdylanok <dm97fx@gmail.com>
2025-04-18 08:13:34 +0200

    test: update ProjectOverviewDuration tests to use mock project data

--- packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/__tests__/ProjectOverviewDuration.test.tsx
+++ packages/client/src/features/project/components/project-overview/components/project-overview-container/components/project-overview-grid/components/project-overview-duration/__tests__/ProjectOverviewDuration.test.tsx
@@ -1,22 +1,35 @@
 import { render, screen } from "@testing-library/react";
 import ProjectOverviewDuration from "../ProjectOverviewDuration";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectOverviewDuration tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders ProjectOverviewDuration element", () => {
-      render(<ProjectOverviewDuration />);
-      const element = screen.getByLabelText("Project overview duration");
-      expect(element).toBeInTheDocument();
+      render(<ProjectOverviewDuration project={mockProjects[0]} />);
+      const projectOverviewDuration = screen.getByLabelText("Project overview duration");
+      expect(projectOverviewDuration).toBeInTheDocument();
    });
 
    test("renders ProjectOverviewDuration subtitle", () => {
-      render(<ProjectOverviewDuration />);
-      const subtitle = screen.getByRole("heading", { name: "Duration" });
-      expect(subtitle).toBeInTheDocument();
+      render(<ProjectOverviewDuration project={mockProjects[0]} />);
+      const durationTitle = screen.getByRole("heading");
+      expect(durationTitle).toHaveTextContent("Duration");
+      expect(durationTitle).toBeInTheDocument();
    });
 
    test("renders ProjectOverviewDuration value", () => {
-      render(<ProjectOverviewDuration />);
-      const value = screen.getByRole("paragraph");
-      expect(value).toBeInTheDocument();
+      render(<ProjectOverviewDuration project={mockProjects[0]} />);
+      expect(screen.getByText(mockProjects[0].project_overview_duration)).toBeInTheDocument();
    });
 });
