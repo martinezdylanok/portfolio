19078788b319ef0c0cb1353ec5f77b2d71835459
martinezdylanok <dm97fx@gmail.com>
2025-04-18 10:17:35 +0200

    test: update ProjectResultsContainer tests to include project prop and lifecycle hooks

--- packages/client/src/features/project/components/project-results/components/project-results-container/__tests__/ProjectResultsContainer.test.tsx
+++ packages/client/src/features/project/components/project-results/components/project-results-container/__tests__/ProjectResultsContainer.test.tsx
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import ProjectResultsContainer from "../ProjectResultsContainer";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("ProjectResultsContainer tests", () => {
+   beforeAll(() => {
+      vi.mock("../../../../../../../utils/hooks/useTheme.tsx");
+   });
+
+   beforeEach(() => {
+      setupLightMode();
+   });
+
+   afterAll(() => {
+      resetModes();
+   });
+
    test("renders ProjectResultsContainer element", () => {
-      render(<ProjectResultsContainer />);
-      const ProjectResultsContainerElement = screen.getByLabelText("Project results container");
-      expect(ProjectResultsContainerElement).toBeInTheDocument();
+      render(<ProjectResultsContainer project={mockProjects[0]} />);
+      const projectResultsContainer = screen.getByLabelText("Project results container");
+      expect(projectResultsContainer).toBeInTheDocument();
    });
 });
