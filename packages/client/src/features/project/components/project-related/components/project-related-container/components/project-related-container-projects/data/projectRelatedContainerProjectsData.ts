25cadc083d07560e873470884484dcceaa27d6ab
martinezdylanok <dm97fx@gmail.com>
2025-04-19 09:59:53 +0200

    feat: add ARIA label and placeholder constants for project-related container

--- packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-projects/data/projectRelatedContainerProjectsData.ts
+++ packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-projects/data/projectRelatedContainerProjectsData.ts
@@ -1,4 +1,6 @@
 export const PROJECT_RELATED_CONTAINER_PROJECTS_ARIA_LABEL: string = "Related projects container projects";
+export const PROJECT_RELATED_CONTAINER_PROJECTS_IMAGE_ALT_TEXT: string = "Project image";
+export const PROJECT_RELATED_CONTAINER_PROJECTS_SECOND_PART_NAME_PLACEHOLDER: string = "A look into the project";
 
 export interface ProjectInterface {
    project_id: number;
