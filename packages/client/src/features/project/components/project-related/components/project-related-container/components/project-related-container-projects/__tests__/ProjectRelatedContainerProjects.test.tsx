fe59bf9745af099aa28c1c61ae8dac53ddfc1fb2
martinezdylanok <dm97fx@gmail.com>
2025-05-28 13:36:41 +0200

    fix: improve ProjectRelatedContainerProjects tests with enhanced fetch handling and error scenarios

--- packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-projects/__tests__/ProjectRelatedContainerProjects.test.tsx
+++ packages/client/src/features/project/components/project-related/components/project-related-container/components/project-related-container-projects/__tests__/ProjectRelatedContainerProjects.test.tsx
@@ -1,192 +1,242 @@
-import { act, render, screen, within } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { act, render, screen } from "@testing-library/react";
 import ProjectRelatedContainerProjects from "../ProjectRelatedContainerProjects";
-import { firstNamesExpected, mockProjects, resetModes, secondNamesExpected, setupLightMode } from "./test-utils/testUtils";
+import { mockProjects, resetModes, setupLightMode } from "./test-utils/testUtils";
+
+vi.mock("react-router", () => ({
+   ...vi.importActual("react-router"),
+   Link: vi.fn(),
+}));
 
 describe("ProjectRelatedContainerProjects tests", () => {
    beforeAll(() => {
       vi.mock("../../../../../../../../../utils/hooks/useTheme.tsx");
-      vi.useFakeTimers();
    });
 
    beforeEach(() => {
       setupLightMode();
+      global.fetch = vi.fn(async () => {
+         return Promise.resolve({
+            ok: true,
+            status: 200,
+            json: async () => ({ success: true, data: {} }),
+         });
+      }) as vi.Mock;
    });
 
    afterAll(() => {
       resetModes();
-      vi.useRealTimers();
    });
 
-   test("renders ProjectRelatedContainerProjects element", () => {
+   test("renders ProjectRelatedContainerProjects loading span element by default", async () => {
+      // Fetching delayed with promise to emulate a delay in real fetch function
+
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
       render(<ProjectRelatedContainerProjects />);
+
+      const loadingElement = screen.getByLabelText("Loading projects");
+      expect(loadingElement).toBeInTheDocument();
+
+      await act(async () => {
+         resolveFetch({
+            ok: true,
+            json: async () => ({
+               success: true,
+               status: 200,
+               data: {
+                  project_name: "Test Project: Part One",
+               },
+            }),
+         });
+
+         await new Promise(process.nextTick);
+      });
+   });
+
+   test("renders ProjectRelatedContainerProjects error span when API fetch fails", async () => {
+      // Fetching delayed with promise to emulate a delay in real fetch function
+
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
+      render(<ProjectRelatedContainerProjects />);
+
+      const loadingElement = screen.getByLabelText("Loading projects");
+      expect(loadingElement).toBeInTheDocument();
+
+      await act(async () => {
+         resolveFetch({
+            ok: false,
+            status: 500,
+            json: async () => ({ message: "HTTP error! status: 500" }),
+         });
+
+         await new Promise(process.nextTick);
+      });
+
+      const projectErrorSpan = await screen.findByLabelText("Error message");
+
+      expect(projectErrorSpan).toBeInTheDocument();
+      expect(projectErrorSpan).toHaveTextContent("Error loading projects: HTTP error! status: 500");
+      expect(screen.queryByLabelText("Loading proejcts")).not.toBeInTheDocument();
+   });
+
+   test("renders ProjectRelatedContainerProjects error span element when API returns an empy list of projects", async () => {
+      // Fetching delayed with promise to emulate a delay in real fetch function
+
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
+      render(<ProjectRelatedContainerProjects />);
+
+      const loadingProjectSpan = screen.getByLabelText("Loading projects");
+      expect(loadingProjectSpan).toBeInTheDocument();
+
+      await act(async () => {
+         resolveFetch({
+            ok: true,
+            json: async () => ({
+               success: true,
+               data: [],
+            }),
+         });
+
+         await new Promise(process.nextTick);
+      });
+
+      const projectErrorSpan = screen.getByLabelText("Error message");
+      expect(projectErrorSpan).toHaveTextContent("No projects data available or unexpected format");
+      expect(projectErrorSpan).toBeInTheDocument();
+      expect(screen.queryByLabelText("Loading projects")).not.toBeInTheDocument();
+   });
+
+   test("renders ProjectRelatedContainerProjects error span element when API returns null data", async () => {
+      // Fetching delayed with promise to emulate a delay in real fetch function
+
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
+      render(<ProjectRelatedContainerProjects />);
+
+      const loadingProjectSpan = screen.getByLabelText("Loading projects");
+      expect(loadingProjectSpan).toBeInTheDocument();
+
+      await act(async () => {
+         resolveFetch({
+            ok: true,
+            json: async () => ({
+               success: true,
+               data: null,
+            }),
+         });
+
+         await new Promise(process.nextTick);
+      });
+
+      const projectErrorSpan = screen.getByLabelText("Error message");
+      expect(projectErrorSpan).toHaveTextContent("No projects data available or unexpected format.");
+      expect(projectErrorSpan).toBeInTheDocument();
+      expect(screen.queryByLabelText("Loading projects")).not.toBeInTheDocument();
+   });
+
+   test(`renders ProjectRelatedContainerProjects not found span element when returned "API returned success: false"`, async () => {
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
+      render(<ProjectRelatedContainerProjects />);
+
+      const loadingProjectSpan = screen.getByLabelText("Loading projects");
+      expect(loadingProjectSpan).toBeInTheDocument();
+
+      await act(async () => {
+         resolveFetch({
+            ok: true,
+            json: async () => ({
+               success: false,
+               message: "API success returned: false",
+            }),
+         });
+
+         await new Promise(process.nextTick);
+      });
+
+      const projectErrorSpan = screen.getByLabelText("Error message");
+      expect(projectErrorSpan).toHaveTextContent("API success returned: false");
+      expect(projectErrorSpan).toBeInTheDocument();
+   });
+
+   test("renders ProjectRelatedContainerProjects element", async () => {
+      // Fetching delayed with promise to emulate a delay in real fetch function
+
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
+      render(<ProjectRelatedContainerProjects />);
+
+      await act(async () => {
+         resolveFetch({
+            ok: true,
+            json: async () => ({
+               success: true,
+               status: 200,
+               data: mockProjects,
+            }),
+         });
+      });
+
       const relatedContainerProjects = screen.getByLabelText("Related projects container projects");
       expect(relatedContainerProjects).toBeInTheDocument();
    });
 
-   test("renders ProjectRelatedContainerProjects list element", () => {
+   test("renders ProjectRelatedContainerProjects list element", async () => {
+      // Fetching delayed with promise to emulate a delay in real fetch function
+
+      let resolveFetch: (value: unknown) => void;
+      const fetchPromise = new Promise((resolve) => {
+         resolveFetch = resolve;
+      });
+
+      (global.fetch as vi.Mock).mockImplementationOnce(() => fetchPromise);
+
       render(<ProjectRelatedContainerProjects />);
+
+      await act(async () => {
+         resolveFetch({
+            ok: true,
+            json: async () => ({
+               success: true,
+               status: 200,
+               data: mockProjects,
+            }),
+         });
+      });
+
       const relatedContainerProjectsList = screen.getByTestId("related-projects-list");
       expect(relatedContainerProjectsList).toBeInTheDocument();
-   });
-
-   test("renders ProjectRelatedContainerProjects list items", async () => {
-      global.fetch = vi.fn().mockImplementation(() => {
-         return new Promise((resolve) => {
-            resolve({
-               ok: true,
-               json: async () => ({ success: true, data: mockProjects }),
-            });
-         });
-      });
-
-      await act(async () => {
-         render(
-            <MemoryRouter>
-               <ProjectRelatedContainerProjects />
-            </MemoryRouter>,
-         );
-         await vi.runAllTimersAsync();
-      });
-
-      const relatedContainerProjectsList = screen.getByTestId("related-projects-list");
-      const relatedContainerProjectsListItems = within(relatedContainerProjectsList).getAllByRole("link");
-      expect(relatedContainerProjectsListItems.length).toBe(2);
-      relatedContainerProjectsListItems.forEach((item) => {
-         expect(item).toBeInTheDocument();
-      });
-   });
-
-   test("renders ProjectRelatedContainerProjects list items with the correct href attributes", async () => {
-      global.fetch = vi.fn().mockImplementation(() => {
-         return new Promise((resolve) => {
-            resolve({
-               ok: true,
-               json: async () => ({ success: true, data: mockProjects }),
-            });
-         });
-      });
-
-      await act(async () => {
-         render(
-            <MemoryRouter>
-               <ProjectRelatedContainerProjects />
-            </MemoryRouter>,
-         );
-         await vi.runAllTimersAsync();
-      });
-
-      const relatedContainerProjectsList = screen.getByTestId("related-projects-list");
-      const relatedContainerProjectsListItems = within(relatedContainerProjectsList).getAllByRole("link");
-      expect(relatedContainerProjectsListItems.length).toBe(2);
-      relatedContainerProjectsListItems.forEach((item, index) => {
-         expect(item).toHaveAttribute("href", `/projects/${mockProjects[index].project_name}`);
-         expect(item).toBeInTheDocument();
-      });
-   });
-
-   test("renders ProjectRelatedContainerProjects project element", async () => {
-      global.fetch = vi.fn().mockImplementation(() => {
-         return new Promise((resolve) => {
-            resolve({
-               ok: true,
-               json: async () => ({ success: true, data: mockProjects }),
-            });
-         });
-      });
-
-      await act(async () => {
-         render(
-            <MemoryRouter>
-               <ProjectRelatedContainerProjects />
-            </MemoryRouter>,
-         );
-         await vi.runAllTimersAsync();
-      });
-
-      const relatedContainerProjectsList = screen.getByTestId("related-projects-list");
-      const relatedContainerProjectsListItems = within(relatedContainerProjectsList).getAllByTestId("related-project");
-      expect(relatedContainerProjectsListItems.length).toBe(2);
-      relatedContainerProjectsListItems.forEach((item) => {
-         expect(item).toBeInTheDocument();
-      });
-   });
-
-   test("renders ProjectRelatedContainerProjects image element", async () => {
-      global.fetch = vi.fn().mockImplementation(() => {
-         return new Promise((resolve) => {
-            resolve({
-               ok: true,
-               json: async () => ({ success: true, data: mockProjects }),
-            });
-         });
-      });
-
-      await act(async () => {
-         render(
-            <MemoryRouter>
-               <ProjectRelatedContainerProjects />
-            </MemoryRouter>,
-         );
-         await vi.runAllTimersAsync();
-      });
-
-      const relatedContainerProjectsList = screen.getByTestId("related-projects-list");
-      const relatedContainerProjectsImages = within(relatedContainerProjectsList).getAllByAltText("Project image");
-      relatedContainerProjectsImages.forEach((image) => {
-         expect(image).toBeInTheDocument();
-      });
-   });
-
-   test("renders ProjectRelatedContainerProjects first name element", async () => {
-      global.fetch = vi.fn().mockImplementation(() => {
-         return new Promise((resolve) => {
-            resolve({
-               ok: true,
-               json: async () => ({ success: true, data: mockProjects }),
-            });
-         });
-      });
-
-      await act(async () => {
-         render(
-            <MemoryRouter>
-               <ProjectRelatedContainerProjects />
-            </MemoryRouter>,
-         );
-         await vi.runAllTimersAsync();
-      });
-
-      const relatedContainerProjectsLowerPart = screen.getAllByTestId("related-project-lower-part");
-      relatedContainerProjectsLowerPart.forEach((part) => {
-         const found = firstNamesExpected.some((name) => within(part).queryByRole("heading", { level: 3, name }));
-         expect(found).toBe(true);
-      });
-   });
-
-   test("renders ProjectRelatedContainerProjects second name element", async () => {
-      global.fetch = vi.fn().mockImplementation(() => {
-         return new Promise((resolve) => {
-            resolve({
-               ok: true,
-               json: async () => ({ success: true, data: mockProjects }),
-            });
-         });
-      });
-
-      await act(async () => {
-         render(
-            <MemoryRouter>
-               <ProjectRelatedContainerProjects />
-            </MemoryRouter>,
-         );
-         await vi.runAllTimersAsync();
-      });
-
-      const relatedContainerProjectsLowerPart = screen.getAllByTestId("related-project-lower-part");
-      relatedContainerProjectsLowerPart.forEach((part, index) => {
-         const span = within(part).queryByTestId("project-second-part-name");
-         expect(span).toHaveTextContent(secondNamesExpected[index]);
-      });
    });
 });
