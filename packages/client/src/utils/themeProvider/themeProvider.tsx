aeff63b49f1b904bbbfbdd5b773ae490dbc9f3bc
martinezdylanok <dm97fx@gmail.com>
2025-05-30 11:31:00 +0200

    refactor: reorder imports and enhance theme mode handling in ThemeProvider

--- packages/client/src/utils/themeProvider/themeProvider.tsx
+++ packages/client/src/utils/themeProvider/themeProvider.tsx
@@ -1,17 +1,17 @@
-import { useState, useEffect, ReactNode } from "react";
-import { ThemeMode, ThemeContextType } from "../themeContext/data/themeContextData";
+import { ReactNode, useEffect, useState } from "react";
+import { ThemeContextType, ThemeMode } from "../themeContext/data/themeContextData";
 import { themeContext } from "../themeContext/themeContext";
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
    const [mode, setMode] = useState<ThemeMode>(() => {
       if (typeof window === "undefined") return "light";
 
+      const savedMode = localStorage.getItem("themeMode") as ThemeMode;
+      if (savedMode === "light" || savedMode === "dark" || savedMode === "retro") return savedMode;
+
       if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
          return "dark";
       }
-
-      const savedMode = localStorage.getItem("themeMode") as ThemeMode;
-      if (savedMode === "light" || savedMode === "dark") return savedMode;
 
       return "light";
    });
@@ -26,6 +26,20 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
    const toggleMode = () => {
       setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
    };
+
+   /* Toggle mode function with retro mode in it.
+   const toggleMode = () => {
+      setMode((prevMode) => {
+         if (prevMode === "light") {
+            return "dark";
+         } else if (prevMode === "dark") {
+            return "retro";
+         } else { 
+            return "light";
+         }
+      });
+   };
+   */
 
    const contextValue: ThemeContextType = {
       mode,
