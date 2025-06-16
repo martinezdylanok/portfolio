79a92a57576ebf74e527cb223f095940be95f10a
martinezdylanok <dm97fx@gmail.com>
2025-06-16 12:16:35 +0200

    refactor: remove unused mockProjects and streamline theme mode setup functions

--- packages/client/src/components/header/components/header-navigation/__tests__/test-utils/test-utils.ts
+++ packages/client/src/components/header/components/header-navigation/__tests__/test-utils/test-utils.ts
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useThemeContext } from "../../../../../../utils/hooks/useTheme";
 
 const setupLightMode = () => {
@@ -15,29 +14,15 @@ const setupDarkMode = () => {
    });
 };
 
+const setupRetroMode = () => {
+   vi.mocked(useThemeContext).mockReturnValue({
+      mode: "retro",
+      toggleMode: vi.fn(),
+   });
+};
+
 const resetModes = () => {
    vi.resetAllMocks();
 };
 
-const mockProjects = [
-   {
-      project_id: 0,
-      project_name: "Project 1",
-      project_description: "Description 1",
-      project_overview_technologies: "Tech 1",
-   },
-   {
-      project_id: 1,
-      project_name: "Project 2",
-      project_description: "Description 2",
-      project_technologies: "Tech 2",
-   },
-   {
-      project_id: 2,
-      project_name: "Project 3",
-      project_description: "Description 3",
-      project_technologies: "Tech 3",
-   },
-];
-
-export { mockProjects, resetModes, setupDarkMode, setupLightMode };
+export { resetModes, setupDarkMode, setupLightMode, setupRetroMode };
