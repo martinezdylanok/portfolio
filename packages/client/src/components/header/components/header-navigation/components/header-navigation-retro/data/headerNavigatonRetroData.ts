0055ffa82a045bf8c679f48b7453209d89718088
martinezdylanok <dm97fx@gmail.com>
2025-06-16 12:22:38 +0200

    feat: add header navigation retro data constants and project interface

---
+++ packages/client/src/components/header/components/header-navigation/components/header-navigation-retro/data/headerNavigatonRetroData.ts
@@ -0,0 +1,17 @@
+const CONTAINER_ARIA_LABEL_TEXT: string = "Retro container";
+const WRAPPER_ARIA_LABEL_TEXT: string = "Retro navigation wrapper";
+const FORM_ARIA_LABEL_TEXT: string = "Search projects form";
+const INPUT_ARIA_LABEL_TEXT: string = "Search projects";
+
+export interface ProjectInterface {
+   project_id: number;
+   project_name: string;
+   project_description: string;
+   project_technologies: string;
+}
+
+const SPAN_TEXT: string = "~/documents/Projects $";
+
+const COMMANDS_LIST: string[] = ["ls", "cd"];
+
+export { COMMANDS_LIST, CONTAINER_ARIA_LABEL_TEXT, FORM_ARIA_LABEL_TEXT, INPUT_ARIA_LABEL_TEXT, SPAN_TEXT, WRAPPER_ARIA_LABEL_TEXT };
