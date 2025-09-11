9abc00cf778cab4464a2de413b44749e9d1f90b6
martinezdylanok <dm97fx@gmail.com>
2025-09-11 15:24:25 +0200

    fix: adjust header logo dimensions and improve title styling for better responsiveness

--- packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
+++ packages/client/src/components/header/components/header-logo/HeaderLogo.tsx
@@ -1,7 +1,7 @@
-import { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT, LOGO_TITLE_TEXT } from "./data/headerLogoConstants";
 import { useState } from "react";
-import "./styles/logo-animations.css";
 import { useThemeContext } from "../../../../utils/hooks/useTheme";
+import { LOGO_CONTAINER_ARIA_LABEL, LOGO_IMAGE_ALT_TEXT, LOGO_TITLE_TEXT } from "./data/headerLogoConstants";
+import "./styles/logo-animations.css";
 
 // NOTE: Set a width/height for the header__logo-wrapper element that is equal to the width/height of the image.
 
@@ -15,9 +15,9 @@ const HeaderLogo = () => {
    return (
       <div className="header__logo" aria-label={LOGO_CONTAINER_ARIA_LABEL} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
          <a href="/" className="text-center header__logo-link">
-            <div className="relative w-[5.945rem] h-[2.813rem] header__logo-wrapper">
+            <div className="relative w-[6.25rem] h-[3.125rem] header__logo-wrapper">
                {mode === "light" ? <img src="/header/dm_logo_light_mode.svg" className={`absolute top-0 left-0 header__logo-image ${isHovered ? "lifted" : ""}`} alt={LOGO_IMAGE_ALT_TEXT} /> : <img src="/header/dm_logo_dark_mode.svg" className={`absolute top-0 left-0 header__logo-image ${isHovered ? "lifted" : ""}`} alt={LOGO_IMAGE_ALT_TEXT} />}
-               <span className={`absolute left-0 text-[0.74rem] ${mode === "light" ? "text-[#ABC4FF]" : "text-[#EDF2FB]"} min-w-max header__logo-title ${isHovered ? "unlifted" : ""}`}>{LOGO_TITLE_TEXT}</span>
+               <span className={`absolute left-0 w-full text-center text-[0.625rem] font-bold ${mode === "light" ? "text-[#ABC4FF]" : "text-[#EDF2FB]"} header__logo-title ${isHovered ? "unlifted" : ""}`}>{LOGO_TITLE_TEXT}</span>
             </div>
          </a>
       </div>
