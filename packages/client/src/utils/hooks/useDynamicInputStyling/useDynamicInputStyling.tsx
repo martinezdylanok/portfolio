ad70ca9c217abc2a6b944cd34d1bba83e06ed944
martinezdylanok <dm97fx@gmail.com>
2025-09-11 15:26:28 +0200

    refactor: simplify useDynamicInputStyling hook by removing unused refs and effects

--- packages/client/src/utils/hooks/useDynamicInputStyling/useDynamicInputStyling.tsx
+++ packages/client/src/utils/hooks/useDynamicInputStyling/useDynamicInputStyling.tsx
@@ -1,55 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 export const useDynamicInputStyling = () => {
-   const inputRef = useRef<HTMLInputElement>(null);
-   const cursorRef = useRef<HTMLSpanElement>(null);
    const [inputValue, setInputValue] = useState("");
-
-   useEffect(() => {
-      const adjustInputWidth = () => {
-         if (inputRef.current) {
-            const inputElement = inputRef.current;
-            const tempSpan = document.createElement("span");
-            tempSpan.style.visibility = "hidden";
-            tempSpan.style.whiteSpace = "pre";
-            tempSpan.style.font = window.getComputedStyle(inputElement).font;
-            tempSpan.textContent = inputValue;
-            document.body.appendChild(tempSpan);
-
-            const textWidth = tempSpan.offsetWidth;
-            document.body.removeChild(tempSpan);
-
-            inputElement.style.width = `${textWidth}px`;
-         }
-      };
-      adjustInputWidth();
-   }, [inputValue]);
-
-   useEffect(() => {
-      const adjustCursorPosition = () => {
-         if (inputRef.current && cursorRef.current) {
-            const inputElement = inputRef.current;
-            const cursorElement = cursorRef.current;
-
-            const tempSpan = document.createElement("span");
-            tempSpan.style.visibility = "hidden";
-            tempSpan.style.whiteSpace = "pre";
-            tempSpan.style.font = window.getComputedStyle(inputElement).font;
-            tempSpan.textContent = inputValue;
-            document.body.appendChild(tempSpan);
-
-            const textWidth = tempSpan.offsetWidth;
-            document.body.removeChild(tempSpan);
-
-            const inputBorder = parseInt(window.getComputedStyle(inputElement).borderWidth);
-            const inputPadding = parseInt(window.getComputedStyle(inputElement).paddingLeft);
-            const cursorPosition = textWidth + inputBorder + inputPadding;
-
-            cursorElement.style.left = `${cursorPosition}px`;
-         }
-      };
-      adjustCursorPosition();
-   }, [inputValue]);
 
    const handleInputChange = () => {
       return (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -61,7 +13,5 @@ export const useDynamicInputStyling = () => {
       inputValue,
       setInputValue,
       handleInputChange,
-      inputRef,
-      cursorRef,
    };
 };
