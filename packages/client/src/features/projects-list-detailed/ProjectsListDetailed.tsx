378a33d198c21441ffefe36ed8ad5fb920f8b31f
martinezdylanok <dm97fx@gmail.com>
2025-11-16 17:49:02 +0100

    feat: enhance ProjectsListDetailed component with new header structure and improved styling
    - Updated the header section to include multiple descriptive spans for better project context.
    - Adjusted layout to utilize a grid system for improved responsiveness and visual appeal.
    - Enhanced hover effects and color transitions for project list items to improve user experience.

--- packages/client/src/features/projects-list-detailed/ProjectsListDetailed.tsx
+++ packages/client/src/features/projects-list-detailed/ProjectsListDetailed.tsx
@@ -3,7 +3,7 @@ import useFetchData from "../../utils/hooks/useFetchData/useFetchData";
 import ProjectsListDetailedProject from "./components/ProjectsListDetailedProject";
 import { isOdd } from "./data/isOdd";
 import type { ProjectInterface } from "./data/projectsListData";
-import { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_DETAILED_HEADER_DESCRIPTION, PROJECTS_LIST_DETAILED_HEADER_TITLE, PROJECTS_LIST_ERROR_ARIA_LABEL, PROJECTS_LIST_ERROR_TEXT, PROJECTS_LIST_LOADING_ARIA_LABEL, PROJECTS_LIST_LOADING_TEXT, PROJECTS_LIST_NO_DATA_TEXT, PROJECTS_LIST_UL_TEST_ID } from "./data/projectsListData";
+import { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_DETAILED_HEADER_SPAN_01, PROJECTS_LIST_DETAILED_HEADER_SPAN_02, PROJECTS_LIST_DETAILED_HEADER_SPAN_03, PROJECTS_LIST_DETAILED_HEADER_SPAN_04, PROJECTS_LIST_DETAILED_HEADER_SPAN_05, PROJECTS_LIST_DETAILED_HEADER_SPAN_06, PROJECTS_LIST_DETAILED_HEADER_TITLE, PROJECTS_LIST_ERROR_ARIA_LABEL, PROJECTS_LIST_ERROR_TEXT, PROJECTS_LIST_LOADING_ARIA_LABEL, PROJECTS_LIST_LOADING_TEXT, PROJECTS_LIST_NO_DATA_TEXT, PROJECTS_LIST_UL_TEST_ID } from "./data/projectsListData";
 import { useProjectsListDetailedHeaderAnimations } from "./utils/useProjectsListDetailedHeaderAnimations";
 
 const ProjectsListDetailed = () => {
@@ -35,10 +35,27 @@ const ProjectsListDetailed = () => {
    }
 
    return (
-      <div id="projects" className="projects-list-detailed__container pb-25 overflow-hidden bg-page" aria-label={PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT}>
-         <motion.div initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="projects-list-detailed__header flex flex-col min-w-screen min-h-screen pt-50">
-            <motion.h3 className="projects-list-detailed__header-title font-hanken-grotesk text-[200px] mb-12.5 text-heading font-bold leading-none">{PROJECTS_LIST_DETAILED_HEADER_TITLE}</motion.h3>
-            <span className="projects-list-detailed__header-description font-hanken-grotesk text-[80px] text-heading font-semibold leading-none">{PROJECTS_LIST_DETAILED_HEADER_DESCRIPTION}</span>
+      <div id="projects" className="projects-list-detailed__container flex flex-col gap-25 pb-25 overflow-hidden bg-page" aria-label={PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT}>
+         <motion.div className="projects-list-detailed__header grid grid-cols-8 gap-y-25 w-full min-w-screen min-h-screen pt-25">
+            <motion.h3 initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="projects-list-detailed__header-title col-start-1 col-span-7 font-hanken-grotesk text-[200px] mb-12.5 text-heading font-bold leading-none">
+               {PROJECTS_LIST_DETAILED_HEADER_TITLE}
+            </motion.h3>
+            <motion.span initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="projects-list-detailed__header-description row-start-2 col-start-2 col-span-2 text-justify font-hanken-grotesk text-2xl text-body font-semibold leading-none">
+               {PROJECTS_LIST_DETAILED_HEADER_SPAN_01}
+            </motion.span>
+            <motion.span initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="projects-list-detailed__header-description row-start-2 col-start-6 col-span-2 text-right font-hanken-grotesk text-6xl text-heading font-bold leading-none">
+               {PROJECTS_LIST_DETAILED_HEADER_SPAN_02}
+            </motion.span>
+            <motion.div initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="flex flex-col gap-5 row-start-3 col-start-3 col-span-4">
+               <span className="projects-list-detailed__header-description text-left italic font-hanken-grotesk text-2xl text-muted leading-none">{PROJECTS_LIST_DETAILED_HEADER_SPAN_03}</span>
+               <span className="projects-list-detailed__header-description text-right font-hanken-grotesk text-2xl text-accent font-semibold leading-none">{PROJECTS_LIST_DETAILED_HEADER_SPAN_04}</span>
+            </motion.div>
+            <motion.span initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="projects-list-detailed__header-description row-start-4 col-start-6 col-span-2 text-justify font-hanken-grotesk text-2xl text-body font-semibold leading-none">
+               {PROJECTS_LIST_DETAILED_HEADER_SPAN_05}
+            </motion.span>
+            <motion.span initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="projects-list-detailed__header-description row-start-4 col-start-2 col-span-2 text-left font-hanken-grotesk text-6xl text-heading font-bold leading-none">
+               {PROJECTS_LIST_DETAILED_HEADER_SPAN_06}
+            </motion.span>
          </motion.div>
          <ul className="projects-list-detailed__list flex flex-col gap-10 px-5" data-testid={PROJECTS_LIST_UL_TEST_ID}>
             {projects.map((project, index) => {
--- packages/client/src/features/projects-list-detailed/data/projectsListData.ts
+++ packages/client/src/features/projects-list-detailed/data/projectsListData.ts
@@ -6,7 +6,12 @@ const PROJECTS_LIST_ERROR_TEXT: string = "Error loading projects:";
 const PROJECTS_LIST_ERROR_ARIA_LABEL: string = "Error message";
 const PROJECTS_LIST_NO_DATA_TEXT: string = "No projects data available or unexpected format.";
 const PROJECTS_LIST_DETAILED_HEADER_TITLE: string = "PROJECTS";
-const PROJECTS_LIST_DETAILED_HEADER_DESCRIPTION: string = "A curated list of my work, every project here is not just a solution for my client's needs, its a piece of myself...";
+const PROJECTS_LIST_DETAILED_HEADER_SPAN_01: string = "The following is a curated selection of my best work. I like to think of each project as an extension of who I was at that moment, what I imagined, how I thought, and how I brought those ideas to life.";
+const PROJECTS_LIST_DETAILED_HEADER_SPAN_02: string = "A PART OF ME";
+const PROJECTS_LIST_DETAILED_HEADER_SPAN_03: string = "Each new project is another opportunity to communicate what’s coming through you. It’s another chance at bat. Another opportunity to connect. Another page filled in the diary of your inner life.";
+const PROJECTS_LIST_DETAILED_HEADER_SPAN_04: string = "Rick Rubin, The Creative Act: A way of being";
+const PROJECTS_LIST_DETAILED_HEADER_SPAN_05: string = "I approach every idea with curiosity and care, shaping it into something real through the craft I love: full-stack development.";
+const PROJECTS_LIST_DETAILED_HEADER_SPAN_06: string = "CARE & CURIOSITY";
 
 export interface ProjectInterface {
    project_id: number;
@@ -15,4 +20,4 @@ export interface ProjectInterface {
    project_overview_technologies: string;
 }
 
-export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_DETAILED_HEADER_DESCRIPTION, PROJECTS_LIST_DETAILED_HEADER_TITLE, PROJECTS_LIST_ERROR_ARIA_LABEL, PROJECTS_LIST_ERROR_TEXT, PROJECTS_LIST_LOADING_ARIA_LABEL, PROJECTS_LIST_LOADING_TEXT, PROJECTS_LIST_NO_DATA_TEXT, PROJECTS_LIST_UL_TEST_ID };
+export { PROJECTS_LIST_CONTAINER_ARIA_LABEL_TEXT, PROJECTS_LIST_DETAILED_HEADER_SPAN_01, PROJECTS_LIST_DETAILED_HEADER_SPAN_02, PROJECTS_LIST_DETAILED_HEADER_SPAN_03, PROJECTS_LIST_DETAILED_HEADER_SPAN_04, PROJECTS_LIST_DETAILED_HEADER_SPAN_05, PROJECTS_LIST_DETAILED_HEADER_SPAN_06, PROJECTS_LIST_DETAILED_HEADER_TITLE, PROJECTS_LIST_ERROR_ARIA_LABEL, PROJECTS_LIST_ERROR_TEXT, PROJECTS_LIST_LOADING_ARIA_LABEL, PROJECTS_LIST_LOADING_TEXT, PROJECTS_LIST_NO_DATA_TEXT, PROJECTS_LIST_UL_TEST_ID };
